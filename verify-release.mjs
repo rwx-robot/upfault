e39442f09da7a00cc9104259093d7defc87862a6
Ny <johnnynode@gmail.com>
2023-01-08 01:35:06 +0000

    docs(root): document verify-release

--- verify-release.mjs
+++ verify-release.mjs
@@ -4,3 +4,5 @@ import fs from 'fs';
 import path from 'path';
 
 const ESBUILD = '/Users/Wang/Code/webfault-lib/faultless-all/faultless/node_modules/esbuild/bin/esbuild';
+
+console.log('🔍 UpFault v0.2.0 发布验证\n');
