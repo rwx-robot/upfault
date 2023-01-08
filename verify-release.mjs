5ef1b44a19f7b0cf047f174c4e78a7e4cde5e5ee
Ny <johnnynode@gmail.com>
2023-01-08 01:04:40 +0000

    docs(root): clarify verify-release

--- verify-release.mjs
+++ verify-release.mjs
@@ -2,3 +2,5 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
+
+const ESBUILD = '/Users/Wang/Code/webfault-lib/faultless-all/faultless/node_modules/esbuild/bin/esbuild';
