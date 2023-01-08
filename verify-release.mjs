07d77ffccfa6363875e5039e049d35efe4021434
Ny <johnnynode@gmail.com>
2023-01-08 02:24:03 +0000

    test(root): assert verify-release

--- verify-release.mjs
+++ verify-release.mjs
@@ -11,3 +11,4 @@ const checks = [];
 
 // 1. 包版本检查
 const pkgs = ['shared', 'scheduler', 'diff', 'reactivity', 'predict-cache', 'compiler', 'cli', 'devtools', 'ssr', 'runtime'];
+for (const pkg of pkgs) {
