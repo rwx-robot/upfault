0073d8ff3ba1f17e32fe8771e6b638c1db4631aa
Ny <johnnynode@gmail.com>
2023-02-13 02:19:21 +0000

    test(example): extend tests for entry-server

--- examples/counter/src/entry-server.ts
+++ examples/counter/src/entry-server.ts
@@ -2,3 +2,4 @@ import { createRenderer, defaultRendererOptions } from '@upfault/runtime';
 import { hydrate, type VNode } from '@upfault/ssr';
 import { Counter } from './Counter';
 
+export function renderToString(app: VNode): string {
