0d6978ba98a90ee12d041aa29a5844c1ae16a9c2
Ny <johnnynode@gmail.com>
2023-02-13 07:16:24 +0000

    feat(example): implement entry-server

--- examples/counter/src/entry-server.ts
+++ examples/counter/src/entry-server.ts
@@ -24,3 +24,4 @@ export function renderToString(app: VNode): string {
     remove: (child) => { /* no-op */ },
     patchProp: (el, key, prev, next) => {
       if (!el.props) el.props = {};
+      if (next == null || next === false) {
