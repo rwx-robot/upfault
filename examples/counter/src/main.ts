ae95f2c57ce39fb7e33dd714771ae696868e83ba
Ny <johnnynode@gmail.com>
2023-02-14 07:29:22 +0000

    fix(example): handle main

--- examples/counter/src/main.ts
+++ examples/counter/src/main.ts
@@ -2,3 +2,4 @@ import { createRenderer, defaultRendererOptions } from '@upfault/runtime';
 import { Counter } from './Counter';
 
 const renderer = createRenderer(defaultRendererOptions);
+const container = document.getElementById('app')!;
