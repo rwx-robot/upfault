01780ae3cc941f36cc92757c474ef775902cd1fc
Ny <johnnynode@gmail.com>
2023-02-15 01:03:50 +0000

    fix(example): guard test-ssr

--- examples/counter/test-ssr.mjs
+++ examples/counter/test-ssr.mjs
@@ -8,3 +8,4 @@ console.log('has hydrateNodeStream:', 'hydrateNodeStream' in ssr);
 console.log('has hydrateWebStream:', 'hydrateWebStream' in ssr);
 console.log('has partialHydrate:', 'partialHydrate' in ssr);
 console.log('has lazyHydrate:', 'lazyHydrate' in ssr);
+console.log('has isHydrated:', 'isHydrated' in ssr);
