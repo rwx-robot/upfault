1ecc7d8982187a1be781a61cd4d41e8821c78438
Ny <johnnynode@gmail.com>
2023-02-15 04:43:04 +0000

    refactor(example): extract vite.config

--- examples/counter/vite.config.ts
+++ examples/counter/vite.config.ts
@@ -11,3 +11,4 @@ export default defineConfig({
       '@upfault/reactivity': path.resolve(__dirname, '../../packages/reactivity/dist'),
       '@upfault/scheduler': path.resolve(__dirname, '../../packages/scheduler/dist'),
       '@upfault/diff': path.resolve(__dirname, '../../packages/diff/dist'),
+    },
