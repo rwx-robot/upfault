8c2872f032111c5c4160703a449bb62627aa6fcd
Ny <johnnynode@gmail.com>
2023-02-15 04:20:43 +0000

    feat(example): support vite.config

--- examples/counter/vite.config.ts
+++ examples/counter/vite.config.ts
@@ -8,3 +8,4 @@ export default defineConfig({
       '@upfault/runtime': path.resolve(__dirname, '../../packages/runtime/dist'),
       '@upfault/ssr': path.resolve(__dirname, '../../packages/ssr/dist'),
       '@upfault/shared': path.resolve(__dirname, '../../packages/shared/dist'),
+      '@upfault/reactivity': path.resolve(__dirname, '../../packages/reactivity/dist'),
