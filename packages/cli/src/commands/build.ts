28fb9b8b3e8c545be43486691a3a2e01484eec41
Ny <johnnynode@gmail.com>
2023-02-17 03:28:44 +0000

    test(cli): extend tests for build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -18,3 +18,4 @@ export async function build(options: BuildOptions = {}): Promise<void> {
   const cwd = process.cwd();
   const outDir = options.outDir || 'dist';
   const minify = options.minify ?? true;
+  const sourcemap = options.sourcemap ?? true;
