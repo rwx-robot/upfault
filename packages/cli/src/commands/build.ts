f0609d0d3ae5b7f9a54cfa09784a3915af0cacab
Ny <johnnynode@gmail.com>
2023-02-17 08:58:56 +0000

    feat(cli): implement build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -41,3 +41,4 @@ export async function build(options: BuildOptions = {}): Promise<void> {
       console.error(pc.red('❌ 未找到入口文件'));
       console.log(pc.yellow('请确保项目中有 index.html 或 main.ts 文件'));
       process.exit(1);
+    }
