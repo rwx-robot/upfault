af7ae5429c046413257a55bc078f3e74fba8253d
Ny <johnnynode@gmail.com>
2023-02-17 03:19:06 +0000

    feat(cli): support build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -17,3 +17,4 @@ export interface BuildOptions {
 export async function build(options: BuildOptions = {}): Promise<void> {
   const cwd = process.cwd();
   const outDir = options.outDir || 'dist';
+  const minify = options.minify ?? true;
