f4c60086f5ed9d90b7f693a220b832d886ff8edf
Ny <johnnynode@gmail.com>
2023-02-18 08:19:37 +0000

    fix(cli): handle build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -79,3 +79,4 @@ export async function build(options: BuildOptions = {}): Promise<void> {
             });
             build.onEnd((result) => {
               if (result.errors.length > 0) {
+                console.error(pc.red('❌ 构建失败:'));
