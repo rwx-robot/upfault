a896ce42b4c40e1c2f4e19842765b8b3687e02d2
Ny <johnnynode@gmail.com>
2023-02-18 08:18:55 +0000

    test(cli): cover build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -78,3 +78,4 @@ export async function build(options: BuildOptions = {}): Promise<void> {
               console.log(pc.blue('⏳ 正在打包...'));
             });
             build.onEnd((result) => {
+              if (result.errors.length > 0) {
