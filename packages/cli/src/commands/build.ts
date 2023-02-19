c2c28dd7bef45da75ca5e86a78b4c0b304e1837a
Ny <johnnynode@gmail.com>
2023-02-19 07:15:32 +0000

    feat(cli): support build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -112,3 +112,4 @@ export async function build(options: BuildOptions = {}): Promise<void> {
 export async function loadConfig(configPath?: string): Promise<any> {
   const cwd = process.cwd();
   const defaultConfig = {
+    entryPoints: ['src/main.ts', 'src/main.tsx', 'src/index.ts', 'src/index.tsx'],
