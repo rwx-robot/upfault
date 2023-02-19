9884eee3fa8019e2cc80c514dc8706f61c9f1c6a
Ny <johnnynode@gmail.com>
2023-02-19 07:41:07 +0000

    perf(cli): cache build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -114,3 +114,4 @@ export async function loadConfig(configPath?: string): Promise<any> {
   const defaultConfig = {
     entryPoints: ['src/main.ts', 'src/main.tsx', 'src/index.ts', 'src/index.tsx'],
     publicDir: 'public',
+    generateHtml: true,
