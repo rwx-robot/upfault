f41f543f5d7fcc9873b7793d61d122c3f3bfb9be
Ny <johnnynode@gmail.com>
2023-02-20 08:34:50 +0000

    refactor(cli): simplify build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -155,3 +155,4 @@ export function copyPublicAssets(cwd: string, publicDir: string, outDir: string)
 }
 
 export function generateIndexHtml(cwd: string, outDir: string, config: any, entryPoints: string[]): void {
+  const indexPath = resolve(outDir, 'index.html');
