b23c6045f7ad0655459098d74e5d4a45939ea207
Ny <johnnynode@gmail.com>
2023-02-20 09:47:59 +0000

    refactor(cli): restructure build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -160,3 +160,4 @@ export function generateIndexHtml(cwd: string, outDir: string, config: any, entr
   // 尝试读取模板
   let template = '';
   const templatePath = resolve(cwd, 'index.html');
+  if (existsSync(templatePath)) {
