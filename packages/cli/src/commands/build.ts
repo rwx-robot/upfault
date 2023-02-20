fb0d665ba58475604d3a716cf7f40ee962b03f33
Ny <johnnynode@gmail.com>
2023-02-20 03:54:32 +0000

    refactor(cli): restructure build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -136,3 +136,4 @@ export async function loadConfig(configPath?: string): Promise<any> {
 
 export function findEntryPoints(cwd: string, config: any): string[] {
   if (config.entryPoints) {
+    return config.entryPoints.filter((p: string) => existsSync(resolve(process.cwd(), p)));
