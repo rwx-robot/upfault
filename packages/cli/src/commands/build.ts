16895979544337d72b510e0726d2806f9d484d16
Ny <johnnynode@gmail.com>
2023-02-20 05:39:37 +0000

    test(cli): add cases for build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -143,3 +143,4 @@ export function findEntryPoints(cwd: string, config: any): string[] {
 
 export function copyPublicAssets(cwd: string, publicDir: string, outDir: string): void {
   const publicPath = resolve(cwd, publicDir);
+  if (!existsSync(publicPath)) return;
