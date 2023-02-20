e60d801396671f1f08f4f75bcf49ae1cd38c3605
Ny <johnnynode@gmail.com>
2023-02-20 01:44:11 +0000

    docs(cli): clarify build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -127,3 +127,4 @@ export async function loadConfig(configPath?: string): Promise<any> {
 
   // 尝试读取 upfault.config.json
   const configFile = resolve(cwd, 'upfault.config.json');
+  if (existsSync(configFile)) {
