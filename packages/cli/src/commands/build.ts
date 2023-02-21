ad011c2bd04944f108f86a6349552008a47417ce
Ny <johnnynode@gmail.com>
2023-02-21 03:03:05 +0000

    feat(cli): implement build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -172,3 +172,4 @@ export function generateIndexHtml(cwd: string, outDir: string, config: any, entr
   const html = template || `<!DOCTYPE html>
 <html lang="zh-CN">
 <head>
+  <meta charset="UTF-8">
