5b88ce6dca8ff440e31f9fe23151cf4c4a39e04f
Ny <johnnynode@gmail.com>
2023-02-21 03:56:01 +0000

    feat(cli): implement build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -176,3 +176,4 @@ export function generateIndexHtml(cwd: string, outDir: string, config: any, entr
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${config.title || 'UpFault App'}</title>
 </head>
+<body>
