1f1d11759101108186cf90ae7d91d01755bf7766
Ny <johnnynode@gmail.com>
2023-02-22 04:19:33 +0000

    feat(cli): support dev

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -8,3 +8,4 @@ import chokidar from 'chokidar';
 import { resolve, relative } from 'path';
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import pc from 'picocolors';
+import { loadConfig, findEntryPoints, copyPublicAssets, generateIndexHtml } from './build';
