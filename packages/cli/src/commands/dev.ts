cf9ed1ce7320232b624cc7bba032e64618a7cacc
Ny <johnnynode@gmail.com>
2023-02-23 06:35:32 +0000

    refactor(cli): simplify dev

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -57,3 +57,4 @@ export async function dev(options: DevOptions = {}): Promise<void> {
       onRequest: (args: any) => {
         // 开发模式下的请求日志
         console.log(pc.gray(`  ${args.method} ${args.path}`));
+      },
