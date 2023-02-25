9e025dfa3f9d82221bf507e3615a9d98bc929ce8
Ny <johnnynode@gmail.com>
2023-02-25 01:42:24 +0000

    test(cli): assert dev

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -117,3 +117,4 @@ export async function dev(options: DevOptions = {}): Promise<void> {
     watcher.on('change', (path) => {
       const relativePath = relative(cwd, path);
       console.log(pc.yellow(`📝 文件变更: ${relativePath}`));
+      // esbuild serve 会自动处理重新构建
