57569972cb4d2ccf928a07bbd06c15a547f0d498
Ny <johnnynode@gmail.com>
2023-02-25 05:18:28 +0000

    fix(cli): handle dev

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -133,3 +133,4 @@ export async function dev(options: DevOptions = {}): Promise<void> {
     // 优雅关闭
     process.on('SIGINT', async () => {
       console.log(pc.cyan('\n🛑 正在关闭开发服务器...'));
+      await watcher.close();
