67f7ff81ace18548577c0ea91b008a8e4496212c
Ny <johnnynode@gmail.com>
2023-02-26 08:19:24 +0000

    test(cli): add cases for init

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -32,3 +32,4 @@ export async function init(projectName: string, options: InitOptions = {}): Prom
       initial: false,
     });
     if (!response.overwrite) {
+      console.log(pc.yellow('操作已取消'));
