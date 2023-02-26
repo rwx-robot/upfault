699fda797d578ba69cbadde03411f50aa09abaca
Ny <johnnynode@gmail.com>
2023-02-26 07:15:17 +0000

    fix(cli): correct init

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -25,3 +25,4 @@ export async function init(projectName: string, options: InitOptions = {}): Prom
 
   if (existsSync(targetDir) && !force) {
     const { default: prompts } = await import('prompts');
+    const response = await prompts({
