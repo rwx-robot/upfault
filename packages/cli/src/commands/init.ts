b696ba0a055ca3cabfd4b757b69953720012fc84
Ny <johnnynode@gmail.com>
2023-02-26 06:08:06 +0000

    refactor(cli): extract init

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -21,3 +21,4 @@ export async function init(projectName: string, options: InitOptions = {}): Prom
 
   console.log(pc.cyan('🎉 初始化 UpFault 项目...'));
   console.log(pc.gray(`模板: ${template}`));
+  console.log(pc.gray(`目标目录: ${targetDir}`));
