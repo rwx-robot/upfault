b7e6e2e3f9e76a4d045964b6b4e8602e33709fa8
Ny <johnnynode@gmail.com>
2023-02-27 07:05:25 +0000

    fix(cli): correct init

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -68,3 +68,4 @@ export async function init(projectName: string, options: InitOptions = {}): Prom
 function getTemplateFiles(template: string): Record<string, string> {
   const baseFiles = {
     'package.json': JSON.stringify({
+      name: 'upfault-app',
