b1c539209216b9a9911889099a87791b50bcb937
Ny <johnnynode@gmail.com>
2023-02-27 04:30:28 +0000

    test(cli): add cases for init

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -57,3 +57,4 @@ export async function init(projectName: string, options: InitOptions = {}): Prom
 
   console.log(pc.green('\n✅ 项目初始化完成!'));
   console.log(pc.cyan('\n📋 下一步:'));
+  if (projectName) {
