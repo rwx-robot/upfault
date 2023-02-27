1245705541a45fd695ba6d30aa7a49666a5b6b04
Ny <johnnynode@gmail.com>
2023-02-27 07:25:43 +0000

    refactor(cli): simplify init

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -70,3 +70,4 @@ function getTemplateFiles(template: string): Record<string, string> {
     'package.json': JSON.stringify({
       name: 'upfault-app',
       version: '0.2.0',
+      private: true,
