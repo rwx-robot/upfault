b27d854adc6f6279ea526b854dbbd660dfb78a8a
Ny <johnnynode@gmail.com>
2023-02-28 09:01:25 +0000

    refactor(cli): extract init

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -117,3 +117,4 @@ function getTemplateFiles(template: string): Record<string, string> {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>UpFault App</title>
