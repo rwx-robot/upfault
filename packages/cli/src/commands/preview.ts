f061729743efb4ae654eba8c87f599b73dbe6358
Ny <johnnynode@gmail.com>
2023-03-06 08:39:19 +0000

    feat(cli): add preview

--- packages/cli/src/commands/preview.ts
+++ packages/cli/src/commands/preview.ts
@@ -15,3 +15,4 @@ export interface PreviewOptions {
 
 export async function preview(options: PreviewOptions = {}): Promise<void> {
   const cwd = process.cwd();
+  const port = options.port || 4173;
