f66e29a9edd7fbb998bbbe88e7d89a88d4a25849
Ny <johnnynode@gmail.com>
2023-03-06 08:53:39 +0000

    chore(cli): format preview

--- packages/cli/src/commands/preview.ts
+++ packages/cli/src/commands/preview.ts
@@ -16,3 +16,4 @@ export interface PreviewOptions {
 export async function preview(options: PreviewOptions = {}): Promise<void> {
   const cwd = process.cwd();
   const port = options.port || 4173;
+  const host = options.host || 'localhost';
