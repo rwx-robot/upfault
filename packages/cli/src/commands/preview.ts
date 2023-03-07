7879156dfd7ca0c5930d705c0a3bd39623629760
Ny <johnnynode@gmail.com>
2023-03-07 01:21:57 +0000

    test(cli): add cases for preview

--- packages/cli/src/commands/preview.ts
+++ packages/cli/src/commands/preview.ts
@@ -25,3 +25,4 @@ export async function preview(options: PreviewOptions = {}): Promise<void> {
     console.error(pc.red('❌ 未找到 dist 目录'));
     console.log(pc.yellow('请先运行 `upfault build` 构建项目'));
     process.exit(1);
+  }
