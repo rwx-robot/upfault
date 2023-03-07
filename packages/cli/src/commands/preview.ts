66c7622e0fe9a0adf3fb5d33e3bee08724e5f121
Ny <johnnynode@gmail.com>
2023-03-07 01:00:16 +0000

    fix(cli): guard preview

--- packages/cli/src/commands/preview.ts
+++ packages/cli/src/commands/preview.ts
@@ -23,3 +23,4 @@ export async function preview(options: PreviewOptions = {}): Promise<void> {
 
   if (!existsSync(distDir)) {
     console.error(pc.red('❌ 未找到 dist 目录'));
+    console.log(pc.yellow('请先运行 `upfault build` 构建项目'));
