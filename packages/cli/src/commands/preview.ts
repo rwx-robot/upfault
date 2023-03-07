d5fe54865356306f21bae8444c695ce9e19342e8
Ny <johnnynode@gmail.com>
2023-03-07 01:00:20 +0000

    perf(cli): cache preview

--- packages/cli/src/commands/preview.ts
+++ packages/cli/src/commands/preview.ts
@@ -24,3 +24,4 @@ export async function preview(options: PreviewOptions = {}): Promise<void> {
   if (!existsSync(distDir)) {
     console.error(pc.red('❌ 未找到 dist 目录'));
     console.log(pc.yellow('请先运行 `upfault build` 构建项目'));
+    process.exit(1);
