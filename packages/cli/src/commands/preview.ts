1febeac055c2c3d909bb350d824763f5ded68722
Ny <johnnynode@gmail.com>
2023-03-07 07:50:21 +0000

    chore(cli): format preview

--- packages/cli/src/commands/preview.ts
+++ packages/cli/src/commands/preview.ts
@@ -50,3 +50,4 @@ export async function preview(options: PreviewOptions = {}): Promise<void> {
     process.on('SIGINT', async () => {
       console.log(pc.cyan('\n🛑 正在关闭预览服务器...'));
       await server.stop();
+      console.log(pc.gray('预览服务器已停止'));
