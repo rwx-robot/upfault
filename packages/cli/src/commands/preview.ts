f2d824f5a08bb155e54bfe17625fef35d44b3a2c
Ny <johnnynode@gmail.com>
2023-03-07 05:21:08 +0000

    refactor(cli): inline preview

--- packages/cli/src/commands/preview.ts
+++ packages/cli/src/commands/preview.ts
@@ -39,3 +39,4 @@ export async function preview(options: PreviewOptions = {}): Promise<void> {
     }, {
       entryPoints: [], // No entry points for static serving
       bundle: false,
+      outdir: distDir,
