96005c8424595002b436d34e7ec3bdcb0d13d5c7
Ny <johnnynode@gmail.com>
2023-03-10 04:20:12 +0000

    fix(cli): handle types.d

--- packages/cli/src/types.d.ts
+++ packages/cli/src/types.d.ts
@@ -43,3 +43,5 @@ declare module 'esbuild' {
   }
 
   export function serve(serveOptions: ServeOptions, buildOptions: BuildOptions): Promise<ServeResult>;
+
+  export function context(options: BuildOptions): Promise<{ build: () => Promise<BuildResult>; watch: () => Promise<any>; serve: (options: ServeOptions) => Promise<ServeResult>; dispose: () => Promise<void> }>;
