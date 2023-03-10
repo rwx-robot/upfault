e5639cbaee5e847763211285ee8053b846474dcc
Ny <johnnynode@gmail.com>
2023-03-10 09:42:44 +0000

    fix(cli): fix types.d

--- packages/cli/src/types.d.ts
+++ packages/cli/src/types.d.ts
@@ -65,3 +65,4 @@ declare module 'commander' {
 declare module 'chokidar' {
   export interface FSWatcher {
     on(event: string, listener: (...args: any[]) => void): this;
+    close(): Promise<void>;
