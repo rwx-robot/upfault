902855f20a922e58f534fc8f6217ae9c5662ab11
Ny <johnnynode@gmail.com>
2023-03-12 08:52:31 +0000

    test(compiler): add cases for block-tree.test

--- packages/compiler/src/block-tree.test.ts
+++ packages/compiler/src/block-tree.test.ts
@@ -1,3 +1,5 @@
 import { describe, it, expect } from 'vitest';
 import { parse } from './parser';
 import { buildBlockTree, BlockGranularity } from './block-tree';
+
+describe('Block Tree Builder', () => {
