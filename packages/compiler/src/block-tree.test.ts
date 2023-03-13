086b64e2fe1d09ef570faae651cc03569946d2b0
Ny <johnnynode@gmail.com>
2023-03-13 08:55:34 +0000

    chore(compiler): clean up block-tree.test

--- packages/compiler/src/block-tree.test.ts
+++ packages/compiler/src/block-tree.test.ts
@@ -43,3 +43,4 @@ describe('Block Tree Builder', () => {
 
   describe('粒度控制', () => {
     it('Coarse 粒度：仅根 Block', () => {
+      const { ast, context } = parse('<div><span>{{ a }}</span>{{ b }}</div>');
