5db3228ca729c1482fe300b4895173fe73ae5d8c
Ny <johnnynode@gmail.com>
2023-03-13 06:04:59 +0000

    chore(compiler): format block-tree.test

--- packages/compiler/src/block-tree.test.ts
+++ packages/compiler/src/block-tree.test.ts
@@ -29,3 +29,4 @@ describe('Block Tree Builder', () => {
       expect(result.rootBlock.type).toBe('root');
       expect(result.allBlocks).toHaveLength(1);
       expect(result.dynamicNodeCount).toBe(0);
+      expect(result.staticNodeCount).toBeGreaterThan(0);
