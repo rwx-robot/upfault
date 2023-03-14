0820446127c248e7a256d0e03a1dfa88f4c39d2f
Ny <johnnynode@gmail.com>
2023-03-14 02:49:08 +0000

    feat(compiler): introduce block-tree.test

--- packages/compiler/src/block-tree.test.ts
+++ packages/compiler/src/block-tree.test.ts
@@ -56,3 +56,4 @@ describe('Block Tree Builder', () => {
       // v-for 应创建单独 Block
       const forBlocks = result.allBlocks.filter(b => b.type === 'for');
       expect(forBlocks.length).toBeGreaterThanOrEqual(1);
+    });
