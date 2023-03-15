8a5abbe684bd07d33da9766fda89bbb06eeeab02
Ny <johnnynode@gmail.com>
2023-03-15 07:58:20 +0000

    test(compiler): cover block-tree

--- packages/compiler/src/block-tree.ts
+++ packages/compiler/src/block-tree.ts
@@ -9,3 +9,4 @@ import { VNodeFlags, VNodeType } from '@upfault/shared';
 import type { TemplateAST, TemplateNode, ElementNode, ComponentNode, SlotNode, IfNode, ForNode, InterpolationNode, CommentNode, TextNode, CompileTimeFlags, CompileContext } from './parser';
 
 // ============================================================================
+// Block 类型定义
