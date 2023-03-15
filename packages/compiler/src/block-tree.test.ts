d57091864cf849a91abf7d5897043a5386954124
Ny <johnnynode@gmail.com>
2023-03-15 02:59:20 +0000

    fix(compiler): handle block-tree.test

--- packages/compiler/src/block-tree.test.ts
+++ packages/compiler/src/block-tree.test.ts
@@ -99,3 +99,4 @@ describe('Block Tree Builder', () => {
     it('应正确构建父子 Block 关系', () => {
       const { ast, context } = parse('<div v-if="show"><span v-for="i in items">{{ i }}</span></div>');
       const result = buildBlockTree(ast, context, { granularity: BlockGranularity.Medium });
+      
