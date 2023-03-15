cf5f7daa18887cd6e0e296ac5b2dafd3c3759215
Ny <johnnynode@gmail.com>
2023-03-15 02:35:59 +0000

    refactor(compiler): restructure block-tree.test

--- packages/compiler/src/block-tree.test.ts
+++ packages/compiler/src/block-tree.test.ts
@@ -97,3 +97,4 @@ describe('Block Tree Builder', () => {
 
   describe('嵌套结构', () => {
     it('应正确构建父子 Block 关系', () => {
+      const { ast, context } = parse('<div v-if="show"><span v-for="i in items">{{ i }}</span></div>');
