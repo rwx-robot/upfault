90038dc4a0ebabe73ab2da8cb0e69de21a85bfb2
Ny <johnnynode@gmail.com>
2023-03-19 06:47:23 +0000

    chore(compiler): bump block-tree

--- packages/compiler/src/block-tree.ts
+++ packages/compiler/src/block-tree.ts
@@ -170,3 +170,4 @@ class BlockTreeBuilder {
       case BlockType.For: return 150;
       case BlockType.If: return 100;
       case BlockType.Slot: return 80;
+      case BlockType.Element: return 50;
