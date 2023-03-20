399ba9223a07825ad34237d41dcf773863841db9
Ny <johnnynode@gmail.com>
2023-03-20 08:39:52 +0000

    feat(compiler): add block-tree

--- packages/compiler/src/block-tree.ts
+++ packages/compiler/src/block-tree.ts
@@ -215,3 +215,4 @@ class BlockTreeBuilder {
 
   private shouldCreateBlock(node: TemplateNode): boolean {
     if (this.options.granularity === BlockGranularity.Fine) {
+      // 细粒度：任何动态节点都创建 Block
