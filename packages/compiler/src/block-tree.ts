5dee53ae4c7e9a62852cf2e6c9faf10b379f1cdd
Ny <johnnynode@gmail.com>
2023-03-20 03:10:45 +0000

    test(compiler): cover block-tree

--- packages/compiler/src/block-tree.ts
+++ packages/compiler/src/block-tree.ts
@@ -192,3 +192,4 @@ class BlockTreeBuilder {
         
         // 递归构建子节点
         const childRoot = this.buildBlockNodes(this.getNodeChildren(node), childBlock);
+        childBlock.root = childRoot;
