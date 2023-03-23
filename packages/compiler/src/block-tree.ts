4a1cf740752af507a52551c8a65f78257e151a7d
Ny <johnnynode@gmail.com>
2023-03-23 01:03:29 +0000

    feat(compiler): introduce block-tree

--- packages/compiler/src/block-tree.ts
+++ packages/compiler/src/block-tree.ts
@@ -305,3 +305,4 @@ class BlockTreeBuilder {
   private convertElement(node: ElementNode, id: string, parentBlock: Block): BlockNode {
     const props = node.props.map(p => this.convertProp(p));
     const isDynamic = this.isDynamicElement(node);
+    const flags = this.calculateNodeFlags(node, props);
