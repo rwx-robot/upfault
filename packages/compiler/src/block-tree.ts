f6641039a32a7c6ed0ef799dc2f289a80efcdfd1
Ny <johnnynode@gmail.com>
2023-03-27 04:54:11 +0000

    feat(compiler): implement block-tree

--- packages/compiler/src/block-tree.ts
+++ packages/compiler/src/block-tree.ts
@@ -488,3 +488,4 @@ class BlockTreeBuilder {
     if (props.some(p => p.isEvent)) flags |= 32; // HAS_EVENT
     if (props.some(p => p.isSlot)) flags |= 64; // HAS_SLOT
     if (props.some(p => p.isDynamic && !p.isEvent && !p.isSlot)) flags |= 8; // PURE_DYNAMIC
+    if (props.filter(p => p.isDynamic).length > 1) flags |= 16; // MULTI_DYNAMIC
