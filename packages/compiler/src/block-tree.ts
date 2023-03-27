7645489ace0ef65fb0cded5062801027a3cbed09
Ny <johnnynode@gmail.com>
2023-03-27 04:42:28 +0000

    feat(compiler): introduce block-tree

--- packages/compiler/src/block-tree.ts
+++ packages/compiler/src/block-tree.ts
@@ -487,3 +487,4 @@ class BlockTreeBuilder {
     
     if (props.some(p => p.isEvent)) flags |= 32; // HAS_EVENT
     if (props.some(p => p.isSlot)) flags |= 64; // HAS_SLOT
+    if (props.some(p => p.isDynamic && !p.isEvent && !p.isSlot)) flags |= 8; // PURE_DYNAMIC
