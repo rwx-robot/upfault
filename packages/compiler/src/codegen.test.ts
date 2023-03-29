88abcecb8e6af6d96defa95a98135987bfdd51c0
Ny <johnnynode@gmail.com>
2023-03-29 06:03:25 +0000

    feat(compiler): add codegen.test

--- packages/compiler/src/codegen.test.ts
+++ packages/compiler/src/codegen.test.ts
@@ -2,3 +2,4 @@ import { describe, it, expect } from 'vitest';
 import { parse } from './parser';
 import { buildBlockTree, BlockGranularity } from './block-tree';
 import { generateRenderFunction } from './codegen';
+
