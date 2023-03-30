9eaa49a5ffa8b9ec86a09e59afd44f7fd6b95164
Ny <johnnynode@gmail.com>
2023-03-30 08:33:22 +0000

    refactor(compiler): simplify codegen.test

--- packages/compiler/src/codegen.test.ts
+++ packages/compiler/src/codegen.test.ts
@@ -58,3 +58,4 @@ describe('Code Generator', () => {
     it('应生成事件处理渲染代码', () => {
       const result = compileTemplate('<button @click="handleClick">Click</button>');
       
+      expect(result.code).toContain('createElementVNode');
