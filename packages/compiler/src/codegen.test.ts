c19be18b75890a421c18fca51c60f6fe4b73b5b5
Ny <johnnynode@gmail.com>
2023-04-01 03:17:40 +0000

    test(compiler): cover codegen.test

--- packages/compiler/src/codegen.test.ts
+++ packages/compiler/src/codegen.test.ts
@@ -119,3 +119,4 @@ describe('Code Generator', () => {
       // 组件名从文件名提取
       expect(result.metadata.templateHash).toMatch(/^0x[0-9a-f]{8}$/);
       expect(result.metadata.compileFlags).toBeGreaterThan(0);
+      expect(result.metadata.helpers).toContain('h');
