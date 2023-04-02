9ba2cf8f4bcd41dbcac35daffbac5cd7c4c32dab
Ny <johnnynode@gmail.com>
2023-04-02 04:41:06 +0000

    test(compiler): assert codegen

--- packages/compiler/src/codegen.ts
+++ packages/compiler/src/codegen.ts
@@ -4,3 +4,5 @@
  * 从 Block Tree 生成渲染函数代码
  * 产出：带有编译时 metadata 的渲染函数，支持运行时高效 Diff
  */
+
+import { BlockType, type Block, type BlockNode, type BlockTreeResult, type BlockGranularity } from './block-tree';
