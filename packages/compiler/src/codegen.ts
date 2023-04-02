0abfd91fbb70a93a8a8e1b732381fb3e9c9b47d7
Ny <johnnynode@gmail.com>
2023-04-02 05:27:59 +0000

    fix(compiler): fix codegen

--- packages/compiler/src/codegen.ts
+++ packages/compiler/src/codegen.ts
@@ -8,3 +8,4 @@
 import { BlockType, type Block, type BlockNode, type BlockTreeResult, type BlockGranularity } from './block-tree';
 import type { TemplateAST, CompileContext, ImportSpec, CompileError, CompileWarning } from './parser';
 import { VNodeType, PatchFlags } from '@upfault/shared';
+import type { VNodeFlags } from '@upfault/shared';
