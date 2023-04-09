8d86606724193e01ce680208d2879d9a9ac0e86a
Ny <johnnynode@gmail.com>
2023-04-09 08:11:36 +0000

    fix(compiler): fix codegen

--- packages/compiler/src/codegen.ts
+++ packages/compiler/src/codegen.ts
@@ -313,3 +313,4 @@ class CodeGenerator {
 
   private generateTextCode(node: BlockNode, hoisted?: boolean): string {
     if (node.isDynamic) {
+      return `createTextVNode(_ctx.${node.dynamicProps[0] || 'textContent'})`;
