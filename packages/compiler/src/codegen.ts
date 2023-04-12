1882c25149bbd66b3a79f74b6bb0ba9b65462b74
Ny <johnnynode@gmail.com>
2023-04-12 06:57:17 +0000

    test(compiler): cover codegen

--- packages/compiler/src/codegen.ts
+++ packages/compiler/src/codegen.ts
@@ -433,3 +433,4 @@ export interface CompilerResult {
 export function compile(template: string, options: CompilerOptions): CompilerResult {
   // 1. Parse
   const { parse } = require('./parser');
+  const { ast, context } = parse(template, { 
