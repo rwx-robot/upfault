3d57b0d756bb57dc1ff50fcacd59976efcce2363
Ny <johnnynode@gmail.com>
2023-04-13 01:24:40 +0000

    test(compiler): extend tests for codegen

--- packages/compiler/src/codegen.ts
+++ packages/compiler/src/codegen.ts
@@ -449,3 +449,4 @@ export function compile(template: string, options: CompilerOptions): CompilerRes
   // 3. Generate Code
   const { generateRenderFunction } = require('./codegen');
   const result = generateRenderFunction(ast, context, blockTree, {
+    mode: 'module',
