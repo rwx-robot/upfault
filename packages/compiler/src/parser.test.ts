a2e31ac528104cfcad0f9f87b562585750822c27
Ny <johnnynode@gmail.com>
2023-04-15 05:35:04 +0000

    test(compiler): add cases for parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -8,3 +8,4 @@ describe('Template Parser', () => {
       expect(ast.children).toHaveLength(1);
       expect(ast.children[0].type).toBe('Text');
       expect((ast.children[0] as any).content).toBe('Hello World');
+    });
