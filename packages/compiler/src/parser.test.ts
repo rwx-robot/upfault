ca0656ea1bc5cd33eca9b5dfd7197a9e8e710200
Ny <johnnynode@gmail.com>
2023-04-15 07:05:17 +0000

    feat(compiler): introduce parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -15,3 +15,4 @@ describe('Template Parser', () => {
       expect(ast.children).toHaveLength(1);
       expect(ast.children[0].type).toBe('Element');
       expect((ast.children[0] as any).tag).toBe('div');
+      expect((ast.children[0] as any).children).toHaveLength(1);
