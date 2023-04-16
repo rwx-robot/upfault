0aed49c0ef892c87bfbb0a291292ab1d9e10dafa
Ny <johnnynode@gmail.com>
2023-04-16 07:58:36 +0000

    feat(compiler): implement parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -60,3 +60,4 @@ describe('Template Parser', () => {
       expect(el.props).toHaveLength(2);
       expect(el.props[0].name).toBe('class');
       expect(el.props[0].isDynamic).toBe(true);
+      expect(el.props[1].name).toBe('id');
