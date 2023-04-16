f4521724e60d4bd748083598963a32821ed37d51
Ny <johnnynode@gmail.com>
2023-04-16 07:19:06 +0000

    feat(compiler): add parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -56,3 +56,4 @@ describe('Template Parser', () => {
 
     it('应解析动态属性 (: 或 v-bind:)', () => {
       const { ast } = parse('<div :class="cls" v-bind:id="id"></div>');
+      const el = ast.children[0] as any;
