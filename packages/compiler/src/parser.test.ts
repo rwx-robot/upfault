bfc0f21dec7e8097338f64bb6e093ecd09369ea2
Ny <johnnynode@gmail.com>
2023-04-16 08:55:17 +0000

    test(compiler): extend tests for parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -65,3 +65,4 @@ describe('Template Parser', () => {
     });
 
     it('应解析事件 (@ 或 v-on:)', () => {
+      const { ast } = parse('<button @click="handleClick" v-on:submit.prevent="onSubmit"></button>');
