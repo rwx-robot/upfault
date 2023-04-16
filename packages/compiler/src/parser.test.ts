a458fe85a3f458f89f4ea016dd2bcabc7c994c2b
Ny <johnnynode@gmail.com>
2023-04-16 09:36:09 +0000

    fix(compiler): handle parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -67,3 +67,4 @@ describe('Template Parser', () => {
     it('应解析事件 (@ 或 v-on:)', () => {
       const { ast } = parse('<button @click="handleClick" v-on:submit.prevent="onSubmit"></button>');
       const el = ast.children[0] as any;
+      expect(el.props).toHaveLength(2);
