954e2b2df750634972c13c395c21ffa8d45babd6
Ny <johnnynode@gmail.com>
2023-04-17 01:21:20 +0000

    refactor(compiler): simplify parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -70,3 +70,4 @@ describe('Template Parser', () => {
       expect(el.props).toHaveLength(2);
       expect(el.props[0].name).toBe('click');
       expect(el.props[0].isEvent).toBe(true);
+      expect(el.props[1].name).toBe('submit');
