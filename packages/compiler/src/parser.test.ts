35b7006429f1f624520d8126f8ccb49cee1ffa85
Ny <johnnynode@gmail.com>
2023-04-17 01:49:35 +0000

    test(compiler): assert parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -72,3 +72,4 @@ describe('Template Parser', () => {
       expect(el.props[0].isEvent).toBe(true);
       expect(el.props[1].name).toBe('submit');
       expect(el.props[1].isEvent).toBe(true);
+      expect(el.props[1].eventModifiers).toContain('prevent');
