b3f1b62e9970a009662685bc3178083885205f14
Ny <johnnynode@gmail.com>
2023-04-17 06:02:54 +0000

    docs(compiler): document parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -91,3 +91,4 @@ describe('Template Parser', () => {
       const el = ast.children[0] as any;
       expect(el.type).toBe('Component');
       expect(el.name).toBe('MyComponent');
+      expect(el.isComponent).toBe(true);
