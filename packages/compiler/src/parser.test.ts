bfa60c15f6e028ca18987b433e49b2533ab9cee3
Ny <johnnynode@gmail.com>
2023-04-17 03:26:30 +0000

    test(compiler): add cases for parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -78,3 +78,4 @@ describe('Template Parser', () => {
     it('应解析布尔属性简写', () => {
       const { ast } = parse('<input disabled required />');
       const el = ast.children[0] as any;
+      expect(el.props).toHaveLength(2);
