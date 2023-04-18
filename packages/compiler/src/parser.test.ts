b820509943d614f53fd1694c22f231329020d0d7
Ny <johnnynode@gmail.com>
2023-04-18 05:52:23 +0000

    docs(compiler): annotate parser.test

--- packages/compiler/src/parser.test.ts
+++ packages/compiler/src/parser.test.ts
@@ -132,3 +132,4 @@ describe('Template Parser', () => {
   describe('插槽', () => {
     it('应解析默认插槽', () => {
       const { ast } = parse('<slot>Fallback</slot>');
+      const el = ast.children[0] as any;
