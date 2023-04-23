b08b07eaa355b21ba21f8cbddc5d88ab93e7e110
Ny <johnnynode@gmail.com>
2023-04-23 07:04:53 +0000

    feat(compiler): implement parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -192,3 +192,4 @@ export interface ParseResult {
 
 export function parse(template: string, options: ParseOptions = {}): ParseResult {
   const context = createCompileContext(template, options);
+  const ast = parseTemplate(template, context);
