c0ac822fede7596330dc9645f5ee464653e2a68f
Ny <johnnynode@gmail.com>
2023-04-26 07:51:50 +0000

    fix(compiler): fix parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -314,3 +314,4 @@ private parseElement(): TemplateNode | null {
       const forProp = props.find(p => p.name === 'for' || p.name === 'v-for');
       if (forProp) {
 const forValue = forProp.value?.type === 'Expression' ? forProp.value.value : forProp.value?.value?.toString() || '';
+        const match = forValue.match(/^\s*(\w+)\s+(?:in|of)\s+(.+)\s*$/);
