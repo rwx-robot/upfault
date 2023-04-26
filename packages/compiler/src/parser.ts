4dc55d3d0e23c074b22fc2af1c3c9e4ee19b2713
Ny <johnnynode@gmail.com>
2023-04-26 01:18:01 +0000

    test(compiler): add cases for parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -289,3 +289,4 @@ private parseElement(): TemplateNode | null {
     const tag = this.parseTagName();
     const isComponent = tag.length > 0 && tag[0] !== undefined && tag[0] === tag[0].toUpperCase() && tag.length > 1;
     
+    const props = this.parseProps();
