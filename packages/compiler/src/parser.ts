dc40121b4ac23d18266e9a9fb93699d5280c8e2e
Ny <johnnynode@gmail.com>
2023-04-26 08:57:52 +0000

    feat(compiler): introduce parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -319,3 +319,4 @@ const forValue = forProp.value?.type === 'Expression' ? forProp.value.value : fo
         const source = match ? match[2] : forValue;
         const keyProp = props.find(p => p.name === 'key');
 const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.value : keyProp?.value?.value?.toString() || null;
+        
