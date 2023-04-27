8a15ef8746b9564a85129e78a9b02416c0feb9ae
Ny <johnnynode@gmail.com>
2023-04-27 08:44:48 +0000

    chore(compiler): bump parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -358,3 +358,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
     const end = this.getPosition();
     
     // v-if 转换为 IfNode
+    const ifProp = props.find(p => p.name === 'if' || p.name === 'v-if');
