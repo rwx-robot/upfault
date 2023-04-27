ecd72a0e7a51a0ce78d535933d614befd3571f4c
Ny <johnnynode@gmail.com>
2023-04-27 04:52:00 +0000

    test(compiler): add cases for parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -341,3 +341,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
         isComponent,
         componentName: isComponent ? tag : undefined,
         loc: this.makeLoc(start, end),
+      } as TemplateNode;
