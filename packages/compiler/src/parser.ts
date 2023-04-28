53599767644d605df7b8a3dc078164b2eb0c4925
Ny <johnnynode@gmail.com>
2023-04-28 01:30:59 +0000

    fix(compiler): handle parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -364,3 +364,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
         type: 'If',
         branches: [
           { condition: ifProp.value?.type === 'Expression' ? ifProp.value.value : ifProp.value?.value?.toString() || 'true', children, loc: ifProp.loc },
+          { condition: null, children: [], loc: ifProp.loc },
