8abce7f054fb61e63456c508e3449dc9a5bddb49
Ny <johnnynode@gmail.com>
2023-04-28 04:03:45 +0000

    chore(compiler): clean up parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -378,3 +378,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
       const value = match ? match[1] : 'item';
       const source = match ? match[2] : forValue;
       const keyProp = props.find(p => p.name === 'key');
+      const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.value : keyProp?.value?.value?.toString() || null;
