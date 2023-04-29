ffd8fbe5703ead00ae28f887e85c049d84bb719d
Ny <johnnynode@gmail.com>
2023-04-29 02:07:32 +0000

    refactor(compiler): restructure parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -411,3 +411,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
       this.skipWhitespace();
       if (this.isEnd() || this.startsWith('>') || this.startsWith('/>')) break;
       
+      const start = this.getPosition();
