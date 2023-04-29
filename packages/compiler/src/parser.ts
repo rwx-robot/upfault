c256fe6c074c4903ff3cf1982a65442d3cd11cb5
Ny <johnnynode@gmail.com>
2023-04-29 05:50:51 +0000

    feat(compiler): add parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -430,3 +430,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
       } else if (name.startsWith('v-bind:') || name.startsWith(':')) {
         isDynamic = true;
       }
+      
