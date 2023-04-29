4eaf6d2b306a45a10cb41d2254aeda44c7a751bf
Ny <johnnynode@gmail.com>
2023-04-29 07:47:29 +0000

    fix(compiler): fix parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -438,3 +438,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
         this.expect('=');
         this.skipWhitespace();
         value = this.parseAttributeValue();
+      } else {
