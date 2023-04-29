1e85fc1851e8658fb007ecd93105629366af239d
Ny <johnnynode@gmail.com>
2023-04-29 02:41:24 +0000

    refactor(compiler): simplify parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -413,3 +413,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
       
       const start = this.getPosition();
       const name = this.parseAttributeName();
+      
