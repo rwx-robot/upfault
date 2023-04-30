4f56923a0220f4d7fc1bb0947a99d618ddfc49cc
Ny <johnnynode@gmail.com>
2023-04-30 05:32:42 +0000

    refactor(compiler): simplify parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -470,3 +470,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
     this.skipWhitespace();
     const quote = this.current();
     if (quote === '"' || quote === "'") {
+      this.advance();
