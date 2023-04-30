2312e332ad02b1e0d541191fe2e30a414f204953
Ny <johnnynode@gmail.com>
2023-04-30 06:46:51 +0000

    feat(compiler): implement parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -475,3 +475,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
       while (!this.isEnd() && this.current() !== quote) {
         value += this.current();
         this.advance();
+      }
