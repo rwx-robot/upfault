86c4d6040ee9876a5052ed17f2ce899aba81db30
Ny <johnnynode@gmail.com>
2023-04-30 03:31:27 +0000

    feat(compiler): implement parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -460,3 +460,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
         isEvent,
         isDirective,
         eventModifiers,
+        loc: this.makeLoc(start, this.getPosition()),
