79c4c94db4653266d6e7d42dd0f1f9b1ea0e2ac5
Ny <johnnynode@gmail.com>
2023-04-30 06:27:00 +0000

    refactor(compiler): inline parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -472,3 +472,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
     if (quote === '"' || quote === "'") {
       this.advance();
       let value = '';
+      while (!this.isEnd() && this.current() !== quote) {
