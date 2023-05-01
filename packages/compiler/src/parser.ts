a813021850b106049840b54d4e7c600ca574cdcc
Ny <johnnynode@gmail.com>
2023-05-01 03:10:29 +0000

    perf(compiler): short-circuit parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -505,3 +505,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
 
   private parseInterpolation(): InterpolationNode {
     const start = this.getPosition();
+    this.expect('{{');
