bc531eee91ba1a282f3c19749fbe5d9a26ab6a89
Ny <johnnynode@gmail.com>
2023-05-02 02:20:57 +0000

    feat(compiler): support parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -539,3 +539,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   private parseTagName(): string {
     this.skipWhitespace();
     let name = '';
+    while (!this.isEnd() && this.current().match(/[a-zA-Z0-9-_:]/)) {
