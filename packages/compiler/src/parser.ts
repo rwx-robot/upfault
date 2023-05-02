07aef87bcc930cf6bfd23a067f455380c1e2027c
Ny <johnnynode@gmail.com>
2023-05-02 06:49:53 +0000

    feat(compiler): implement parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -557,3 +557,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   }
 
   private getPosition(): Position {
+    return { offset: this.pos, line: this.line, column: this.column };
