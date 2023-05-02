09a52cf8d1b70c47bee12f97c37236a789f0abb2
Ny <johnnynode@gmail.com>
2023-05-02 05:21:59 +0000

    feat(compiler): add parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -552,3 +552,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
     while (!this.isEnd() && this.current().match(/[a-zA-Z0-9-_:.]/)) {
       name += this.current();
       this.advance();
+    }
