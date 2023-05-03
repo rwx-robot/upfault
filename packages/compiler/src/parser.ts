edb8e10e3468f4f4556e2a1bb1553131834b5339
Ny <johnnynode@gmail.com>
2023-05-03 04:33:25 +0000

    feat(compiler): support parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -586,3 +586,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   private startsWith(str: string): boolean {
     return this.source.slice(this.pos, this.pos + str.length) === str;
   }
+
