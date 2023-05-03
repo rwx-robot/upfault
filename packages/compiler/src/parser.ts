12ebe2a3a6041320016ad4d0365d1ee05105a3ca
Ny <johnnynode@gmail.com>
2023-05-03 06:14:57 +0000

    refactor(compiler): inline parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -595,3 +595,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   }
 
   private skipWhitespace(): void {
+    while (!this.isEnd() && this.current().match(/\s/)) {
