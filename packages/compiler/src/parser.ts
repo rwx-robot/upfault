53e48611bc49064e7326026ebd2488129756211c
Ny <johnnynode@gmail.com>
2023-05-03 08:22:30 +0000

    refactor(compiler): restructure parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -607,3 +607,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
     if (this.current() === '>') this.advance();
   }
   
+  // ============================================================================
