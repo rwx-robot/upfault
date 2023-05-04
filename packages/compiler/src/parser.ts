871acbfa77da537c613a76dc5d7c6d1498ee90f9
Ny <johnnynode@gmail.com>
2023-05-04 05:34:08 +0000

    fix(compiler): correct parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -632,3 +632,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
         this.analyzeComponent(node);
         break;
       case 'Slot':
+        this.analyzeSlot(node);
