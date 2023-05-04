b3f08f774a06e874d2b4599bf155aae37bbceab9
Ny <johnnynode@gmail.com>
2023-05-04 01:03:55 +0000

    feat(compiler): introduce parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -613,3 +613,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   
   analyze(ast: TemplateAST): CompileTimeFlags {
     this.analyzeChildren(ast.children);
+    this.mergeFlags();
