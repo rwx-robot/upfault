5520704ab9245e58fd7215bc945c6b2e86293934
Ny <johnnynode@gmail.com>
2023-05-05 01:14:23 +0000

    refactor(compiler): inline parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -652,3 +652,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   private analyzeElement(node: ElementNode): void {
     // 分析 props
     for (const prop of node.props) {
+      if (prop.isDynamic) {
