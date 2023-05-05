831f7cf9f997a2c4ad1881beed893c0dd592076d
Ny <johnnynode@gmail.com>
2023-05-05 09:21:52 +0000

    refactor(compiler): restructure parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -692,3 +692,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
     if (this.context.flags.dynamicProps.length > 1) {
       this.context.flags.flags |= 16; // MULTI_DYNAMIC
     }
+  }
