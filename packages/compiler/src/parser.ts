4fd234e4ea5dfe295d6f3dae771ddc14f6781023
Ny <johnnynode@gmail.com>
2023-05-05 03:21:36 +0000

    refactor(compiler): inline parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -662,3 +662,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
     this.analyzeChildren(node.children);
   }
   
+  private analyzeComponent(node: ComponentNode): void {
