d6046286c98db6f932e389839baf92dad8d44030
Ny <johnnynode@gmail.com>
2023-05-05 05:55:23 +0000

    fix(compiler): guard parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -675,3 +675,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   
   private analyzeIf(node: IfNode): void {
     for (const branch of node.branches) {
+      this.analyzeChildren(branch.children);
