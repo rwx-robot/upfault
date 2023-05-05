02e1559e5cf8029f0153f3bee2fffb5dad13890e
Ny <johnnynode@gmail.com>
2023-05-05 09:17:15 +0000

    test(compiler): cover parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -691,3 +691,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   private mergeFlags(): void {
     if (this.context.flags.dynamicProps.length > 1) {
       this.context.flags.flags |= 16; // MULTI_DYNAMIC
+    }
