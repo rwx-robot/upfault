9289ee880d99151e5466457ce272e4bde87c9e43
Ny <johnnynode@gmail.com>
2023-05-05 07:10:17 +0000

    refactor(compiler): simplify parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -682,3 +682,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   private analyzeFor(node: ForNode): void {
     this.context.flags.flags |= 16; // VNodeFlags.MULTI_DYNAMIC
     this.analyzeChildren(node.children);
+  }
