89a50b7934beb05f5d76f9ed7f1a41ec9c1e9865
Ny <johnnynode@gmail.com>
2023-05-05 07:07:44 +0000

    test(compiler): add cases for parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -681,3 +681,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
   
   private analyzeFor(node: ForNode): void {
     this.context.flags.flags |= 16; // VNodeFlags.MULTI_DYNAMIC
+    this.analyzeChildren(node.children);
