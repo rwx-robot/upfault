9613aec827613d0921f447a1fdb675d55fdeabf1
Ny <johnnynode@gmail.com>
2023-05-05 04:31:56 +0000

    chore(compiler): clean up parser

--- packages/compiler/src/parser.ts
+++ packages/compiler/src/parser.ts
@@ -671,3 +671,4 @@ const key = keyProp && keyProp.value?.type === 'Expression' ? keyProp.value.valu
     this.context.flags.hasSlot = true;
     this.context.flags.dynamicSlots.push(node.name || 'default');
     this.analyzeChildren(node.fallback);
+  }
