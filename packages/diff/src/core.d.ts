599305a929d549d2395a9e6fb25c91fb4c381ba8
Ny <johnnynode@gmail.com>
2023-05-08 07:54:16 +0000

    fix(diff): fix core.d

--- packages/diff/src/core.d.ts
+++ packages/diff/src/core.d.ts
@@ -23,3 +23,4 @@ interface BlockWithChildren extends Block {
  * @returns DiffResult 包含操作序列和统计信息
  */
 export declare function aeroDiff(oldChildren: VNode[], newChildren: VNode[], options?: Partial<AeroDiffOptions>): DiffResult;
+/**
