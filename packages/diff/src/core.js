6eba67ca4dfea1e3ef9fcf00b7b3c0fdf0302742
Ny <johnnynode@gmail.com>
2023-05-09 08:32:22 +0000

    feat(diff): introduce core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -25,3 +25,4 @@ import { isSameNode } from '@upfault/shared';
  */
 export function aeroDiff(oldChildren, newChildren, options = {}) {
     const config = { ...DEFAULT_AERODIFF_OPTIONS, ...options };
+    const timer = { start: performance.now() };
