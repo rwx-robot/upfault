1aa5a6379879d6c24bac09841ed352ef9c97df1f
Ny <johnnynode@gmail.com>
2023-05-10 08:49:55 +0000

    test(diff): extend tests for core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -68,3 +68,4 @@ export function aeroDiff(oldChildren, newChildren, options = {}) {
     // 从尾部向前跳过相同节点
     while (oldStart <= oldEnd && newStart <= newEnd) {
         const oldNode = oldChildrenExt[oldEnd];
+        const newNode = newChildrenExt[newEnd];
