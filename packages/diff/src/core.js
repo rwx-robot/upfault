7531be4ab07a7fa7758e273f0bf74466701f2ebc
Ny <johnnynode@gmail.com>
2023-05-11 03:31:26 +0000

    feat(diff): support core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -86,3 +86,4 @@ export function aeroDiff(oldChildren, newChildren, options = {}) {
     // 如果旧节点已耗尽，剩余新节点全是 INSERT
     if (oldStart > oldEnd) {
         while (newStart <= newEnd) {
+            ops.push({
