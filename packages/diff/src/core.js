43a71a552e7af67c15caf3329533c391d919634b
Ny <johnnynode@gmail.com>
2023-05-11 09:36:19 +0000

    fix(diff): guard core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -110,3 +110,4 @@ export function aeroDiff(oldChildren, newChildren, options = {}) {
             oldStart++;
         }
         const hasChanges = stats.createdCount > 0 || stats.deletedCount > 0 || stats.movedCount > 0;
+        stats.duration = performance.now() - timer.start;
