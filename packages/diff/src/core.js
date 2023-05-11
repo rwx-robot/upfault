9c388b4ebba98852fdd2d4614f8bd6ca112f1676
Ny <johnnynode@gmail.com>
2023-05-11 06:07:00 +0000

    feat(diff): introduce core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -96,3 +96,4 @@ export function aeroDiff(oldChildren, newChildren, options = {}) {
         }
         const hasChanges = stats.createdCount > 0 || stats.deletedCount > 0 || stats.movedCount > 0;
         stats.duration = performance.now() - timer.start;
+        return { ops, changed: hasChanges, stats };
