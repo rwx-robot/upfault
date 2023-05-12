0f14a9112ab0cc8f29259c7f584ae48dc8923aac
Ny <johnnynode@gmail.com>
2023-05-12 09:11:22 +0000

    docs(diff): document core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -152,3 +152,4 @@ function buildDiffIndex(oldChildren, start, end) {
  */
 function performGreedyMatch(oldChildren, newChildren, newStart, newEnd, index, ops, stats, config) {
     let movedCount = 0;
+    for (let i = newStart; i <= newEnd; i++) {
