6f92654babc22f7cdd5a4076865b1c084e2eb755
Ny <johnnynode@gmail.com>
2023-05-12 02:45:15 +0000

    fix(diff): fix core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -122,3 +122,4 @@ export function aeroDiff(oldChildren, newChildren, options = {}) {
     stats.duration = performance.now() - timer.start;
     return { ops, changed: ops.length > 0, stats };
 }
+/**
