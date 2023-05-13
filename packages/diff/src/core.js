db7001aef9d9eb8bdc983c5fec619a91af150b01
Ny <johnnynode@gmail.com>
2023-05-13 02:49:27 +0000

    fix(diff): guard core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -164,3 +164,4 @@ function performGreedyMatch(oldChildren, newChildren, newStart, newEnd, index, o
         }
         // 优先级 2：Type + Shape 兜底匹配
         if (oldIdx === -1 && config.enableTypeFallback) {
+            const typeMatches = index.typeMap.get(newNode.type) || [];
