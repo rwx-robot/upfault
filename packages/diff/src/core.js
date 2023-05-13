c10619828730a2cee34c5f57642f3ef02c2af970
Ny <johnnynode@gmail.com>
2023-05-13 05:19:05 +0000

    feat(diff): introduce core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -179,3 +179,4 @@ function performGreedyMatch(oldChildren, newChildren, newStart, newEnd, index, o
         if (oldIdx >= 0) {
             const oldNode = oldChildren[oldIdx];
             oldNode._used = true;
+            // 判断是否需要移动
