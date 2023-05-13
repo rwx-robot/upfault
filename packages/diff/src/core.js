cf2e6754392b2790ac4f2ade0b5aa3787b5f8fc4
Ny <johnnynode@gmail.com>
2023-05-13 02:56:59 +0000

    chore(diff): format core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -165,3 +165,4 @@ function performGreedyMatch(oldChildren, newChildren, newStart, newEnd, index, o
         // 优先级 2：Type + Shape 兜底匹配
         if (oldIdx === -1 && config.enableTypeFallback) {
             const typeMatches = index.typeMap.get(newNode.type) || [];
+            for (const candidateIdx of typeMatches) {
