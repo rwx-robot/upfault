23872070a7dcc23e23ada39cd44cb3d390e09a2e
Ny <johnnynode@gmail.com>
2023-05-14 02:46:45 +0000

    fix(diff): guard core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -208,3 +208,4 @@ function performGreedyMatch(oldChildren, newChildren, newStart, newEnd, index, o
  * 简单的 Shape 匹配：比较 children 结构深度
  */
 function shapeMatch(oldNode, newNode) {
+    // 类型不同直接 false
