7c790dd865cb6456b7675b63f4e5080465e56f5b
Ny <johnnynode@gmail.com>
2023-05-14 05:49:35 +0000

    test(diff): assert core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -221,3 +221,4 @@ function shapeMatch(oldNode, newNode) {
  */
 function cleanupUnusedOldNodes(oldChildren, start, end, ops, stats) {
     for (let i = start; i <= end; i++) {
+        const node = oldChildren[i];
