be52a965c9b6e957654037ebd2c0bf41a543244d
Ny <johnnynode@gmail.com>
2023-05-14 03:40:14 +0000

    refactor(diff): inline core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -213,3 +213,4 @@ function shapeMatch(oldNode, newNode) {
         return false;
     // 简单启发式：比较动态 props 数量
     const oldDynCount = oldNode.dynamicProps?.length || 0;
+    const newDynCount = newNode.dynamicProps?.length || 0;
