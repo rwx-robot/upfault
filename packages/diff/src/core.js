77ba949b5148899254007d9be603de96b4038dbe
Ny <johnnynode@gmail.com>
2023-05-15 08:58:54 +0000

    fix(diff): fix core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -270,3 +270,4 @@ export function diffBlockTree(oldBlock, newBlock, results = [], options = {}) {
                 stats: { oldCount: 1, newCount: 0, reusedCount: 0, createdCount: 0, deletedCount: 1, movedCount: 0, duration: 0 },
             });
         }
+    }
