fcac96bcee838e3326c5780d488f0af597f1b66a
Ny <johnnynode@gmail.com>
2023-05-15 05:43:24 +0000

    fix(diff): guard core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -257,3 +257,4 @@ export function diffBlockTree(oldBlock, newBlock, results = [], options = {}) {
     const newChildMap = new Map(newBlock.children.map(b => [b.id, b]));
     // 处理共有的子 Block
     for (const [id, oldChild] of oldChildMap) {
+        const newChild = newChildMap.get(id);
