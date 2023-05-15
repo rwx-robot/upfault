8b8dd16c577e7429cad7af2b97cd096c2e597a06
Ny <johnnynode@gmail.com>
2023-05-15 04:10:01 +0000

    feat(diff): introduce core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -251,3 +251,4 @@ export function blockDiff(oldBlock, newBlock, options = {}) {
 export function diffBlockTree(oldBlock, newBlock, results = [], options = {}) {
     // 当前 Block Diff
     const result = blockDiff(oldBlock, newBlock, options);
+    results.push(result);
