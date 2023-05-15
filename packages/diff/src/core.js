2ba0c702fccfce584f01087777f20c5716f7ef9c
Ny <johnnynode@gmail.com>
2023-05-15 08:27:17 +0000

    test(diff): add cases for core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -268,3 +268,4 @@ export function diffBlockTree(oldBlock, newBlock, results = [], options = {}) {
                 ops: [{ type: DiffOpType.REMOVE, oldNode: oldChild.root }],
                 changed: true,
                 stats: { oldCount: 1, newCount: 0, reusedCount: 0, createdCount: 0, deletedCount: 1, movedCount: 0, duration: 0 },
+            });
