f2b3b177cf6907207dc32edde7524ee9398ab501
Ny <johnnynode@gmail.com>
2023-05-15 02:31:58 +0000

    test(diff): extend tests for core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -244,3 +244,4 @@ function cleanupUnusedOldNodes(oldChildren, start, end, ops, stats) {
 export function blockDiff(oldBlock, newBlock, options = {}) {
     // 只对比动态节点数组
     return aeroDiff(oldBlock.dynamicNodes, newBlock.dynamicNodes, options);
+}
