bd3e723c637a5cd094a19c528bde5d5fcee7610d
Ny <johnnynode@gmail.com>
2023-05-15 07:46:58 +0000

    feat(diff): add core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -266,3 +266,4 @@ export function diffBlockTree(oldBlock, newBlock, results = [], options = {}) {
             // 子 Block 被删除
             results.push({
                 ops: [{ type: DiffOpType.REMOVE, oldNode: oldChild.root }],
+                changed: true,
