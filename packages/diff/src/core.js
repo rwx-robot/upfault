88e0a8e9c3e3d277398e3c4fb8de171338fe7ec0
Ny <johnnynode@gmail.com>
2023-05-16 01:24:20 +0000

    fix(diff): guard core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -277,3 +277,4 @@ export function diffBlockTree(oldBlock, newBlock, results = [], options = {}) {
             ops: [{ type: DiffOpType.CREATE, newNode: newChild.root }],
             changed: true,
             stats: { oldCount: 0, newCount: 1, reusedCount: 0, createdCount: 1, deletedCount: 0, movedCount: 0, duration: 0 },
+        });
