7e6ea89b65e8449d89f01da8bb4980674fdacc85
Ny <johnnynode@gmail.com>
2023-05-17 02:29:13 +0000

    feat(diff): implement core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -324,3 +324,4 @@ export function computePatchFlags(oldVNode, newVNode) {
             const hasKeyed = oldVNode.children.some((c) => c.key != null) ||
                 newVNode.children.some((c) => c.key != null);
             flags |= hasKeyed ? PatchFlags.KEYED_FRAGMENT : PatchFlags.UNKEYED_FRAGMENT;
+        }
