9ba4a62ec97adcec9b4ff36ee64ae801f332ec7a
Ny <johnnynode@gmail.com>
2023-05-17 01:41:13 +0000

    feat(diff): support core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -320,3 +320,4 @@ export function computePatchFlags(oldVNode, newVNode) {
     // Children 对比（简化）
     if (oldVNode.children !== newVNode.children) {
         if (Array.isArray(oldVNode.children) && Array.isArray(newVNode.children)) {
+            // Keyed children
