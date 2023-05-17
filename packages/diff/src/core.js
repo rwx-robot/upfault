3d1fbef2250cfc336d2f37667976550c816193ca
Ny <johnnynode@gmail.com>
2023-05-17 01:46:27 +0000

    fix(diff): handle core

--- packages/diff/src/core.js
+++ packages/diff/src/core.js
@@ -321,3 +321,4 @@ export function computePatchFlags(oldVNode, newVNode) {
     if (oldVNode.children !== newVNode.children) {
         if (Array.isArray(oldVNode.children) && Array.isArray(newVNode.children)) {
             // Keyed children
+            const hasKeyed = oldVNode.children.some((c) => c.key != null) ||
