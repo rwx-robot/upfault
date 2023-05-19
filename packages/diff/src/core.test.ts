54e7c72eb56ef4e0c13672ebafdd092738577f4e
Ny <johnnynode@gmail.com>
2023-05-19 06:02:23 +0000

    feat(diff): introduce core.test

--- packages/diff/src/core.test.ts
+++ packages/diff/src/core.test.ts
@@ -66,3 +66,4 @@ describe('AeroDiff Core Algorithm', () => {
       expect(result.stats.reusedCount).toBe(3);
       expect(result.stats.createdCount).toBe(0);
       expect(result.stats.deletedCount).toBe(0);
+      expect(result.stats.movedCount).toBe(0);
