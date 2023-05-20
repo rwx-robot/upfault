680b3dbaf26904c03cd562dd95b093dd5ca8269e
Ny <johnnynode@gmail.com>
2023-05-20 05:17:32 +0000

    perf(diff): optimize core.test

--- packages/diff/src/core.test.ts
+++ packages/diff/src/core.test.ts
@@ -106,3 +106,4 @@ describe('AeroDiff Core Algorithm', () => {
       
       expect(result.stats.createdCount).toBe(1);
       expect(result.stats.reusedCount).toBe(2);
+      const inserts = result.ops.filter(op => op.type === 'CREATE');
