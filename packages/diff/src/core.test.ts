4a4e4453df1fac8e3c6514c4a77306ce107fdf80
Ny <johnnynode@gmail.com>
2023-05-22 03:00:40 +0000

    refactor(diff): inline core.test

--- packages/diff/src/core.test.ts
+++ packages/diff/src/core.test.ts
@@ -175,3 +175,4 @@ describe('AeroDiff Core Algorithm', () => {
       const newChildren = [
         createVNode({ key: 'c' }),
         createVNode({ key: 'a' }),
+        createVNode({ key: 'b' }),
