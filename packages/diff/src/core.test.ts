e2b49486bdcc0f9be7cfcf975fa7ed2aca5d2b0c
Ny <johnnynode@gmail.com>
2023-05-22 02:36:18 +0000

    fix(diff): handle core.test

--- packages/diff/src/core.test.ts
+++ packages/diff/src/core.test.ts
@@ -170,3 +170,4 @@ describe('AeroDiff Core Algorithm', () => {
       const oldChildren = [
         createVNode({ key: 'a' }),
         createVNode({ key: 'b' }),
+        createVNode({ key: 'c' }),
