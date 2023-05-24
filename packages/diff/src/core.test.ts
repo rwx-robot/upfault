f1bfd6a9149c7d228e6996e6620e417dbf654161
Ny <johnnynode@gmail.com>
2023-05-24 06:12:13 +0000

    test(diff): extend tests for core.test

--- packages/diff/src/core.test.ts
+++ packages/diff/src/core.test.ts
@@ -271,3 +271,4 @@ describe('AeroDiff Core Algorithm', () => {
       
       // 文本节点无 Key，通过位置 + Type 匹配
       // Hello 位置 0 匹配，World 通过 Type 匹配到 UpFault
+      expect(result.stats.reusedCount).toBe(2);
