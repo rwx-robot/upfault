a22160007b6a90f67d3833c0bd8aa889641865c6
Ny <johnnynode@gmail.com>
2023-05-24 02:12:13 +0000

    test(diff): add cases for core.test

--- packages/diff/src/core.test.ts
+++ packages/diff/src/core.test.ts
@@ -253,3 +253,4 @@ describe('AeroDiff Core Algorithm', () => {
       
       // 算法会将 'b' 通过 Type 兜底匹配到 'd'（都是 ELEMENT），所以复用 3 个
       expect(result.stats.reusedCount).toBe(3); // a, b->d, c
+      expect(result.stats.createdCount).toBe(0); // d 复用了 b
