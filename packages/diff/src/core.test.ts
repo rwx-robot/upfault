4184ab2c3c648a46cddd684fbd93857be4bbd440
Ny <johnnynode@gmail.com>
2023-05-25 09:42:38 +0000

    chore(diff): bump core.test

--- packages/diff/src/core.test.ts
+++ packages/diff/src/core.test.ts
@@ -329,3 +329,4 @@ describe('Block Diff', () => {
 describe('PatchFlags 计算', () => {
   it('类型不同应返回 FULL_DIFF', () => {
     const oldVNode = createVNode({ type: VNodeType.ELEMENT });
+    const newVNode = createVNode({ type: VNodeType.COMPONENT });
