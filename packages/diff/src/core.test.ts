8d47a48e757a1ad1127592345294e6c981c3f66b
Ny <johnnynode@gmail.com>
2023-05-26 05:33:42 +0000

    test(diff): assert core.test

--- packages/diff/src/core.test.ts
+++ packages/diff/src/core.test.ts
@@ -352,3 +352,4 @@ describe('PatchFlags 计算', () => {
   it('Class 变化应标记 CLASS', () => {
     const oldVNode = createVNode({ props: { class: 'old' } });
     const newVNode = createVNode({ props: { class: 'new' } });
+    
