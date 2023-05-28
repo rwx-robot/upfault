d1070a930682f6441a8624566f3049dab97afde3
Ny <johnnynode@gmail.com>
2023-05-28 05:45:07 +0000

    feat(diff): add core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -51,3 +51,4 @@ type VNodeLike = { type: unknown; key: string | number | null };
  * @param oldChildren 旧子节点数组
  * @param newChildren 新子节点数组
  * @param options 算法配置
+ * @returns DiffResult 包含操作序列和统计信息
