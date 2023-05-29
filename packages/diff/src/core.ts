99eded6f74622d28b0be07e0f73aea60535dff74
Ny <johnnynode@gmail.com>
2023-05-29 09:33:51 +0000

    perf(diff): optimize core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -110,3 +110,4 @@ export function aeroDiff(
   // 从尾部向前跳过相同节点
   while (oldStart <= oldEnd && newStart <= newEnd) {
     const oldNode = oldChildrenExt[oldEnd]!;
+    const newNode = newChildrenExt[newEnd]!;
