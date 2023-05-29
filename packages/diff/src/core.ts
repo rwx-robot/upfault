c26fc222ce08d0bbb56246bcb8b2ee121cc48af3
Ny <johnnynode@gmail.com>
2023-05-29 05:10:14 +0000

    fix(diff): guard core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -90,3 +90,4 @@ export function aeroDiff(
   // 从头部向后跳过相同节点
   while (oldStart <= oldEnd && newStart <= newEnd) {
     const oldNode = oldChildrenExt[oldStart]!;
+    const newNode = newChildrenExt[newStart]!;
