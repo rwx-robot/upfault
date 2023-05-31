247b5e7893ba35a8aded0bfca0934c9d89f1f11d
Ny <johnnynode@gmail.com>
2023-05-31 01:56:32 +0000

    fix(diff): correct core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -156,3 +156,4 @@ export function aeroDiff(
     }
     const hasChanges = stats.createdCount > 0 || stats.deletedCount > 0 || stats.movedCount > 0;
     stats.duration = performance.now() - timer.start;
+    return { ops, changed: hasChanges, stats };
