f9f25154a73b03b1c2571a1be7c109ba1bcb3bcb
Ny <johnnynode@gmail.com>
2023-06-02 05:07:44 +0000

    feat(diff): add core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -255,3 +255,4 @@ function performGreedyMatch(
         if (!oldChildren[candidateIdx]!._used) {
           // 可选：Shape 匹配检查
           if (config.enableShapeMatching && !shapeMatch(oldChildren[candidateIdx]!, newNode)) {
+            continue;
