c516c0e6a39d4672da94187277aa5b1ffed614c9
Ny <johnnynode@gmail.com>
2023-06-05 08:18:55 +0000

    test(diff): assert core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -391,3 +391,4 @@ export function diffBlockTree(
     results.push({
       ops: [{ type: DiffOpType.CREATE, newNode: newChild.root }],
       changed: true,
+      stats: { oldCount: 0, newCount: 1, reusedCount: 0, createdCount: 1, deletedCount: 0, movedCount: 0, duration: 0 },
