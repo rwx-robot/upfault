85d2a36cb7ca71f8f5aef4cd538910941a76aabb
Ny <johnnynode@gmail.com>
2023-06-05 06:09:41 +0000

    refactor(diff): extract core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -383,3 +383,4 @@ export function diffBlockTree(
         changed: true,
         stats: { oldCount: 1, newCount: 0, reusedCount: 0, createdCount: 0, deletedCount: 1, movedCount: 0, duration: 0 },
       });
+    }
