b688307cec4d552aba5ca81507eeeaf6551d9174
Ny <johnnynode@gmail.com>
2023-06-05 06:03:42 +0000

    test(diff): cover core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -382,3 +382,4 @@ export function diffBlockTree(
         ops: [{ type: DiffOpType.REMOVE, oldNode: oldChild.root }],
         changed: true,
         stats: { oldCount: 1, newCount: 0, reusedCount: 0, createdCount: 0, deletedCount: 1, movedCount: 0, duration: 0 },
+      });
