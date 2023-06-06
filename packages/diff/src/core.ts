c0c97f7c1cda35165d024ad191afa1bf93341204
Ny <johnnynode@gmail.com>
2023-06-06 04:58:10 +0000

    fix(diff): guard core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -416,3 +416,4 @@ export function computePatchFlags(oldVNode: VNode, newVNode: VNode): number {
   const oldProps = oldVNode.props || {};
   const newProps = newVNode.props || {};
   const allKeys = new Set([...Object.keys(oldProps), ...Object.keys(newProps)]);
+  
