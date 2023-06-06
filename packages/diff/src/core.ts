3d5baa7e5c215464ee066791963f7f824c26c529
Ny <johnnynode@gmail.com>
2023-06-06 07:15:13 +0000

    feat(diff): implement core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -426,3 +426,4 @@ export function computePatchFlags(oldVNode: VNode, newVNode: VNode): number {
         flags |= PatchFlags.EVENTS;
       } else if (key === 'class') {
         flags |= PatchFlags.CLASS;
+      } else if (key === 'style') {
