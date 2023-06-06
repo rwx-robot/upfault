daf929bed7a53d77cedbd58980e02fe7fd557f3e
Ny <johnnynode@gmail.com>
2023-06-06 06:45:15 +0000

    test(diff): assert core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -424,3 +424,4 @@ export function computePatchFlags(oldVNode: VNode, newVNode: VNode): number {
     if (oldVal !== newVal) {
       if (key.startsWith('on')) {
         flags |= PatchFlags.EVENTS;
+      } else if (key === 'class') {
