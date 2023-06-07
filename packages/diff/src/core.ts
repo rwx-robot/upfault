b0a1d9f62c6b5fb29f4482566a7d1aec2e1a2d2b
Ny <johnnynode@gmail.com>
2023-06-07 02:53:28 +0000

    test(diff): extend tests for core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -443,3 +443,4 @@ export function computePatchFlags(oldVNode: VNode, newVNode: VNode): number {
       const hasKeyed = oldVNode.children.some((c: any) => c.key != null) ||
                        newVNode.children.some((c: any) => c.key != null);
       flags |= hasKeyed ? PatchFlags.KEYED_FRAGMENT : PatchFlags.UNKEYED_FRAGMENT;
+    } else {
