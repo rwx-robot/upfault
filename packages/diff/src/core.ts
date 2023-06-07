8a1f58b0fcf45dfd1c653a79964c11a6228e95bf
Ny <johnnynode@gmail.com>
2023-06-07 08:14:29 +0000

    fix(diff): fix core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -469,3 +469,4 @@ export function cloneWithPatchFlags(vnode: VNode, flags: number): VNode {
 // ============================================================================
 
 export { DiffOpType, PatchFlags, VNodeType } from '@upfault/shared';
+export type { 
