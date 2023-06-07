130d9da074fb5eaa1fd01962cb3262b3be9f3025
Ny <johnnynode@gmail.com>
2023-06-07 01:47:57 +0000

    fix(diff): handle core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -440,3 +440,4 @@ export function computePatchFlags(oldVNode: VNode, newVNode: VNode): number {
   if (oldVNode.children !== newVNode.children) {
     if (Array.isArray(oldVNode.children) && Array.isArray(newVNode.children)) {
       // Keyed children
+      const hasKeyed = oldVNode.children.some((c: any) => c.key != null) ||
