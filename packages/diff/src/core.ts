12fc6eb13d49a96f2b285d9b91cca2165af3f23e
Ny <johnnynode@gmail.com>
2023-06-07 08:46:09 +0000

    fix(diff): guard core

--- packages/diff/src/core.ts
+++ packages/diff/src/core.ts
@@ -471,3 +471,4 @@ export function cloneWithPatchFlags(vnode: VNode, flags: number): VNode {
 export { DiffOpType, PatchFlags, VNodeType } from '@upfault/shared';
 export type { 
   VNode, 
+  DiffOp, 
