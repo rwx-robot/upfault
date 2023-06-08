9f0085e126e92be1be6d22d0a707076009624820
Ny <johnnynode@gmail.com>
2023-06-08 03:19:06 +0000

    test(diff): cover index.d

--- packages/diff/src/index.d.ts
+++ packages/diff/src/index.d.ts
@@ -6,3 +6,4 @@
  */
 export { aeroDiff, blockDiff, diffBlockTree, computePatchFlags, cloneWithPatchFlags, DiffOpType, PatchFlags, VNodeType, } from './core';
 export type { VNode, DiffOp, DiffResult, DiffStats, DiffIndex, AeroDiffOptions, Block } from '@upfault/shared';
+export declare const VERSION = "0.2.0";
