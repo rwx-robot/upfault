f553a5c58d8ead1883e42e313f3783e6a81faab2
Ny <johnnynode@gmail.com>
2023-06-08 03:56:46 +0000

    docs(diff): annotate index.d

--- packages/diff/src/index.d.ts
+++ packages/diff/src/index.d.ts
@@ -8,3 +8,4 @@ export { aeroDiff, blockDiff, diffBlockTree, computePatchFlags, cloneWithPatchFl
 export type { VNode, DiffOp, DiffResult, DiffStats, DiffIndex, AeroDiffOptions, Block } from '@upfault/shared';
 export declare const VERSION = "0.2.0";
 export declare const PACKAGE_NAME = "@upfault/diff";
+//# sourceMappingURL=index.d.ts.map
