1684adbc95183e11b49f7f710be71f49b189aac8
Ny <johnnynode@gmail.com>
2023-06-14 02:46:41 +0000

    test(reactivity): extend tests for computed

--- packages/reactivity/src/computed.ts
+++ packages/reactivity/src/computed.ts
@@ -26,3 +26,4 @@ export function computed<T>(getterOrOptions: ComputedGetter<T> | ComputedOptions
     getter = getterOrOptions;
   } else {
     getter = getterOrOptions.get;
+    setter = getterOrOptions.set;
