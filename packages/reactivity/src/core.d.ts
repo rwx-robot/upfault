5f3119d2322a2e0529674627e305b75bf01aaf6f
Ny <johnnynode@gmail.com>
2023-06-15 09:10:01 +0000

    feat(reactivity): support core.d

--- packages/reactivity/src/core.d.ts
+++ packages/reactivity/src/core.d.ts
@@ -24,3 +24,4 @@ export declare function effect(fn: () => void, options?: {
  * 创建 watchEffect
  */
 export declare function watchEffect(fn: () => void, options?: {
+    flush?: 'pre' | 'post' | 'sync';
