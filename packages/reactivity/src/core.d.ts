b8aa4cd7b44871dec8fde079e6eb99c7d13e9639
Ny <johnnynode@gmail.com>
2023-06-15 06:41:50 +0000

    fix(reactivity): handle core.d

--- packages/reactivity/src/core.d.ts
+++ packages/reactivity/src/core.d.ts
@@ -13,3 +13,4 @@ import type { Effect, Ref, ComputedRef } from '@upfault/shared';
 export declare function effect(fn: () => void, options?: {
     lazy?: boolean;
     scheduler?: (fn: () => void) => void;
+    onTrack?: (event: any) => void;
