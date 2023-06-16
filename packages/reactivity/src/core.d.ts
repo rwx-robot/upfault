648964919c79288b250b116db4d0f655ced7c6a5
Ny <johnnynode@gmail.com>
2023-06-16 04:04:39 +0000

    refactor(reactivity): inline core.d

--- packages/reactivity/src/core.d.ts
+++ packages/reactivity/src/core.d.ts
@@ -41,3 +41,4 @@ export declare function ref<T>(value: T): Ref<T>;
  */
 export declare function computed<T>(getter: () => T): ComputedRef<T>;
 export { track, trigger, createDep, stopEffect, pauseEffect, resumeEffect, getCurrentEffect, pushEffect, popEffect, runEffect, runEffectSync, createEffect, isRef, isFunction, isArray, hasChanged, unref, toRef, toRefs, shallowRef, readonlyRef, } from './dep';
+export type { Effect, Ref, ComputedRef, WatchOptions, WatchCallback, WatchSource, WatchStopHandle, DebuggerEvent, TrackOpTypes, TriggerOpTypes, } from '@upfault/shared';
