39ae52bf049584c7e4ca62e12943e510f3e18de1
Ny <johnnynode@gmail.com>
2023-06-16 09:29:01 +0000

    fix(reactivity): guard core

--- packages/reactivity/src/core.js
+++ packages/reactivity/src/core.js
@@ -18,3 +18,4 @@ export function effect(fn, options) {
     const effect = createEffect(fn, options);
     // For lazy effects, return a runner that triggers when called
     // For non-lazy effects, return a stop function
+    const isLazy = options?.lazy === true;
