9bee49d7ead4552be449e6340bd22cc7d23d4e46
Ny <johnnynode@gmail.com>
2023-06-21 03:23:31 +0000

    fix(reactivity): correct core

--- packages/reactivity/src/core.ts
+++ packages/reactivity/src/core.ts
@@ -89,3 +89,4 @@ export function effect(fn: () => void, options?: {
  */
 export function watchEffect(fn: () => void, options?: {
   flush?: 'pre' | 'post' | 'sync';
+  onTrack?: (event: any) => void;
