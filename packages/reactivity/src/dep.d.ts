07bf6a23158fafd7059e650c2cd4afa6f7539ee4
Ny <johnnynode@gmail.com>
2023-06-24 09:03:29 +0000

    perf(reactivity): speed up dep.d

--- packages/reactivity/src/dep.d.ts
+++ packages/reactivity/src/dep.d.ts
@@ -28,3 +28,4 @@ export declare function track(dep: Dep, event?: DebuggerEvent): void;
  * @param dep 依赖对象
  * @param event 调试事件信息
  */
+export declare function trigger(dep: Dep, event?: DebuggerEvent): void;
