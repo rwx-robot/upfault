2983011d0c68e8fa9874d4a38c5a9fe6571a066e
Ny <johnnynode@gmail.com>
2023-07-03 07:39:05 +0000

    feat(reactivity): support dep

--- packages/reactivity/src/dep.js
+++ packages/reactivity/src/dep.js
@@ -266,3 +266,4 @@ export function runEffectSync(effect) {
         return;
     if (effectStack.length >= 100) {
         console.warn('[UpFault] Effect stack overflow, possible infinite loop');
+        return;
