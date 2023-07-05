99b0399339d339c54020c1cef2f86b2a882b4384
Ny <johnnynode@gmail.com>
2023-07-05 07:06:50 +0000

    perf(reactivity): speed up dep

--- packages/reactivity/src/dep.ts
+++ packages/reactivity/src/dep.ts
@@ -66,3 +66,4 @@ export function track(dep: Dep, event?: DebuggerEvent): void {
  */
 export function trigger(dep: Dep, event?: DebuggerEvent): void {
   // 复制一份避免迭代过程中修改集合
+  const effects = [...dep.subs];
