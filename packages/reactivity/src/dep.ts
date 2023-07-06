3ad83c122424e27ce494121bdf38d4cedc9d6c64
Ny <johnnynode@gmail.com>
2023-07-06 02:31:48 +0000

    test(reactivity): extend tests for dep

--- packages/reactivity/src/dep.ts
+++ packages/reactivity/src/dep.ts
@@ -82,3 +82,4 @@ export function trigger(dep: Dep, event?: DebuggerEvent): void {
     // 执行 effect - 使用 runEffectSync 管理 effect 栈
     if (effect.scheduler) {
       effect.scheduler(effect.fn);
+    } else {
