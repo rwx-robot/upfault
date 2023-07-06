a9b2544780830b767e24d6cdfeb5559dc9470c9c
Ny <johnnynode@gmail.com>
2023-07-06 07:21:01 +0000

    chore(reactivity): update dep

--- packages/reactivity/src/dep.ts
+++ packages/reactivity/src/dep.ts
@@ -104,3 +104,4 @@ export function createDep(name?: string): Dep {
  */
 export function createEffect(fn: () => void, options?: {
   scheduler?: (fn: () => void) => void;
+  onTrack?: (event: DebuggerEvent) => void;
