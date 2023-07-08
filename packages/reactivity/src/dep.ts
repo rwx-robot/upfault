ef218b11f10df589f2c357aa6387dc38ac2ad997
Ny <johnnynode@gmail.com>
2023-07-08 05:08:46 +0000

    fix(reactivity): correct dep

--- packages/reactivity/src/dep.ts
+++ packages/reactivity/src/dep.ts
@@ -179,3 +179,4 @@ export function isRef<T>(val: any): val is Ref<T> {
  */
 export function unref<T>(ref: T | Ref<T>): T extends Ref<infer U> ? U : T {
   return (isRef(ref) ? ref.value : ref) as any;
+}
