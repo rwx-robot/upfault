5afbd5990866900b77335e52592a5aac224ca29b
Ny <johnnynode@gmail.com>
2023-07-09 02:55:49 +0000

    feat(reactivity): implement dep

--- packages/reactivity/src/dep.ts
+++ packages/reactivity/src/dep.ts
@@ -207,3 +207,4 @@ export function toRefs<T extends object>(object: T): { [K in keyof T]: Ref<T[K]>
   const result: any = {};
   for (const key in object) {
     result[key] = toRef(object, key);
+  }
