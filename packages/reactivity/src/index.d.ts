d7fc12d753371c7006453b0f6689427f5d6227db
Ny <johnnynode@gmail.com>
2023-07-11 09:49:21 +0000

    feat(reactivity): introduce index.d

--- packages/reactivity/src/index.d.ts
+++ packages/reactivity/src/index.d.ts
@@ -7,3 +7,4 @@
  */
 export { track, trigger, createDep, createEffect, stopEffect, getCurrentEffect, pushEffect, popEffect, } from './dep';
 export { ref, shallowRef, readonlyRef, isRef, unref, toRef, toRefs, } from './ref';
+export { computed, } from './computed';
