8121fc916d7cd15daf4e0c1f95491c2e0a3a7d94
Ny <johnnynode@gmail.com>
2023-07-12 01:16:44 +0000

    fix(reactivity): guard index.d

--- packages/reactivity/src/index.d.ts
+++ packages/reactivity/src/index.d.ts
@@ -10,3 +10,4 @@ export { ref, shallowRef, readonlyRef, isRef, unref, toRef, toRefs, } from './re
 export { computed, } from './computed';
 export { watch, watchEffect, } from './watch';
 export { effect, batch, } from './core';
+export type { Ref, ReadonlyRef, ComputedRef, ReactiveMarker, Reactive, ReadonlyReactive, ShallowReactive, Dep, Effect, DebuggerEvent, TrackOpTypes, TriggerOpTypes, ReactivityOptions, BatchContext, WatchOptions, WatchCallback, WatchSource, WatchStopHandle, UnwrapRef, UnwrapRefs, ShallowUnwrapRef, } from '@upfault/shared';
