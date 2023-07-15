b4df85e73cebc0ee7192e1d159c00b69e6fb9703
Ny <johnnynode@gmail.com>
2023-07-15 08:51:19 +0000

    fix(reactivity): correct reactivity.test

--- packages/reactivity/src/reactivity.test.ts
+++ packages/reactivity/src/reactivity.test.ts
@@ -52,3 +52,4 @@ describe('UpFault Reactivity', () => {
   describe('computed', () => {
     it('should compute derived value', () => {
       const r = ref(1);
+      const c = computed(() => r.value * 2);
