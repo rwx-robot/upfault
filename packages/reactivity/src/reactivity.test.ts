0a3f67c0e605b2787c2c95c778134897cd5aa777
Ny <johnnynode@gmail.com>
2023-07-18 05:36:05 +0000

    refactor(reactivity): simplify reactivity.test

--- packages/reactivity/src/reactivity.test.ts
+++ packages/reactivity/src/reactivity.test.ts
@@ -160,3 +160,5 @@ describe('UpFault Reactivity', () => {
       r.value.count = 2;
       expect(cb).toHaveBeenCalledWith({ count: 2 }, { count: 1 });
     });
+
+    it('should support cleanup', () => {
