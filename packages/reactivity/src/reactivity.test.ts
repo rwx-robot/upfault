91ede782d65c1f98c65840ee704e24bccc77511a
Ny <johnnynode@gmail.com>
2023-07-18 04:21:03 +0000

    test(reactivity): assert reactivity.test

--- packages/reactivity/src/reactivity.test.ts
+++ packages/reactivity/src/reactivity.test.ts
@@ -155,3 +155,4 @@ describe('UpFault Reactivity', () => {
     it('should support deep option', () => {
       const r = ref({ count: 1 });
       const cb = vi.fn();
+      watch(r, cb, { deep: true });
