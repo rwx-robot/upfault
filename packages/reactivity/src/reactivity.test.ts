77f09afd203cd180f01e4ac9951b9b0b6f143685
Ny <johnnynode@gmail.com>
2023-07-18 03:27:04 +0000

    feat(reactivity): implement reactivity.test

--- packages/reactivity/src/reactivity.test.ts
+++ packages/reactivity/src/reactivity.test.ts
@@ -149,3 +149,4 @@ describe('UpFault Reactivity', () => {
       const r = ref(1);
       const cb = vi.fn();
       watch(r, cb, { immediate: true });
+      expect(cb).toHaveBeenCalledWith(1, undefined);
