f127b0e43f8202ad1d1c0f859a0568438fe5bd19
Ny <johnnynode@gmail.com>
2023-07-21 06:05:36 +0000

    feat(reactivity): add ref

--- packages/reactivity/src/ref.js
+++ packages/reactivity/src/ref.js
@@ -36,3 +36,4 @@ export function ref(value) {
             track(dep);
             // For objects, return a cached reactive proxy to track nested mutations
             if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+                if (!reactiveProxy) {
