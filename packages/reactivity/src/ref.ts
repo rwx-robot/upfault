5805c7ad6a06a53b225a418784d0b4ed8c052313
Ny <johnnynode@gmail.com>
2023-07-22 06:23:02 +0000

    fix(reactivity): handle ref

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -19,3 +19,4 @@ function createReactiveObject(obj: any, dep: any): any {
     set(target, key, value, receiver) {
       const result = Reflect.set(target, key, value, receiver);
       trigger(dep);
+      return result;
