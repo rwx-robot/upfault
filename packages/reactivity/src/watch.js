1b628bb583896a1801430384f7251a08526f48f6
Ny <johnnynode@gmail.com>
2023-07-27 07:54:39 +0000

    test(reactivity): cover watch

--- packages/reactivity/src/watch.js
+++ packages/reactivity/src/watch.js
@@ -144,3 +144,4 @@ export function watch(source, callback, options) {
 export function watchEffect(fn, options) {
     const effect = createEffect(fn, {
         lazy: false,
+        scheduler: (fn) => {
