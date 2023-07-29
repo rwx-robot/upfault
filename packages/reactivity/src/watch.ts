c39e6df2fe28e9bb736b20259975635af6c3bb06
Ny <johnnynode@gmail.com>
2023-07-29 03:51:31 +0000

    feat(reactivity): add watch

--- packages/reactivity/src/watch.ts
+++ packages/reactivity/src/watch.ts
@@ -51,3 +51,4 @@ function hasChangedDeep(a: any, b: any): boolean {
   
   for (const key of keysA) {
     if (!keysB.includes(key)) return true;
+    if (hasChangedDeep(aUnwrapped[key], bUnwrapped[key])) return true;
