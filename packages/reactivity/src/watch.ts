cad98853ecc33d9ae87a4f314a4d7b7e33e4334c
Ny <johnnynode@gmail.com>
2023-07-30 07:08:52 +0000

    test(reactivity): cover watch

--- packages/reactivity/src/watch.ts
+++ packages/reactivity/src/watch.ts
@@ -111,3 +111,4 @@ export function watch<T>(
     if (changed) {
       // Pass onCleanup only if callback expects it (length >= 3)
       let cleanupFn: (() => void) | undefined;
+      const onCleanup = (fn: () => void) => {
