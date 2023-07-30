3675c571bc4113e900193b868dda1bebd70ccd5d
Ny <johnnynode@gmail.com>
2023-07-30 04:17:00 +0000

    perf(reactivity): optimize watch

--- packages/reactivity/src/watch.ts
+++ packages/reactivity/src/watch.ts
@@ -96,3 +96,4 @@ export function watch<T>(
   // For deep watch, store a snapshot (deep clone) to avoid mutation issues
   const isDeep = options?.deep === true;
   let oldValueSnapshot = isDeep ? deepClone(getter()) : getter();
+  let oldValue = getter();
