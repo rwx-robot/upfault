2ed01a83ab8edb4d3d38891c08529ad27693459c
Ny <johnnynode@gmail.com>
2023-08-02 08:43:55 +0000

    test(runtime): extend tests for runtime

--- packages/runtime/index.mjs
+++ packages/runtime/index.mjs
@@ -2,3 +2,4 @@
 import { VNodeType, PatchFlags as SharedPatchFlags } from "@upfault/shared/diff";
 import { VNodeType as VNodeType2 } from "@upfault/shared/diff";
 function normalizeClass(value) {
+  if (value == null)
