4f35a449289345e514ebf5e968a16b2a9ee80632
Ny <johnnynode@gmail.com>
2023-08-02 08:35:17 +0000

    feat(runtime): introduce runtime

--- packages/runtime/index.mjs
+++ packages/runtime/index.mjs
@@ -1,3 +1,4 @@
 // upfault/packages/runtime/src/h.ts
 import { VNodeType, PatchFlags as SharedPatchFlags } from "@upfault/shared/diff";
 import { VNodeType as VNodeType2 } from "@upfault/shared/diff";
+function normalizeClass(value) {
