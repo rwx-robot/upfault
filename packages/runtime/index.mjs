d3e89b7400e5e16ff5afac9306a77525eda2e2b9
Ny <johnnynode@gmail.com>
2023-08-04 01:03:29 +0000

    feat(runtime): implement runtime

--- packages/runtime/index.mjs
+++ packages/runtime/index.mjs
@@ -49,3 +49,4 @@ function normalizeChildren(children) {
       return [];
     if (typeof child === "string" || typeof child === "number") {
       return [{ type: Text, children: String(child), props: null, key: null, ref: null, shapeFlag: VNodeShapeFlags.TEXT_NODE, patchFlag: SharedPatchFlags.NONE, dynamicProps: [], el: null, anchor: null, parent: null, componentInstance: null, component: void 0, vnodeType: VNodeType.TEXT }];
+    }
