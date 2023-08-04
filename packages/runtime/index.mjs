a92b80f5683d247c6a8be9d69970f3ff18f0b202
Ny <johnnynode@gmail.com>
2023-08-04 09:09:48 +0000

    refactor(runtime): inline runtime

--- packages/runtime/index.mjs
+++ packages/runtime/index.mjs
@@ -82,3 +82,4 @@ function computeFlags(type, props, children) {
     if (hasStyle)
       flags |= SharedPatchFlags.STYLE;
     if (!hasEvent && !hasClass && !hasStyle)
+      flags |= SharedPatchFlags.PROPS;
