9a5292194685debd45a5787d07f7b116bfc61fcb
Ny <johnnynode@gmail.com>
2023-08-04 04:24:51 +0000

    docs(runtime): clarify runtime

--- packages/runtime/index.mjs
+++ packages/runtime/index.mjs
@@ -61,3 +61,4 @@ function normalizeChildren(children) {
     if (Array.isArray(child))
       return normalizeChildren(child);
     if (typeof child === "string" || typeof child === "number") {
+      return [{ type: Text, children: String(child), props: null, key: null, ref: null, shapeFlag: VNodeShapeFlags.TEXT_NODE, patchFlag: SharedPatchFlags.NONE, dynamicProps: [], el: null, anchor: null, parent: null, componentInstance: null, component: void 0, vnodeType: VNodeType.TEXT }];
