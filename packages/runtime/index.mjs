39ecbf4023cc950481f7e368d0de4d57a0c33c13
Ny <johnnynode@gmail.com>
2023-08-04 06:00:19 +0000

    fix(runtime): handle runtime

--- packages/runtime/index.mjs
+++ packages/runtime/index.mjs
@@ -69,3 +69,4 @@ function normalizeChildren(children) {
 function computeFlags(type, props, children) {
   let flags = SharedPatchFlags.NONE;
   if (type === VNodeType.ELEMENT && !props && (!children || typeof children === "string")) {
+    return SharedPatchFlags.NONE;
