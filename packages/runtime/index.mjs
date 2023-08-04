51f7ffc491fcf42209cb32c9bc617aa165fee3b0
Ny <johnnynode@gmail.com>
2023-08-04 04:32:11 +0000

    test(runtime): extend tests for runtime

--- packages/runtime/index.mjs
+++ packages/runtime/index.mjs
@@ -63,3 +63,4 @@ function normalizeChildren(children) {
     if (typeof child === "string" || typeof child === "number") {
       return [{ type: Text, children: String(child), props: null, key: null, ref: null, shapeFlag: VNodeShapeFlags.TEXT_NODE, patchFlag: SharedPatchFlags.NONE, dynamicProps: [], el: null, anchor: null, parent: null, componentInstance: null, component: void 0, vnodeType: VNodeType.TEXT }];
     }
+    return [child];
