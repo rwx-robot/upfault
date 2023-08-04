ea019185b1e664b774d7ab3c54eb79659699b304
Ny <johnnynode@gmail.com>
2023-08-04 01:26:24 +0000

    refactor(runtime): extract runtime

--- packages/runtime/index.mjs
+++ packages/runtime/index.mjs
@@ -51,3 +51,4 @@ function normalizeChildren(children) {
       return [{ type: Text, children: String(child), props: null, key: null, ref: null, shapeFlag: VNodeShapeFlags.TEXT_NODE, patchFlag: SharedPatchFlags.NONE, dynamicProps: [], el: null, anchor: null, parent: null, componentInstance: null, component: void 0, vnodeType: VNodeType.TEXT }];
     }
     if (Array.isArray(child)) {
+      return normalizeChildren(child);
