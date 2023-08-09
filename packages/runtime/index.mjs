bf5e9c785abca0893fd41c4f6b1447b5d522a1fc
Ny <johnnynode@gmail.com>
2023-08-09 01:29:11 +0000

    test(runtime): extend tests for runtime

--- packages/runtime/index.mjs
+++ packages/runtime/index.mjs
@@ -251,3 +251,4 @@ function injectHook(hookName, hook, instance = currentInstance) {
     warn(`[UpFault] ${hookName} \u53EA\u80FD\u5728 setup() \u6216\u7EC4\u4EF6\u521D\u59CB\u5316\u65F6\u8C03\u7528`);
     return false;
   }
+  if (!instance[hookName]) {
