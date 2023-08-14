1f21469b48c4e3a9615e4072d5ae5101ef902bf7
Ny <johnnynode@gmail.com>
2023-08-14 02:26:09 +0000

    refactor(runtime): simplify renderer

--- packages/runtime/renderer.mjs
+++ packages/runtime/renderer.mjs
@@ -37,3 +37,4 @@ export function createRenderer(options) {
     if (props) {
       for (const key in props) {
         if (key !== 'children' && key !== 'key' && key !== 'ref') {
+          patchProp(el, key, null, props[key]);
