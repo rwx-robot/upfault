fbe72d2c44b1cafaca67d9435eb91eaf5f95b06e
Ny <johnnynode@gmail.com>
2023-08-15 02:22:05 +0000

    fix(runtime): fix renderer

--- packages/runtime/renderer.mjs
+++ packages/runtime/renderer.mjs
@@ -80,3 +80,4 @@ export function createRenderer(options) {
 
   function mountComponent(vnode, parent, anchor = null) {
     console.warn('Component mounting not fully implemented in minimal renderer');
+    if (typeof vnode.type === 'function') {
