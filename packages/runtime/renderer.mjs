55e82e9f799dd013d794dde8ae23c4f55c0f8d3e
Ny <johnnynode@gmail.com>
2023-08-15 09:38:34 +0000

    chore(runtime): update renderer

--- packages/runtime/renderer.mjs
+++ packages/runtime/renderer.mjs
@@ -115,3 +115,4 @@ export function createRenderer(options) {
     if (vnodeType === 2) { // ELEMENT
       patchElement(n1, n2);
     } else if (vnodeType === 1) { // TEXT
+      if (n1.children !== n2.children) {
