4bd0b8d34178b17652ab1f239e6e9d64b3a054da
Ny <johnnynode@gmail.com>
2023-08-16 02:24:11 +0000

    perf(runtime): short-circuit renderer

--- packages/runtime/renderer.mjs
+++ packages/runtime/renderer.mjs
@@ -121,3 +121,4 @@ export function createRenderer(options) {
     } else if (vnodeType === 5) { // FRAGMENT
       patchChildren(n1, n2, n1.el);
     } else if (vnodeType === 3) { // COMPONENT
+      // Component update not implemented in minimal version
