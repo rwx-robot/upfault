01fb03e2b8b3823333abc3598ecfbcb6adfc5114
Ny <johnnynode@gmail.com>
2023-08-19 05:04:03 +0000

    perf(runtime): short-circuit renderer

--- packages/runtime/renderer.mjs
+++ packages/runtime/renderer.mjs
@@ -268,3 +268,4 @@ export const defaultRendererOptions = {
   },
   createText: (text) => document.createTextNode(text),
   createComment: (text) => document.createComment(text),
+  setElementText: (el, text) => {
