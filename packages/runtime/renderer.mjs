09843723e2a085ee7cd9eecfae85db5f753a1a07
Ny <johnnynode@gmail.com>
2023-08-19 04:33:53 +0000

    test(runtime): cover renderer

--- packages/runtime/renderer.mjs
+++ packages/runtime/renderer.mjs
@@ -266,3 +266,4 @@ export const defaultRendererOptions = {
       ? document.createElementNS('http://www.w3.org/2000/svg', tag)
       : document.createElement(tag);
   },
+  createText: (text) => document.createTextNode(text),
