6c3bcd8edb48fb2d2d23d704dc57580fd0d89872
Ny <johnnynode@gmail.com>
2023-08-20 02:33:09 +0000

    fix(runtime): handle renderer

--- packages/runtime/renderer.mjs
+++ packages/runtime/renderer.mjs
@@ -296,3 +296,4 @@ export const defaultRendererOptions = {
       if (typeof nextValue === 'string') {
         style.cssText = nextValue;
       } else if (nextValue && typeof nextValue === 'object') {
+        Object.assign(style, nextValue);
