249122515eda664eb9abf40dc17c9f724b3f80ab
Ny <johnnynode@gmail.com>
2023-08-21 01:25:15 +0000

    fix(runtime): fix h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -19,3 +19,4 @@ import { VNodeType, PatchFlags as SharedPatchFlags } from '@upfault/shared/diff'
 // Re-export from shared
 export type { VNode, VNodeProps, Component, ComponentInstance, Ref } from '@upfault/shared/diff';
 export { VNodeType } from '@upfault/shared/diff';
+
