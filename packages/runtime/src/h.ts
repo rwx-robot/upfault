d8d8d4564fa58a84756c434395fd45970489147b
Ny <johnnynode@gmail.com>
2023-08-22 07:53:04 +0000

    feat(runtime): support h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -83,3 +83,4 @@ export interface HFunction {
  */
 function normalizeClass(value: unknown): string | undefined {
   if (value == null) return undefined;
+  if (typeof value === 'string') return value;
