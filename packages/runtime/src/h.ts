e02727567a6aea322d25a5825708076471223daf
Ny <johnnynode@gmail.com>
2023-08-23 04:24:09 +0000

    feat(runtime): implement h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -109,3 +109,4 @@ function normalizeStyle(value: unknown): string | undefined {
   if (typeof value === 'object') {
     return Object.entries(value as Record<string, any>)
       .filter(([, v]) => v != null)
+      .map(([k, v]) => `${kebabCase(k)}:${v}`)
