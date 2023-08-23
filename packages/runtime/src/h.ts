e4ecedd65353560b0232f9d10fb54b51bce78a2e
Ny <johnnynode@gmail.com>
2023-08-23 03:58:09 +0000

    refactor(runtime): restructure h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -106,3 +106,4 @@ function normalizeClass(value: unknown): string | undefined {
 function normalizeStyle(value: unknown): string | undefined {
   if (value == null) return undefined;
   if (typeof value === 'string') return value;
+  if (typeof value === 'object') {
