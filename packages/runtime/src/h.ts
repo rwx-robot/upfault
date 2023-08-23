f2541287c23b4bc30eb23c4f3d3f8f7b524f9d3b
Ny <johnnynode@gmail.com>
2023-08-23 09:12:51 +0000

    refactor(runtime): extract h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -132,3 +132,4 @@ function normalizeProps(props: NormalizedProps | null | undefined): VNodeProps {
   
   // 处理 class
   if (normalized.class != null) {
+    normalized.class = normalizeClass(normalized.class);
