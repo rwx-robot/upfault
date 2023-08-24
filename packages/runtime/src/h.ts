ae3a78ea67eb55b5e829713ffe27f500b35af48f
Ny <johnnynode@gmail.com>
2023-08-24 06:54:56 +0000

    feat(runtime): add h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -158,3 +158,4 @@ function normalizeChildren(children: VNodeChild[]): VNode[] | null {
     if (typeof child === 'string' || typeof child === 'number') {
       return [{ type: Text, children: String(child), props: null, key: null, ref: null, shapeFlag: VNodeShapeFlags.TEXT_NODE, patchFlag: SharedPatchFlags.NONE, dynamicProps: [], el: null, anchor: null, parent: null, componentInstance: null, component: undefined, vnodeType: VNodeType.TEXT }];
     }
+    if (Array.isArray(child)) {
