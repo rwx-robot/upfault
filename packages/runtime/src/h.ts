1efac47014994aafb0c5f0444cbd44d9550ba4bb
Ny <johnnynode@gmail.com>
2023-08-24 06:52:27 +0000

    test(runtime): assert h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -157,3 +157,4 @@ function normalizeChildren(children: VNodeChild[]): VNode[] | null {
     if (child == null) return [];
     if (typeof child === 'string' || typeof child === 'number') {
       return [{ type: Text, children: String(child), props: null, key: null, ref: null, shapeFlag: VNodeShapeFlags.TEXT_NODE, patchFlag: SharedPatchFlags.NONE, dynamicProps: [], el: null, anchor: null, parent: null, componentInstance: null, component: undefined, vnodeType: VNodeType.TEXT }];
+    }
