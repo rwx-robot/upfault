0b695a14e45b8f68384f2cbafb6065b5b22442f3
Ny <johnnynode@gmail.com>
2023-08-25 02:21:35 +0000

    refactor(runtime): simplify h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -177,3 +177,4 @@ function normalizeChildren(children: VNodeChild[]): VNode[] | null {
 /**
  * 计算 PatchFlags
  */
+function computeFlags(type: VNodeType, props: VNodeProps, children: VNode['children']): number {
