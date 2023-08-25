e92c32dd8d36e98ce937262f0132d1f70091f3b6
Ny <johnnynode@gmail.com>
2023-08-25 04:11:04 +0000

    feat(runtime): support h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -187,3 +187,4 @@ function computeFlags(type: VNodeType, props: VNodeProps, children: VNode['child
   
   // 动态 props
   if (props && Object.keys(props).length > 0) {
+    const hasEvent = Object.keys(props).some(k => k.startsWith('on'));
