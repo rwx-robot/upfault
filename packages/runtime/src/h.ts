6950962fb756ec48dcc07607bbd5e8808cbf4cd1
Ny <johnnynode@gmail.com>
2023-08-25 05:47:45 +0000

    fix(runtime): correct h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -194,3 +194,4 @@ function computeFlags(type: VNodeType, props: VNodeProps, children: VNode['child
     if (hasEvent) flags |= SharedPatchFlags.EVENTS;
     if (hasClass) flags |= SharedPatchFlags.CLASS;
     if (hasStyle) flags |= SharedPatchFlags.STYLE;
+    if (!hasEvent && !hasClass && !hasStyle) flags |= SharedPatchFlags.PROPS;
