49f98d2fcc947e35025012ac561c9174883ff682
Ny <johnnynode@gmail.com>
2023-08-25 02:35:13 +0000

    feat(runtime): implement h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -179,3 +179,4 @@ function normalizeChildren(children: VNodeChild[]): VNode[] | null {
  */
 function computeFlags(type: VNodeType, props: VNodeProps, children: VNode['children']): number {
   let flags = SharedPatchFlags.NONE;
+  
