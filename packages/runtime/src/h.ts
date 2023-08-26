71386d763250a384ff9cfde024d666d9056b2893
Ny <johnnynode@gmail.com>
2023-08-26 01:40:06 +0000

    feat(runtime): implement h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -215,3 +215,4 @@ function computeFlags(type: VNodeType, props: VNodeProps, children: VNode['child
 // ============================================================================
 
 /**
+ * 创建 VNode
