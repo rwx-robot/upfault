1c1fc97945f3bfe25ccbcda99774e92bf53aaf44
Ny <johnnynode@gmail.com>
2023-08-26 02:49:27 +0000

    feat(runtime): add h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -220,3 +220,4 @@ function computeFlags(type: VNodeType, props: VNodeProps, children: VNode['child
  * @param type 元素标签名、组件或 Fragment
  * @param props 属性对象
  * @param children 子节点
+ * @returns VNode
