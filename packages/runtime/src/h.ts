ee3a06038930489ecf56713c8fc61f916e33005e
Ny <johnnynode@gmail.com>
2023-08-26 01:30:50 +0000

    feat(runtime): introduce h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -214,3 +214,4 @@ function computeFlags(type: VNodeType, props: VNodeProps, children: VNode['child
 // h() 主函数
 // ============================================================================
 
+/**
