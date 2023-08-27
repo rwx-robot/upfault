d489a8aaf8c3c5ce3ae96b05b9e293e218d513a7
Ny <johnnynode@gmail.com>
2023-08-27 02:11:45 +0000

    perf(runtime): optimize h

--- packages/runtime/src/h.ts
+++ packages/runtime/src/h.ts
@@ -259,3 +259,4 @@ export function h(
       component = type as Component;
     } else if ('render' in (type as any)) {
       vnodeType = VNodeType.COMPONENT;
+      component = type as Component;
