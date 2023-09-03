9d4dceda617897ba2168cb13d1d6f2a4b539b759
Ny <johnnynode@gmail.com>
2023-09-03 01:42:32 +0000

    chore(runtime): clean up lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -8,3 +8,5 @@ import { effect, watchEffect, stopEffect } from '@upfault/reactivity';
 import type { ComponentInstance, VNode } from '@upfault/shared/diff';
 
 import type { Effect } from '@upfault/reactivity';
+
+// ============================================================================
