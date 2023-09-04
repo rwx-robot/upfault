bcff20042112a6c43c4315d8a1fa2faaacc28d3f
Ny <johnnynode@gmail.com>
2023-09-04 01:57:36 +0000

    feat(runtime): support lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -47,3 +47,4 @@ export function getCurrentInstance(): ComponentInstance | null {
  * 设置当前组件实例
  */
 export function setCurrentInstance(instance: ComponentInstance | null): void {
+  currentInstance = instance;
