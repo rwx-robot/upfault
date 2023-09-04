76071209be3836fac89cefd55bc48a3de86bf452
Ny <johnnynode@gmail.com>
2023-09-04 07:29:57 +0000

    feat(runtime): add lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -72,3 +72,4 @@ export function popInstance(): ComponentInstance | null {
 // ============================================================================
 
 function injectHook(
+  hookName: keyof LifecycleHooks,
