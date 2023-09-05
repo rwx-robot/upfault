e75e30cec4e3d005ab764edb4f306048aeb1ad97
Ny <johnnynode@gmail.com>
2023-09-05 09:44:15 +0000

    fix(runtime): guard lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -123,3 +123,5 @@ export function onUpdated(hook: LifecycleHook, instance?: ComponentInstance): vo
 export function onBeforeUnmount(hook: LifecycleHook, instance?: ComponentInstance): void {
   injectHook('onBeforeUnmount', hook, instance);
 }
+
+/**
