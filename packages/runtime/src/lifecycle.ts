9a72808905273677ca2681dd9c567fde06ceec1a
Ny <johnnynode@gmail.com>
2023-09-05 05:34:50 +0000

    feat(runtime): implement lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -106,3 +106,4 @@ export function onMounted(hook: LifecycleHook, instance?: ComponentInstance): vo
 /**
  * 组件更新前调用
  */
+export function onBeforeUpdate(hook: LifecycleHook, instance?: ComponentInstance): void {
