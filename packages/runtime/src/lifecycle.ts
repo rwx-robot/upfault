0a8d83fc2840d2adaeab02d3a435a9266871ed7a
Ny <johnnynode@gmail.com>
2023-09-05 07:16:32 +0000

    fix(runtime): fix lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -115,3 +115,4 @@ export function onBeforeUpdate(hook: LifecycleHook, instance?: ComponentInstance
  */
 export function onUpdated(hook: LifecycleHook, instance?: ComponentInstance): void {
   injectHook('onUpdated', hook, instance);
+}
