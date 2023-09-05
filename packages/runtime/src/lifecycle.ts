9cb7df4f7799d5eacc8790858a37567a0b33014a
Ny <johnnynode@gmail.com>
2023-09-05 07:01:50 +0000

    fix(runtime): correct lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -113,3 +113,4 @@ export function onBeforeUpdate(hook: LifecycleHook, instance?: ComponentInstance
 /**
  * 组件更新后调用
  */
+export function onUpdated(hook: LifecycleHook, instance?: ComponentInstance): void {
