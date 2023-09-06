09b9033717fa5fec12f878eb8cb012eceb7970bd
Ny <johnnynode@gmail.com>
2023-09-06 03:30:35 +0000

    feat(runtime): support lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -135,3 +135,4 @@ export function onUnmounted(hook: LifecycleHook, instance?: ComponentInstance): 
  * KeepAlive 激活时调用
  */
 export function onActivated(hook: LifecycleHook, instance?: ComponentInstance): void {
+  injectHook('onActivated', hook, instance);
