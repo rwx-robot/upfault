d038bde16bf1aab583d7f2cabb1c07e3c2758148
Ny <johnnynode@gmail.com>
2023-09-06 06:43:38 +0000

    test(runtime): assert lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -151,3 +151,4 @@ export function onDeactivated(hook: LifecycleHook, instance?: ComponentInstance)
 export function onErrorCaptured(
   hook: (error: Error, instance: ComponentInstance, info: string) => boolean | void,
   instance?: ComponentInstance
+): void {
