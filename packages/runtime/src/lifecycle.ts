1e5dd94caf4448aea5ac33d698def0f53a8c436d
Ny <johnnynode@gmail.com>
2023-09-07 08:49:19 +0000

    test(runtime): add cases for lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -200,3 +200,4 @@ export async function callHooks(
  */
 export function callBeforeMount(instance: ComponentInstance): void {
   // 同步执行
+  if (instance.onBeforeMount && instance.onBeforeMount.length > 0) {
