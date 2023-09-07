b68d5ac2ea6ff6d8da7c52e5380ce5ef9406cafa
Ny <johnnynode@gmail.com>
2023-09-07 03:44:52 +0000

    test(runtime): cover lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -174,3 +174,4 @@ export function onRenderTriggered(hook: (event: any) => void, instance?: Compone
 // ============================================================================
 
 /**
+ * 执行生命周期钩子数组
