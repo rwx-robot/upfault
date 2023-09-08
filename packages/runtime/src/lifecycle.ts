d9055058f5c10ccf23d10805e86774c6c1addd08
Ny <johnnynode@gmail.com>
2023-09-08 04:08:00 +0000

    chore(runtime): bump lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -221,3 +221,4 @@ export async function callMounted(instance: ComponentInstance): Promise<void> {
 /**
  * 调用 onBeforeUpdate
  */
+export async function callBeforeUpdate(instance: ComponentInstance): Promise<void> {
