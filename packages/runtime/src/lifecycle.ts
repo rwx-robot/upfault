25df694fe2aa1afe301bc811e76d12f04cd0b52f
Ny <johnnynode@gmail.com>
2023-09-08 06:52:26 +0000

    refactor(runtime): inline lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -231,3 +231,5 @@ export async function callBeforeUpdate(instance: ComponentInstance): Promise<voi
 export async function callUpdated(instance: ComponentInstance): Promise<void> {
   await callHooks(instance.onUpdated, instance, 'updated');
 }
+
+/**
