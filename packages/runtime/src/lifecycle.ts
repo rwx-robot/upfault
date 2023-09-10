fce91c71a5f4a8642292b92c5987d1e6caa408be
Ny <johnnynode@gmail.com>
2023-09-10 03:01:03 +0000

    refactor(runtime): inline lifecycle

--- packages/runtime/src/lifecycle.ts
+++ packages/runtime/src/lifecycle.ts
@@ -298,3 +298,4 @@ export function createRenderEffect(
     onTrigger: instance.onRenderTriggered ? instance.onRenderTriggered[0] : undefined,
   });
   // effect() 返回 runner，实际 Effect 在 runner.effect
+  return runner.effect;
