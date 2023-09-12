7b0861c57a48bc8e73dd06f483fad369bf5715a8
Ny <johnnynode@gmail.com>
2023-09-12 07:37:08 +0000

    docs(runtime): annotate renderer_debug.test

--- packages/runtime/src/renderer_debug.test.ts
+++ packages/runtime/src/renderer_debug.test.ts
@@ -2,3 +2,4 @@ import { describe, it, expect, vi } from 'vitest';
 import { createRenderer, defaultRendererOptions } from './renderer';
 
 describe('Renderer debug', () => {
+  let container: HTMLElement;
