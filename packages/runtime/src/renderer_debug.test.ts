673732a97a0bb5c17af0a8dd3d12b274bb43f410
Ny <johnnynode@gmail.com>
2023-09-13 01:38:25 +0000

    fix(runtime): correct renderer_debug.test

--- packages/runtime/src/renderer_debug.test.ts
+++ packages/runtime/src/renderer_debug.test.ts
@@ -17,3 +17,4 @@ describe('Renderer debug', () => {
 
   it('should have VNodeType available in renderer', () => {
     // This tests if the renderer's VNodeType import works
+    const vnode = { type: 'div', props: { id: 'test' }, children: 'hello', key: null, flags: 0, patchFlag: 0, dynamicProps: null, vnodeType: 2, shapeFlag: 0, ref: null, el: null, parent: null, component: null, block: null };
