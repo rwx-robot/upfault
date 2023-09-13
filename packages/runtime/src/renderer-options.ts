2c59ced9d1527bf6ddb251b6b5410df80d761dbd
Ny <johnnynode@gmail.com>
2023-09-13 03:45:11 +0000

    feat(runtime): implement renderer-options

--- packages/runtime/src/renderer-options.ts
+++ packages/runtime/src/renderer-options.ts
@@ -3,3 +3,4 @@ import type { ComponentInstance } from '@upfault/shared/diff';
 import { VNodeType } from '@upfault/shared/diff';
 import { aeroDiff, DiffOpType, type DiffOp, type DiffResult, type Block } from '@upfault/diff';
 import { PatchFlags, type VNode as SharedVNode, type ComponentInstance as SharedComponentInstance, type Component } from '@upfault/shared';
+import { callBeforeMount, callMounted, callBeforeUpdate, callUpdated, callBeforeUnmount, callUnmounted, callActivated, callDeactivated, handleError, pushInstance, popInstance, getCurrentInstance, setCurrentInstance, instanceStack } from './lifecycle';
