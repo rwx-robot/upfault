ca07829836a71fda7ac29fc778d1676c47260a77
Ny <johnnynode@gmail.com>
2023-09-13 03:34:04 +0000

    feat(runtime): introduce renderer-options

--- packages/runtime/src/renderer-options.ts
+++ packages/runtime/src/renderer-options.ts
@@ -2,3 +2,4 @@ import type { VNode } from './h';
 import type { ComponentInstance } from '@upfault/shared/diff';
 import { VNodeType } from '@upfault/shared/diff';
 import { aeroDiff, DiffOpType, type DiffOp, type DiffResult, type Block } from '@upfault/diff';
+import { PatchFlags, type VNode as SharedVNode, type ComponentInstance as SharedComponentInstance, type Component } from '@upfault/shared';
