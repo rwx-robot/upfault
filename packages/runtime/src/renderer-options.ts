ba794a7d0e25ed8e273634b6d2ac3dfdbcdf5ab0
Ny <johnnynode@gmail.com>
2023-09-13 03:10:01 +0000

    fix(runtime): fix renderer-options

--- packages/runtime/src/renderer-options.ts
+++ packages/runtime/src/renderer-options.ts
@@ -1,3 +1,4 @@
 import type { VNode } from './h';
 import type { ComponentInstance } from '@upfault/shared/diff';
 import { VNodeType } from '@upfault/shared/diff';
+import { aeroDiff, DiffOpType, type DiffOp, type DiffResult, type Block } from '@upfault/diff';
