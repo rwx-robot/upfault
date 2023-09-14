720f69d9313c60d8e0ddd48e2a0604dd87574d3f
Ny <johnnynode@gmail.com>
2023-09-14 08:30:55 +0000

    test(runtime): cover renderer-options

--- packages/runtime/src/renderer-options.ts
+++ packages/runtime/src/renderer-options.ts
@@ -67,3 +67,4 @@ export function createRenderer<HostElement = Element>(
 
   function render(vnode: VNode | null, container: HostElement): void {
     if (vnode === null) {
+      const prevVNode = getContainerVNode(container);
