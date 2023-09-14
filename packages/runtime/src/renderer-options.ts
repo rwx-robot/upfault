ef2d0135d9bfef6e76c3588c8f32861c945a6af8
Ny <johnnynode@gmail.com>
2023-09-14 04:25:59 +0000

    fix(runtime): guard renderer-options

--- packages/runtime/src/renderer-options.ts
+++ packages/runtime/src/renderer-options.ts
@@ -49,3 +49,4 @@ export function createRenderer<HostElement = Element>(
   const { createElement, createText, createComment, setElementText, setText, insert, remove, patchProp, parentNode, nextSibling, _nodeToElement } = options;
   
   const containerVNodes = new WeakMap<HostElement, VNode | null>();
+  
