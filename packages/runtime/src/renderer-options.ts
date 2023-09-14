4059b8efc3d6e9d49b59e768761995601cfaafcf
Ny <johnnynode@gmail.com>
2023-09-14 05:42:14 +0000

    fix(runtime): correct renderer-options

--- packages/runtime/src/renderer-options.ts
+++ packages/runtime/src/renderer-options.ts
@@ -53,3 +53,4 @@ export function createRenderer<HostElement = Element>(
   function getContainerVNode(container: HostElement): VNode | null {
     return containerVNodes.get(container) ?? null;
   }
+  
