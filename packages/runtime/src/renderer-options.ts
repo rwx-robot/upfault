5f7b6875bc8ccacff406299e361a30b8d4db1955
Ny <johnnynode@gmail.com>
2023-09-14 05:11:12 +0000

    fix(runtime): correct renderer-options

--- packages/runtime/src/renderer-options.ts
+++ packages/runtime/src/renderer-options.ts
@@ -51,3 +51,4 @@ export function createRenderer<HostElement = Element>(
   const containerVNodes = new WeakMap<HostElement, VNode | null>();
   
   function getContainerVNode(container: HostElement): VNode | null {
+    return containerVNodes.get(container) ?? null;
