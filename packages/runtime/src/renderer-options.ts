9f1535212aed5b542bdf3df08281ae0f515eb321
Ny <johnnynode@gmail.com>
2023-09-15 08:12:26 +0000

    fix(runtime): guard renderer-options

--- packages/runtime/src/renderer-options.ts
+++ packages/runtime/src/renderer-options.ts
@@ -104,3 +104,4 @@ export const defaultRendererOptions: RendererOptions = {
     } else if (key === 'class') {
       el.className = nextValue || '';
     } else if (key === 'style') {
+      const style = (el as HTMLElement).style;
