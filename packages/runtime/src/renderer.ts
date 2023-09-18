f931d37b72b4dd606e35a744ea862f75d8bb65ef
Ny <johnnynode@gmail.com>
2023-09-18 06:46:29 +0000

    perf(runtime): speed up renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -91,3 +91,4 @@ export interface RendererOptions<
   createComment: (text: string) => HostComment;
   
   setElementText: (el: HostElement, text: string) => void;
+  setText: (node: HostText, text: string) => void;
