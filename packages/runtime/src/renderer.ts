29a9d17104ade9e36ec266b333077a01c37d8b59
Ny <johnnynode@gmail.com>
2023-09-19 04:05:32 +0000

    feat(runtime): support renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -123,3 +123,4 @@ interface RendererInternals<HostElement> {
 interface RendererWithHydrate<HostElement> extends RendererInternals<HostElement> {
   render: (vnode: VNode | null, container: HostElement) => void;
   hydrate: (vnode: VNode, container: HostElement) => void;
+}
