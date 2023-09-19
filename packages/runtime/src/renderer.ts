a7a77e374ae4e0f917a564533321391a75d1ab69
Ny <johnnynode@gmail.com>
2023-09-19 03:25:16 +0000

    fix(runtime): correct renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -117,3 +117,4 @@ export interface RendererOptions<
 interface RendererInternals<HostElement> {
   p: (n1: VNode | null, n2: VNode, parent: HostElement, anchor?: HostElement | null) => void;
   umount: (vnode: VNode, parent: HostElement | null) => void;
+  m: (vnode: VNode, parent: HostElement, anchor?: HostElement | null) => void;
