a671735c557cdbb45656a913751cc597cdc7cf49
Ny <johnnynode@gmail.com>
2023-09-19 09:56:04 +0000

    refactor(runtime): restructure renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -151,3 +151,4 @@ export function createRenderer<HostElement = Element>(
   function getContainerVNode(container: HostElement): VNode | null {
     return containerVNodes.get(container) ?? null;
   }
+  
