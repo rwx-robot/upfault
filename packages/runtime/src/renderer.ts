848785980532428dc08eec46b98304f6036d55ad
Ny <johnnynode@gmail.com>
2023-09-20 05:32:44 +0000

    fix(runtime): handle renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -173,3 +173,4 @@ export function createRenderer<HostElement = Element>(
     // 创建 DOM 元素
     const isSVG = type === 'svg' || (vnode as any).isSVG;
     const el = createElement(type as string, isSVG);
+        
