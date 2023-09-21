09d0f008b22f5ed22dd2b91538135dcc9b3ef5a8
Ny <johnnynode@gmail.com>
2023-09-21 07:11:58 +0000

    fix(runtime): guard renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -222,3 +222,4 @@ export function createRenderer<HostElement = Element>(
     insert(el, parent, anchor);
   }
   
+  function mountComment(vnode: VNode, parent: HostElement, anchor: HostElement | null = null): void {
