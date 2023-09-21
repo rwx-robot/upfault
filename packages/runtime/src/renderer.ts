17e444aad903a7f530fa7ed7171b5e858f373b62
Ny <johnnynode@gmail.com>
2023-09-21 06:30:07 +0000

    docs(runtime): clarify renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -219,3 +219,4 @@ export function createRenderer<HostElement = Element>(
   function mountText(vnode: VNode, parent: HostElement, anchor: HostElement | null = null): void {
     const el = createText(vnode.children as string);
     vnode.el = el;
+    insert(el, parent, anchor);
