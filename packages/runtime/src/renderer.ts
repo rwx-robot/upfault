bc8faf3ef07ca926d7a3654f14982f89451840dd
Ny <johnnynode@gmail.com>
2023-09-24 07:50:40 +0000

    perf(runtime): optimize renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -350,3 +350,4 @@ export function createRenderer<HostElement = Element>(
   }
   
   function getRenderFunction(comp: Component): ((proxy: any, ctx: any) => VNode | null) | null {
+    if (typeof comp === 'function') {
