c3a148662751b66d6555b1f905818ccfd8db8e9f
Ny <johnnynode@gmail.com>
2023-09-24 07:50:44 +0000

    feat(runtime): implement renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -351,3 +351,4 @@ export function createRenderer<HostElement = Element>(
   
   function getRenderFunction(comp: Component): ((proxy: any, ctx: any) => VNode | null) | null {
     if (typeof comp === 'function') {
+      return comp as any;
