c43e53d1ebff09cac1386e5c5ec04dcf5bbf8e10
Ny <johnnynode@gmail.com>
2023-09-24 05:52:56 +0000

    chore(runtime): format renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -341,3 +341,4 @@ export function createRenderer<HostElement = Element>(
   function resolveSlots(children: VNode['children']): Record<string, any> {
     if (!children) return {};
     if (Array.isArray(children)) {
+      return { default: () => children };
