db5a39064928b506f8f8d42fceb42f25a47ef9fe
Ny <johnnynode@gmail.com>
2023-09-25 08:34:18 +0000

    feat(runtime): implement renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -394,3 +394,4 @@ export function createRenderer<HostElement = Element>(
     if (vnodeType === VNodeType.ELEMENT) {
       mountElement(vnode, parent, anchor);
     } else if (vnodeType === VNodeType.TEXT) {
+      mountText(vnode, parent, anchor);
