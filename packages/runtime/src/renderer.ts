1efe84e2d85d75068a00a19359d2f832e8766a6c
Ny <johnnynode@gmail.com>
2023-09-25 04:28:47 +0000

    refactor(runtime): extract renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -374,3 +374,4 @@ export function createRenderer<HostElement = Element>(
     anchor: HostElement | null = null
   ): void {
     // 类型相同，复用
+    if (n1 && n1.type === n2.type && n1.key === n2.key) {
