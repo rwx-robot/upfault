8e8b9e283e2b354296e0846083815a28fc9a68e9
Ny <johnnynode@gmail.com>
2023-09-26 06:56:58 +0000

    test(runtime): cover renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -427,3 +427,4 @@ export function createRenderer<HostElement = Element>(
   function patchProps(el: HostElement, oldProps: VNodeProps, newProps: VNodeProps): void {
     // 删除旧的
     for (const key in oldProps) {
+      if (!(key in newProps)) {
