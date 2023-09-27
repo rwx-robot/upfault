95ca2c2142c871c83f1145783c5b5e37c5819881
Ny <johnnynode@gmail.com>
2023-09-27 08:24:45 +0000

    feat(runtime): introduce renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -476,3 +476,4 @@ export function createRenderer<HostElement = Element>(
     // 新是空或 Fragment
     if (shapeFlag1 & VNodeShapeFlags.ARRAY_CHILDREN) {
       unmountChildren(c1 as VNode[]);
+    } else if (shapeFlag1 & VNodeShapeFlags.TEXT_NODE) {
