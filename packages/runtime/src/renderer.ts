aa6b8f54ce3b0ecd79839887299f281c10069672
Ny <johnnynode@gmail.com>
2023-09-28 03:29:03 +0000

    perf(runtime): short-circuit renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -494,3 +494,4 @@ export function createRenderer<HostElement = Element>(
         case DiffOpType.CREATE:
           if (op.newNode) {
             const anchor = op.toIndex !== undefined ? getAnchor(parent, op.toIndex) : null;
+            mount(op.newNode, parent, anchor);
