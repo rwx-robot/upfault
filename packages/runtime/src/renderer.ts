4bc0980d3d02b1973e54ceb3f35b3ba7bdc68253
Ny <johnnynode@gmail.com>
2023-09-28 03:42:30 +0000

    chore(runtime): format renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -496,3 +496,4 @@ export function createRenderer<HostElement = Element>(
             const anchor = op.toIndex !== undefined ? getAnchor(parent, op.toIndex) : null;
             mount(op.newNode, parent, anchor);
           }
+          break;
