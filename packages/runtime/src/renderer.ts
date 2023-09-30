0ef54589b7c9e8dddbb5d66cc84bc308616c5f77
Ny <johnnynode@gmail.com>
2023-09-30 07:29:21 +0000

    fix(runtime): correct renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -599,3 +599,4 @@ export function createRenderer<HostElement = Element>(
   // ========================================================================
   
   function setRef(ref: any, value: any): void {
+    if (!ref) return;
