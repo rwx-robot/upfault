db5765553abe404cdc5fac08f29eabef68ee00f1
Ny <johnnynode@gmail.com>
2023-09-30 01:10:42 +0000

    refactor(runtime): extract renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -568,3 +568,4 @@ export function createRenderer<HostElement = Element>(
   function unmountChildren(children: VNode[]): void {
     for (const child of children) {
       if (child) unmount(child);
+    }
