4a897225d7707fe0607c8f782bab0f8c57867715
Ny <johnnynode@gmail.com>
2023-09-30 08:28:02 +0000

    fix(runtime): correct renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -604,3 +604,4 @@ export function createRenderer<HostElement = Element>(
     if (typeof ref === 'function') {
       ref(value);
     } else if (ref && typeof ref === 'object' && '__v_isRef' in ref) {
+      (ref as any).value = value;
