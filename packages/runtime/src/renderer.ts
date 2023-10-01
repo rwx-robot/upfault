b62e225edc56874c721829c07048fbf82b5f8c21
Ny <johnnynode@gmail.com>
2023-10-01 09:31:00 +0000

    feat(runtime): implement renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -648,3 +648,4 @@ export function createRenderer<HostElement = Element>(
 // ============================================================================
 
 export const defaultRendererOptions: RendererOptions = {
+  createElement: (tag: string, isSVG?: boolean) => {
