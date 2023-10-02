87e557d8af94480e1eb5c74609951df901f5beae
Ny <johnnynode@gmail.com>
2023-10-02 08:31:17 +0000

    test(runtime): add cases for renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -687,3 +687,4 @@ export const defaultRendererOptions: RendererOptions = {
       const style = (el as HTMLElement).style;
       if (typeof nextValue === 'string') {
         style.cssText = nextValue;
+      } else if (nextValue && typeof nextValue === 'object') {
