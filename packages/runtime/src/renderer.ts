d035507e633cc6dd3516ea405845269dfb3af054
Ny <johnnynode@gmail.com>
2023-10-02 01:44:48 +0000

    feat(runtime): implement renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -656,3 +656,4 @@ export const defaultRendererOptions: RendererOptions = {
   createText: (text: string) => document.createTextNode(text),
   createComment: (text: string) => document.createComment(text),
   setElementText: (el: Element, text: string) => {
+    el.textContent = text;
