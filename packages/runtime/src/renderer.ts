ef27c6fe21c354501527a4b648719da0af364c14
Ny <johnnynode@gmail.com>
2023-10-02 01:34:47 +0000

    refactor(runtime): inline renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -655,3 +655,4 @@ export const defaultRendererOptions: RendererOptions = {
   },
   createText: (text: string) => document.createTextNode(text),
   createComment: (text: string) => document.createComment(text),
+  setElementText: (el: Element, text: string) => {
