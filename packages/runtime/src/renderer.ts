007d885a582a2bf6a62c533168acd974ab09308a
Ny <johnnynode@gmail.com>
2023-10-02 03:26:03 +0000

    refactor(runtime): simplify renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -664,3 +664,4 @@ export const defaultRendererOptions: RendererOptions = {
   insert: (child: Node, parent: Element, anchor?: Element | null) => {
     try {
       parent.insertBefore(child, anchor || null);
+    } catch (e) {
