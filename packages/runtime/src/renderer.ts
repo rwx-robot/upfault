8249f421b30ad262d3858b2f6c2f51d3fbc1a2c8
Ny <johnnynode@gmail.com>
2023-10-03 04:12:33 +0000

    feat(runtime): add renderer

--- packages/runtime/src/renderer.ts
+++ packages/runtime/src/renderer.ts
@@ -705,3 +705,4 @@ export const defaultRendererOptions: RendererOptions = {
     }
   },
   parentNode: (node: Element) => node.parentNode as Element,
+  nextSibling: (node: Element) => node.nextElementSibling,
