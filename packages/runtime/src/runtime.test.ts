7d275c16fa7d3188d14e0b25591e7e07ab6ed646
Ny <johnnynode@gmail.com>
2023-10-04 05:11:38 +0000

    chore(runtime): clean up runtime.test

--- packages/runtime/src/runtime.test.ts
+++ packages/runtime/src/runtime.test.ts
@@ -38,3 +38,4 @@ describe('UpFault Runtime', () => {
       expect(vnode.type).toBe('ul');
       expect(Array.isArray(vnode.children)).toBe(true);
       expect((vnode.children as any[]).length).toBe(2);
+    });
