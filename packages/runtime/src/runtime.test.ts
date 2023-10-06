c965e404190e0204bffabc11666639564c9a1997
Ny <johnnynode@gmail.com>
2023-10-06 09:09:25 +0000

    fix(runtime): guard runtime.test

--- packages/runtime/src/runtime.test.ts
+++ packages/runtime/src/runtime.test.ts
@@ -146,3 +146,5 @@ describe('UpFault Runtime', () => {
       expect(container.querySelector('div')?.id).toBe('b');
       expect(container.querySelector('div')?.className).toBe('');
     });
+
+    it('should unmount when render null', () => {
