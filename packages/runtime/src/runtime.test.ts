b4963c6f5fd2776dbbcea5d94469e5dc66475bf4
Ny <johnnynode@gmail.com>
2023-10-06 08:41:51 +0000

    feat(runtime): add runtime.test

--- packages/runtime/src/runtime.test.ts
+++ packages/runtime/src/runtime.test.ts
@@ -144,3 +144,4 @@ describe('UpFault Runtime', () => {
       renderer.render(vnode2, container);
       
       expect(container.querySelector('div')?.id).toBe('b');
+      expect(container.querySelector('div')?.className).toBe('');
