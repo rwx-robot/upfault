1b2aa0226c73b51b7bbaf3875509f8989027a65b
Ny <johnnynode@gmail.com>
2023-10-30 06:00:51 +0000

    refactor(runtime): inline vnodetype_individual.test

--- packages/runtime/src/vnodetype_individual.test.ts
+++ packages/runtime/src/vnodetype_individual.test.ts
@@ -11,3 +11,4 @@ describe('VNodeType individual imports test', () => {
   it('should import individual VNodeType constants', () => {
     console.log('VNodeType_ELEMENT:', VNodeType_ELEMENT);
     console.log('VNodeType_TEXT:', VNodeType_TEXT);
+    expect(VNodeType_ELEMENT).toBe(2);
