0af2e46b76951ab9f4b769ebe0c829a84756d341
Ny <johnnynode@gmail.com>
2023-10-30 01:40:05 +0000

    refactor(runtime): inline vnodetype_import.test

--- packages/runtime/src/vnodetype_import.test.ts
+++ packages/runtime/src/vnodetype_import.test.ts
@@ -2,3 +2,4 @@ import { describe, it, expect } from 'vitest';
 import { VNodeType } from '../../shared/src/diff';
 
 describe('VNodeType import test', () => {
+  it('should import VNodeType', () => {
