3adda41bf5f8003052bef38a359929ae23f46cf6
Ny <johnnynode@gmail.com>
2023-10-30 05:55:43 +0000

    refactor(runtime): simplify vnodetype_individual.test

--- packages/runtime/src/vnodetype_individual.test.ts
+++ packages/runtime/src/vnodetype_individual.test.ts
@@ -10,3 +10,4 @@ import {
 describe('VNodeType individual imports test', () => {
   it('should import individual VNodeType constants', () => {
     console.log('VNodeType_ELEMENT:', VNodeType_ELEMENT);
+    console.log('VNodeType_TEXT:', VNodeType_TEXT);
