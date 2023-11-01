05e802eed914b8b6bc2af2f2c16d22c6740408b0
Ny <johnnynode@gmail.com>
2023-11-01 02:16:27 +0000

    refactor(runtime): inline vite.config

--- packages/runtime/vite.config.ts
+++ packages/runtime/vite.config.ts
@@ -22,3 +22,4 @@ export default defineConfig({
       '@upfault/reactivity': path.resolve(__dirname, '../../packages/reactivity/src'),
       '@upfault/diff': path.resolve(__dirname, '../../packages/diff/src'),
       '@upfault/scheduler': path.resolve(__dirname, '../../packages/scheduler/src'),
+    },
