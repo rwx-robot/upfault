4bb299bdbad8819761b7df725799a6a39469b123
Ny <johnnynode@gmail.com>
2023-11-03 05:33:01 +0000

    feat(scheduler): support index.d

--- packages/scheduler/src/index.d.ts
+++ packages/scheduler/src/index.d.ts
@@ -9,3 +9,4 @@ export { createScheduler, Scheduler, getDefaultScheduler, scheduleCallback, canc
 export { Priority, SchedulerTask, SchedulerOptions, DEFAULT_SCHEDULER_OPTIONS, SchedulerStats } from '@upfault/shared';
 export declare const VERSION = "0.2.0";
 export declare const PACKAGE_NAME = "@upfault/scheduler";
+//# sourceMappingURL=index.d.ts.map
