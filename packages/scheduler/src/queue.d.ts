2561132c2c9bd92c5654f155efab48a0738463f7
Ny <johnnynode@gmail.com>
2023-11-06 02:55:55 +0000

    feat(scheduler): support queue.d

--- packages/scheduler/src/queue.d.ts
+++ packages/scheduler/src/queue.d.ts
@@ -56,3 +56,4 @@ export declare class PriorityQueue implements TaskQueue {
  * 创建优先队列的工厂函数
  */
 export declare function createPriorityQueue(comparator?: (a: SchedulerTask, b: SchedulerTask) => number): PriorityQueue;
+export declare function generateTaskId(): number;
