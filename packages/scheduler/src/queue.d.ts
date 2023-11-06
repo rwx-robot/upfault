ff7e15b53338410c06c6d2a5539171701b8f29ca
Ny <johnnynode@gmail.com>
2023-11-06 03:53:34 +0000

    refactor(scheduler): extract queue.d

--- packages/scheduler/src/queue.d.ts
+++ packages/scheduler/src/queue.d.ts
@@ -61,3 +61,4 @@ export declare function generateTaskId(): number;
  * 创建调度任务的辅助函数
  */
 export declare function createSchedulerTask(callback: () => void, priority?: Priority, // NORMAL
+fibre?: any, data?: any): SchedulerTask;
