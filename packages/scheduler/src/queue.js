ce27b761b974be9251e3d98e1c76391a8afc94aa
Ny <johnnynode@gmail.com>
2023-11-09 01:27:50 +0000

    chore(scheduler): format queue

--- packages/scheduler/src/queue.js
+++ packages/scheduler/src/queue.js
@@ -103,3 +103,4 @@ export class PriorityQueue {
                 break;
             // Use temporary variables to satisfy TypeScript
             const indexVal = this.heap[index];
+            const parentVal = this.heap[parentIndex];
