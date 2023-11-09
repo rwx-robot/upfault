0885a9e5ae2c97ac512734f313938fbacbae7b94
Ny <johnnynode@gmail.com>
2023-11-09 01:41:44 +0000

    perf(scheduler): speed up queue

--- packages/scheduler/src/queue.js
+++ packages/scheduler/src/queue.js
@@ -104,3 +104,4 @@ export class PriorityQueue {
             // Use temporary variables to satisfy TypeScript
             const indexVal = this.heap[index];
             const parentVal = this.heap[parentIndex];
+            [this.heap[index], this.heap[parentIndex]] = [parentVal, indexVal];
