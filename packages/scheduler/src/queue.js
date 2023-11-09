9e412ae753cb512ef6001930940db550af4a5e8f
Ny <johnnynode@gmail.com>
2023-11-09 04:55:39 +0000

    feat(scheduler): introduce queue

--- packages/scheduler/src/queue.js
+++ packages/scheduler/src/queue.js
@@ -120,3 +120,4 @@ export class PriorityQueue {
             if (rightChild < length && this.comparator(this.heap[rightChild], this.heap[smallest]) < 0) {
                 smallest = rightChild;
             }
+            if (smallest === index)
