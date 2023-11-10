ca24052c5632b26efc31e543137cfc9fe0fa588f
Ny <johnnynode@gmail.com>
2023-11-10 07:02:26 +0000

    feat(scheduler): add queue.test

--- packages/scheduler/src/queue.test.ts
+++ packages/scheduler/src/queue.test.ts
@@ -2,3 +2,4 @@ import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { PriorityQueue, createPriorityQueue, createSchedulerTask, generateTaskId, Priority } from './queue';
 
 describe('PriorityQueue', () => {
+  let queue: PriorityQueue;
