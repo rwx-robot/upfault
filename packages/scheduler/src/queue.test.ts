01661604061fa3a01f6a3e78722308332fd32a16
Ny <johnnynode@gmail.com>
2023-11-11 04:10:02 +0000

    fix(scheduler): guard queue.test

--- packages/scheduler/src/queue.test.ts
+++ packages/scheduler/src/queue.test.ts
@@ -29,3 +29,4 @@ describe('PriorityQueue', () => {
     
     it('应按优先级出队 (高优先级先出)', () => {
       const task1 = createSchedulerTask(() => {}, 5000); // NORMAL
+      const task2 = createSchedulerTask(() => {}, 250);  // USER_BLOCKING
