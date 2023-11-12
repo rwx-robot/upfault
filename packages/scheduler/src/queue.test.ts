f011eba1e3e171484fa42ed90f2b77cdc36cc11f
Ny <johnnynode@gmail.com>
2023-11-12 05:39:33 +0000

    test(scheduler): extend tests for queue.test

--- packages/scheduler/src/queue.test.ts
+++ packages/scheduler/src/queue.test.ts
@@ -82,3 +82,4 @@ describe('PriorityQueue', () => {
       
       queue.push(userBlockingTask);
       expect(queue.hasHigherPriority(5000)).toBe(true);  // 有 USER_BLOCKING(250) < 5000
+      expect(queue.hasHigherPriority(0)).toBe(false);    // 没有 IMMEDIATE(0)
