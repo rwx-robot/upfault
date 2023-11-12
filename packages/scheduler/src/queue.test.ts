8ec174b227b365b4e6ecb6695e23a7667ade6882
Ny <johnnynode@gmail.com>
2023-11-12 01:07:45 +0000

    feat(scheduler): support queue.test

--- packages/scheduler/src/queue.test.ts
+++ packages/scheduler/src/queue.test.ts
@@ -60,3 +60,4 @@ describe('PriorityQueue', () => {
     
     it('同优先级同过期时间按 ID 排序 (稳定性)', () => {
       const task1 = createSchedulerTask(() => {}, 5000);
+      const task2 = createSchedulerTask(() => {}, 5000);
