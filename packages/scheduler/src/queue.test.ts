a8e046fd82af7e5e487da2060ffc6cc6e0011019
Ny <johnnynode@gmail.com>
2023-11-12 08:15:15 +0000

    test(scheduler): add cases for queue.test

--- packages/scheduler/src/queue.test.ts
+++ packages/scheduler/src/queue.test.ts
@@ -96,3 +96,4 @@ describe('PriorityQueue', () => {
     
     it('clear 应清空队列', () => {
       queue.push(createSchedulerTask(() => {}, 5000));
+      queue.push(createSchedulerTask(() => {}, 250));
