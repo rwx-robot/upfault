c85d83a67554cd63db2e8bec939d40eb82f1f9f2
Ny <johnnynode@gmail.com>
2023-11-13 04:57:28 +0000

    test(scheduler): add cases for queue.test

--- packages/scheduler/src/queue.test.ts
+++ packages/scheduler/src/queue.test.ts
@@ -126,3 +126,4 @@ describe('PriorityQueue', () => {
       expect(task.priority).toBe(250);
       expect(task.callback).toBe(callback);
       expect(task.fibre).toBeNull();
+      expect(task.data).toEqual({ foo: 'bar' });
