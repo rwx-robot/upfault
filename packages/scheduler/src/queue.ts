2357c88fecfa525f92539fa9b9d588f3ac05497b
Ny <johnnynode@gmail.com>
2023-11-16 09:35:31 +0000

    feat(scheduler): introduce queue

--- packages/scheduler/src/queue.ts
+++ packages/scheduler/src/queue.ts
@@ -122,3 +122,4 @@ export class PriorityQueue implements TaskQueue {
       const parentIndex = (index - 1) >> 1;
       if (this.comparator(this.heap[index]!, this.heap[parentIndex]!) >= 0) break;
       
+      // Use temporary variables to satisfy TypeScript
