678d7510e685e98e50151a38ee9de7ce6ee619d3
Ny <johnnynode@gmail.com>
2023-11-16 09:44:59 +0000

    feat(scheduler): add queue

--- packages/scheduler/src/queue.ts
+++ packages/scheduler/src/queue.ts
@@ -124,3 +124,4 @@ export class PriorityQueue implements TaskQueue {
       
       // Use temporary variables to satisfy TypeScript
       const indexVal = this.heap[index]!;
+      const parentVal = this.heap[parentIndex]!;
