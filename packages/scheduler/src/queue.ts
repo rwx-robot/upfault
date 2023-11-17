f110a7504eb5e9b7ee8ee5e8fb201f156e348156
Ny <johnnynode@gmail.com>
2023-11-17 04:05:06 +0000

    test(scheduler): cover queue

--- packages/scheduler/src/queue.ts
+++ packages/scheduler/src/queue.ts
@@ -141,3 +141,4 @@ export class PriorityQueue implements TaskQueue {
       if (leftChild < length && this.comparator(this.heap[leftChild]!, this.heap[smallest]!) < 0) {
         smallest = leftChild;
       }
+      if (rightChild < length && this.comparator(this.heap[rightChild]!, this.heap[smallest]!) < 0) {
