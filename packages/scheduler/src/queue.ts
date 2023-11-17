a608f70fc3f86cc56b3759e3cd601c27861c5153
Ny <johnnynode@gmail.com>
2023-11-17 04:08:58 +0000

    feat(scheduler): implement queue

--- packages/scheduler/src/queue.ts
+++ packages/scheduler/src/queue.ts
@@ -142,3 +142,4 @@ export class PriorityQueue implements TaskQueue {
         smallest = leftChild;
       }
       if (rightChild < length && this.comparator(this.heap[rightChild]!, this.heap[smallest]!) < 0) {
+        smallest = rightChild;
