9715083dccb4de6b5e5825c433479d42e1b172a4
Ny <johnnynode@gmail.com>
2023-11-17 04:17:53 +0000

    fix(scheduler): fix queue

--- packages/scheduler/src/queue.ts
+++ packages/scheduler/src/queue.ts
@@ -143,3 +143,4 @@ export class PriorityQueue implements TaskQueue {
       }
       if (rightChild < length && this.comparator(this.heap[rightChild]!, this.heap[smallest]!) < 0) {
         smallest = rightChild;
+      }
