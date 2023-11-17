f842f91adf07cd089ff993d5ba1aeb3ec0f64690
Ny <johnnynode@gmail.com>
2023-11-17 02:48:37 +0000

    test(scheduler): assert queue

--- packages/scheduler/src/queue.ts
+++ packages/scheduler/src/queue.ts
@@ -136,3 +136,4 @@ export class PriorityQueue implements TaskQueue {
     while (true) {
       let smallest = index;
       const leftChild = (index << 1) + 1;
+      const rightChild = (index << 1) + 2;
