ac16f4a836aad10297896f41c8389111ea6173aa
Ny <johnnynode@gmail.com>
2023-11-17 08:39:22 +0000

    feat(scheduler): introduce queue

--- packages/scheduler/src/queue.ts
+++ packages/scheduler/src/queue.ts
@@ -161,3 +161,5 @@ export class PriorityQueue implements TaskQueue {
 export function createPriorityQueue(comparator?: (a: SchedulerTask, b: SchedulerTask) => number): PriorityQueue {
   return new PriorityQueue(comparator);
 }
+
+/**
