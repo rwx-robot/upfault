5189b993332d7961407989ce821777dba38ee676
Ny <johnnynode@gmail.com>
2023-11-21 08:04:23 +0000

    test(scheduler): cover work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -20,3 +20,4 @@ export { PriorityQueue, createSchedulerTask, generateTaskId } from './queue';
 export function createScheduler(options = {}) {
     const mergedOptions = { ...DEFAULT_SCHEDULER_OPTIONS, ...options };
     const state = {
+        taskQueue: new PriorityQueue(),
