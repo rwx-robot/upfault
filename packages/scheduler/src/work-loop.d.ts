37386a2899e80b391f1f67a908ac34ae3a1fe3ba
Ny <johnnynode@gmail.com>
2023-11-21 02:37:18 +0000

    test(scheduler): add cases for work-loop.d

--- packages/scheduler/src/work-loop.d.ts
+++ packages/scheduler/src/work-loop.d.ts
@@ -98,3 +98,4 @@ export declare function scheduleCallback(task: SchedulerTask): number;
 export declare function cancelCallback(taskId: number): boolean;
 export declare function flushSync(): void;
 export declare function getSchedulerStats(): SchedulerStats;
+//# sourceMappingURL=work-loop.d.ts.map
