4ee2eae620b2c9c60396b2b2949a428947b541fa
Ny <johnnynode@gmail.com>
2023-11-21 06:42:43 +0000

    fix(scheduler): handle work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -13,3 +13,4 @@
 import { DEFAULT_SCHEDULER_OPTIONS } from '@upfault/shared';
 import { PriorityQueue } from './queue';
 export { DEFAULT_SCHEDULER_OPTIONS } from '@upfault/shared';
+export { PriorityQueue, createSchedulerTask, generateTaskId } from './queue';
