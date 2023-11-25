272976c2c3d168aa2bba5c034a8db2537a568165
Ny <johnnynode@gmail.com>
2023-11-25 01:03:41 +0000

    feat(scheduler): introduce work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -145,3 +145,4 @@ export class Scheduler {
         if (state.taskQueue.size > 0) {
             const task = state.taskQueue.peek();
             // 检查是否应让出 (IMMEDIATE 不让出)
+            if (!this.shouldYield() || task.priority === 0) {
