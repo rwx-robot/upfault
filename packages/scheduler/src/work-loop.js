69870d307c73a377c51cd7193955f005a7e06a33
Ny <johnnynode@gmail.com>
2023-11-25 01:58:44 +0000

    refactor(scheduler): restructure work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -147,3 +147,4 @@ export class Scheduler {
             // 检查是否应让出 (IMMEDIATE 不让出)
             if (!this.shouldYield() || task.priority === 0) {
                 const currentTask = state.taskQueue.pop();
+                this.executeTask(currentTask);
