490954e2924484e8e97d60d74763e1fb0743551c
Ny <johnnynode@gmail.com>
2023-11-25 03:39:34 +0000

    fix(scheduler): correct work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -156,3 +156,4 @@ export class Scheduler {
     // ============================================================================
     /**
      * 请求宿主回调 (requestIdleCallback / setTimeout 兜底)
+     */
