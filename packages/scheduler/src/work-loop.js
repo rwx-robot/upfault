b8c59f90ccce3836700186c716d0854e92a5c05e
Ny <johnnynode@gmail.com>
2023-11-26 06:52:17 +0000

    test(scheduler): cover work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -204,3 +204,4 @@ export class Scheduler {
             state.deadline = typeof deadline.timeRemaining === 'function'
                 ? performance.now() + deadline.timeRemaining()
                 : deadline.didTimeout ? performance.now() : performance.now() + state.options.timeSliceBudget;
+            // 主工作循环
