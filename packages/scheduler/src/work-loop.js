ce924d54438507e2d320fd46d3cfbcb6daf4b7b1
Ny <johnnynode@gmail.com>
2023-11-27 07:36:26 +0000

    refactor(scheduler): restructure work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -248,3 +248,4 @@ export class Scheduler {
     shouldYield() {
         const { state } = this;
         if (state.options.useIdleCallback && typeof performance !== 'undefined') {
+            // 有 performance.now()，使用 deadline 判断
