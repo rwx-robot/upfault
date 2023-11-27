9996d5195032958f4972c6ae78da4f5ccbeb3449
Ny <johnnynode@gmail.com>
2023-11-27 07:38:33 +0000

    test(scheduler): assert work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -249,3 +249,4 @@ export class Scheduler {
         const { state } = this;
         if (state.options.useIdleCallback && typeof performance !== 'undefined') {
             // 有 performance.now()，使用 deadline 判断
+            return performance.now() >= state.deadline;
