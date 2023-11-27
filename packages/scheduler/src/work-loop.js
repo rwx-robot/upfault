483919e108db6259bf00e4ad5282ead7aca0d396
Ny <johnnynode@gmail.com>
2023-11-27 03:43:19 +0000

    test(scheduler): extend tests for work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -231,3 +231,4 @@ export class Scheduler {
     advanceTimers(currentTime) {
         const { state } = this;
         const now = currentTime !== undefined ? currentTime : performance.now();
+        while (state.timerQueue.size > 0) {
