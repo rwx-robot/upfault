87f1bc7c47db26ea043d3f3d03d24fc932589d56
Ny <johnnynode@gmail.com>
2023-11-27 05:19:49 +0000

    fix(scheduler): fix work-loop

--- packages/scheduler/src/work-loop.js
+++ packages/scheduler/src/work-loop.js
@@ -238,3 +238,4 @@ export class Scheduler {
             state.timerQueue.pop();
             // 任务未取消，加入任务队列
             if (!timerTask.cancelled) {
+                state.taskQueue.push(timerTask);
