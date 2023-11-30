4ecc7bd05de7e6c7929841aac0f2942c4696ccda
Ny <johnnynode@gmail.com>
2023-11-30 03:38:28 +0000

    feat(scheduler): introduce work-loop.test

--- packages/scheduler/src/work-loop.test.ts
+++ packages/scheduler/src/work-loop.test.ts
@@ -35,3 +35,4 @@ describe('Scheduler Work Loop', () => {
   describe('基础调度', () => {
     it('应调度并执行任务', async () => {
       const callback = vi.fn();
+      const task = createSchedulerTask(callback, 0); // IMMEDIATE 立即执行
