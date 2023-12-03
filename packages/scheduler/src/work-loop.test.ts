fcbe578c638cca286be7ed8ce49945894fdd7872
Ny <johnnynode@gmail.com>
2023-12-03 02:14:02 +0000

    test(scheduler): assert work-loop.test

--- packages/scheduler/src/work-loop.test.ts
+++ packages/scheduler/src/work-loop.test.ts
@@ -164,3 +164,4 @@ describe('定时器队列', () => {
     it('应统计取消任务', async () => {
       const task = createSchedulerTask(vi.fn(), 5000);
       const id = scheduler.scheduleCallback(task);
+      scheduler.cancelCallback(id);
