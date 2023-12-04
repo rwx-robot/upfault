86f54239fb943450d0f086b1d84e76e050f0b867
Ny <johnnynode@gmail.com>
2023-12-04 07:06:42 +0000

    docs(scheduler): clarify work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -48,3 +48,4 @@ export function createScheduler(options: Partial<SchedulerOptions> = {}): Schedu
   
   const state: SchedulerState = {
     taskQueue: new PriorityQueue(),
+    timerQueue: new PriorityQueue((a, b) => a.expirationTime - b.expirationTime),
