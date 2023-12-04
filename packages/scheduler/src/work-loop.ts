a1a3f7b35fd6e28b7fe1fa96839cec8b751e5d3e
Ny <johnnynode@gmail.com>
2023-12-04 03:27:05 +0000

    feat(scheduler): add work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -31,3 +31,4 @@ export { PriorityQueue, createSchedulerTask, generateTaskId } from './queue';
 interface SchedulerState {
   taskQueue: PriorityQueue;
   timerQueue: PriorityQueue; // 延迟任务队列 (按 expirationTime 排序)
+  currentTask: SchedulerTask | null;
