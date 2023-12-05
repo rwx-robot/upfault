f2a7b9c2f58a4a1dd7329562f239aa108437dd45
Ny <johnnynode@gmail.com>
2023-12-05 09:21:10 +0000

    refactor(scheduler): simplify work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -101,3 +101,4 @@ export class Scheduler {
     // 统计
     state.stats.totalTasks++;
     state.stats.priorityDistribution[task.priority] = (state.stats.priorityDistribution[task.priority] || 0) + 1;
+    
