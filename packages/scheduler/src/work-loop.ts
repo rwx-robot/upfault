c5422b1e4c6c09091469d85e4d5b457a4c239e03
Ny <johnnynode@gmail.com>
2023-12-05 02:36:51 +0000

    test(scheduler): add cases for work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -67,3 +67,4 @@ export function createScheduler(options: Partial<SchedulerOptions> = {}): Schedu
         [250]: 0,    // USER_BLOCKING
         [5000]: 0,   // NORMAL
         [10000]: 0,  // LOW
+        [0x7fffffff]: 0, // IDLE
