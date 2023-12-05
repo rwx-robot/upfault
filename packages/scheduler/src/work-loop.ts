9629fb305be1bde240ee37c28e9e1fe2819c4e03
Ny <johnnynode@gmail.com>
2023-12-05 05:20:04 +0000

    fix(scheduler): fix work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -82,3 +82,4 @@ export function createScheduler(options: Partial<SchedulerOptions> = {}): Schedu
 export class Scheduler {
   private state: SchedulerState;
   
+  constructor(state: SchedulerState) {
