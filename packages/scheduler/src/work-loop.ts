4b264845b3879d2fa87dfb8e0a5bcbd89f100bd4
Ny <johnnynode@gmail.com>
2023-12-06 07:57:30 +0000

    test(scheduler): add cases for work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -136,3 +136,4 @@ export class Scheduler {
     const timerTasks = state.timerQueue.toArray();
     const timerTask = timerTasks.find(t => t.id === taskId);
     if (timerTask) {
+      timerTask.cancelled = true;
