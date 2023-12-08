c311304171b17f9d5864cb4fc59a4aa828af220e
Ny <johnnynode@gmail.com>
2023-12-08 03:13:58 +0000

    perf(scheduler): speed up work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -196,3 +196,4 @@ export class Scheduler {
       if (!this.shouldYield() || task.priority === 0) {
         const currentTask = state.taskQueue.pop()!;
         this.executeTask(currentTask);
+      }
