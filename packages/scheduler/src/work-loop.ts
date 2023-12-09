91dc4bfa02686365390150d88924c070effadbdc
Ny <johnnynode@gmail.com>
2023-12-09 08:06:57 +0000

    feat(scheduler): introduce work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -261,3 +261,4 @@ export class Scheduler {
         ? performance.now() + deadline.timeRemaining()
         : deadline.didTimeout ? performance.now() : performance.now() + state.options.timeSliceBudget;
       
+      // 主工作循环
