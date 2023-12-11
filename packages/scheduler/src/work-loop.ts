6e3d4d745b3a54a3a11ca9b0b79861156fdd8799
Ny <johnnynode@gmail.com>
2023-12-11 09:05:29 +0000

    fix(scheduler): correct work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -345,3 +345,4 @@ export class Scheduler {
       state.stats.avgExecutionTime = state.stats.totalExecutionTime / state.stats.completedTasks;
     } catch (error) {
       console.error('[UpFault Scheduler] Task execution failed:', error);
+      task.completedAt = performance.now();
