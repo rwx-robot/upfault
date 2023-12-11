5e3b0d0e210c06c48b6b5e2768ad5cd5c53a175f
Ny <johnnynode@gmail.com>
2023-12-11 08:23:58 +0000

    test(scheduler): extend tests for work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -343,3 +343,4 @@ export class Scheduler {
       const execTime = task.completedAt - task.startedAt!;
       state.stats.totalExecutionTime += execTime;
       state.stats.avgExecutionTime = state.stats.totalExecutionTime / state.stats.completedTasks;
+    } catch (error) {
