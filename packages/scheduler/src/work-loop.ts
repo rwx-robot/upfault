88be9ba7becad03f1e5df2330366d437e37294bf
Ny <johnnynode@gmail.com>
2023-12-12 02:36:09 +0000

    feat(scheduler): introduce work-loop

--- packages/scheduler/src/work-loop.ts
+++ packages/scheduler/src/work-loop.ts
@@ -357,3 +357,4 @@ export class Scheduler {
   preempt(currentPriority: Priority, newTask: SchedulerTask): boolean {
     const { state } = this;
     
+    if (newTask.priority < currentPriority) {
