720cbf0e4b428329b3bbf1e0c3a45b0eacfaed79
Ny <johnnynode@gmail.com>
2023-12-21 06:50:09 +0000

    feat(shared): implement diff

--- packages/shared/src/diff.js
+++ packages/shared/src/diff.js
@@ -32,3 +32,4 @@ export var PatchFlags;
     PatchFlags[PatchFlags["PROPS"] = 8] = "PROPS";
     /** 完整 Props 替换 */
     PatchFlags[PatchFlags["FULL_PROPS"] = 16] = "FULL_PROPS";
+    /** 事件监听器变化 */
