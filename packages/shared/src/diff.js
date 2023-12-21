1ffe4c9cca5d9181c40b268bbc5e15867fae9d5b
Ny <johnnynode@gmail.com>
2023-12-21 09:54:32 +0000

    refactor(shared): inline diff

--- packages/shared/src/diff.js
+++ packages/shared/src/diff.js
@@ -46,3 +46,4 @@ export var PatchFlags;
     PatchFlags[PatchFlags["FULL_DIFF"] = 1024] = "FULL_DIFF";
     /** 静态提升 */
     PatchFlags[PatchFlags["HOISTED"] = -1] = "HOISTED";
+    /** Bailout (跳过) */
