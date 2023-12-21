8edab15051f6961c932717a98140a41bf5f21517
Ny <johnnynode@gmail.com>
2023-12-21 07:51:59 +0000

    feat(shared): add diff

--- packages/shared/src/diff.js
+++ packages/shared/src/diff.js
@@ -37,3 +37,4 @@ export var PatchFlags;
     /** Keyed Fragment 重排 */
     PatchFlags[PatchFlags["KEYED_FRAGMENT"] = 64] = "KEYED_FRAGMENT";
     /** Unkeyed Fragment */
+    PatchFlags[PatchFlags["UNKEYED_FRAGMENT"] = 128] = "UNKEYED_FRAGMENT";
