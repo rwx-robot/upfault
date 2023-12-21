767d516a1191b593b1cef641ae615317cf934d98
Ny <johnnynode@gmail.com>
2023-12-21 06:53:42 +0000

    docs(shared): document diff

--- packages/shared/src/diff.js
+++ packages/shared/src/diff.js
@@ -33,3 +33,4 @@ export var PatchFlags;
     /** 完整 Props 替换 */
     PatchFlags[PatchFlags["FULL_PROPS"] = 16] = "FULL_PROPS";
     /** 事件监听器变化 */
+    PatchFlags[PatchFlags["EVENTS"] = 32] = "EVENTS";
