d2ce6e565eab2391f8ca6c24e71874075bff0e12
Ny <johnnynode@gmail.com>
2023-12-24 06:35:05 +0000

    refactor(shared): extract diff.test

--- packages/shared/src/diff.test.ts
+++ packages/shared/src/diff.test.ts
@@ -77,3 +77,4 @@ describe('Diff Types', () => {
       expect(DEFAULT_AERODIFF_OPTIONS.enableShapeMatching).toBe(true);
       expect(DEFAULT_AERODIFF_OPTIONS.maxDepth).toBe(100);
       expect(DEFAULT_AERODIFF_OPTIONS.collectStats).toBe(true);
+    });
