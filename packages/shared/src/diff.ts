bf4fc9a9849492a33d7ac95d3f862b9b360c2664
Ny <johnnynode@gmail.com>
2023-12-25 06:04:12 +0000

    chore(shared): format diff

--- packages/shared/src/diff.ts
+++ packages/shared/src/diff.ts
@@ -34,3 +34,4 @@ export const VNodeType_KEEPALIVE = 9;
 
 // Type for VNodeType values
 // VNodeType 同时作为值与类型使用：值为常量对象，类型为 1|2|...|9 的联合
+export type VNodeType = typeof VNodeType[keyof typeof VNodeType];
