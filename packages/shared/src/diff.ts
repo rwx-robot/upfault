1b22078b1ffb84133882626986d0f9245b2edfc4
Ny <johnnynode@gmail.com>
2023-12-25 04:44:27 +0000

    test(shared): cover diff

--- packages/shared/src/diff.ts
+++ packages/shared/src/diff.ts
@@ -29,3 +29,4 @@ export const VNodeType_BLOCK = 4;
 export const VNodeType_FRAGMENT = 5;
 export const VNodeType_COMMENT = 6;
 export const VNodeType_TELEPORT = 7;
+export const VNodeType_SUSPENSE = 8;
