f2e5fe3e2ed7fad2b491eecf4ec4828eef681469
Ny <johnnynode@gmail.com>
2024-01-06 01:16:38 +0000

    fix(shared): correct flags

--- packages/shared/src/flags.js
+++ packages/shared/src/flags.js
@@ -26,3 +26,4 @@ export var VNodeFlags;
     /** 组合标志：静态节点 (文本 + 元素) */
     VNodeFlags[VNodeFlags["STATIC"] = 3] = "STATIC";
     /** 组合标志：动态节点 (单一 + 多重) */
+    VNodeFlags[VNodeFlags["DYNAMIC"] = 24] = "DYNAMIC";
