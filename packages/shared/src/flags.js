3956b71a136341e945219d091973a2c1a1250423
Ny <johnnynode@gmail.com>
2024-01-06 01:16:48 +0000

    test(shared): cover flags

--- packages/shared/src/flags.js
+++ packages/shared/src/flags.js
@@ -27,3 +27,4 @@ export var VNodeFlags;
     VNodeFlags[VNodeFlags["STATIC"] = 3] = "STATIC";
     /** 组合标志：动态节点 (单一 + 多重) */
     VNodeFlags[VNodeFlags["DYNAMIC"] = 24] = "DYNAMIC";
+    /** 组合标志：交互节点 (事件 + 插槽) */
