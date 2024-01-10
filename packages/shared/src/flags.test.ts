0eee1fb696ff324ba8fa0ce61bb12a61372a645e
Ny <johnnynode@gmail.com>
2024-01-10 09:52:16 +0000

    feat(shared): support flags.test

--- packages/shared/src/flags.test.ts
+++ packages/shared/src/flags.test.ts
@@ -18,3 +18,4 @@ describe('VNodeFlags', () => {
       expect(VNodeFlags.STATIC_ELEMENT).toBe(0b0000_0010);
       expect(VNodeFlags.STATIC_KEY).toBe(0b0000_0100);
       expect(VNodeFlags.PURE_DYNAMIC).toBe(0b0000_1000);
+      expect(VNodeFlags.MULTI_DYNAMIC).toBe(0b0001_0000);
