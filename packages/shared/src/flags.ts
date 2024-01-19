7ce5b6d171258ea17838758cc2617cc81e11e604
Ny <johnnynode@gmail.com>
2024-01-19 05:13:48 +0000

    test(shared): add cases for flags

--- packages/shared/src/flags.ts
+++ packages/shared/src/flags.ts
@@ -65,3 +65,4 @@ export function isPureDynamic(flags: number): boolean {
  * 判断是否有交互 (事件或插槽)
  */
 export function isInteractive(flags: number): boolean {
+  return (flags & VNodeFlags.INTERACTIVE) !== 0;
