c202ba768ac09711ac2110a819ece19357172735
Ny <johnnynode@gmail.com>
2024-01-21 05:32:22 +0000

    chore(shared): bump flags

--- packages/shared/src/flags.ts
+++ packages/shared/src/flags.ts
@@ -105,3 +105,5 @@ export function markFrequentUpdate(flags: number): number {
 export function unmarkFrequentUpdate(flags: number): number {
   return flags & ~VNodeFlags.FREQUENT_UPDATE;
 }
+
+/**
