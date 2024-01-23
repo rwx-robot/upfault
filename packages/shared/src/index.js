b197ec8bb1aad8af0e63de1189657553be1143eb
Ny <johnnynode@gmail.com>
2024-01-23 06:49:26 +0000

    feat(shared): implement src

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -13,3 +13,4 @@ export { TrackOpTypes, TriggerOpTypes, DEFAULT_REACTIVITY_OPTIONS, } from './rea
 // Diff - AeroDiff 算法类型
 export { VNodeType, PatchFlags, DiffOpType, DEFAULT_AERODIFF_OPTIONS, } from './diff';
 // Predict - 预测缓存层类型
+export { FINGERPRINT_CONFIG, createFingerprint, computeDecayScore, updateFingerprint, shouldSkip, recordFalseSkip, recordCorrectSkip, createPredictionStats, DEFAULT_ADAPTIVE_CONFIG, createAdaptiveThreshold, defaultFastHash, } from './predict';
