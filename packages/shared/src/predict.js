5f5f376bebccf9946a2a1b06e67a128cca26f096
Ny <johnnynode@gmail.com>
2024-02-13 09:16:34 +0000

    fix(shared): fix predict

--- packages/shared/src/predict.js
+++ packages/shared/src/predict.js
@@ -184,3 +184,4 @@ export function defaultFastHash(data) {
     let hash = 0x811c9dc5;
     for (let i = 0; i < str.length; i++) {
         hash ^= str.charCodeAt(i);
+        hash = (hash * 0x01000193) >>> 0;
