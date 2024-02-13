168d56e0708c96fe5f2d9ff81a98368948f052ca
Ny <johnnynode@gmail.com>
2024-02-13 08:19:19 +0000

    test(shared): extend tests for predict

--- packages/shared/src/predict.js
+++ packages/shared/src/predict.js
@@ -181,3 +181,4 @@ export function createAdaptiveThreshold(config = {}) {
 /** 默认快速哈希 (FNV-1a 变体) */
 export function defaultFastHash(data) {
     const str = JSON.stringify(data);
+    let hash = 0x811c9dc5;
