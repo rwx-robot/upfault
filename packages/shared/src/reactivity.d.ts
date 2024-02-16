d80887fc2a1cac534e20669dd9cdf4fd94fb1099
Ny <johnnynode@gmail.com>
2024-02-16 01:57:03 +0000

    fix(shared): guard reactivity.d

--- packages/shared/src/reactivity.d.ts
+++ packages/shared/src/reactivity.d.ts
@@ -37,3 +37,4 @@ export interface ReactiveMarker {
 export type Reactive<T extends object> = T & ReactiveMarker;
 /** 只读响应式对象 */
 export type ReadonlyReactive<T extends object> = Readonly<T> & ReactiveMarker;
+/** 浅层响应式 */
