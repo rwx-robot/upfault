67d0b9846047bbe1b8011a940a3bd16f9e7b15a8
Ny <johnnynode@gmail.com>
2024-02-16 01:21:56 +0000

    refactor(shared): restructure reactivity.d

--- packages/shared/src/reactivity.d.ts
+++ packages/shared/src/reactivity.d.ts
@@ -36,3 +36,4 @@ export interface ReactiveMarker {
 /** 响应式对象类型 */
 export type Reactive<T extends object> = T & ReactiveMarker;
 /** 只读响应式对象 */
+export type ReadonlyReactive<T extends object> = Readonly<T> & ReactiveMarker;
