8b67a911ea8855268ba0b66e0bcfdad8ffc35dc7
Ny <johnnynode@gmail.com>
2024-02-20 04:08:33 +0000

    fix(shared): correct reactivity.d

--- packages/shared/src/reactivity.d.ts
+++ packages/shared/src/reactivity.d.ts
@@ -127,3 +127,4 @@ export interface WatchOptions<Immediate extends boolean = false> {
     /** 调度器 */
     scheduler?: (fn: () => void) => void;
     /** 回调触发时的上下文 */
+    onTrack?: (event: DebuggerEvent) => void;
