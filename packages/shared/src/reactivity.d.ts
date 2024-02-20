5022a133030a9d71cca98ea6bc284b895ccafb7e
Ny <johnnynode@gmail.com>
2024-02-20 08:48:32 +0000

    fix(shared): guard reactivity.d

--- packages/shared/src/reactivity.d.ts
+++ packages/shared/src/reactivity.d.ts
@@ -138,3 +138,4 @@ export type WatchSource<T> = Ref<T> | ComputedRef<T> | (() => T) | T[];
 /** 停止监听函数 */
 export interface WatchStopHandle {
     (): void;
+    readonly __v_isWatchStopHandle: true;
