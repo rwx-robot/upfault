6181c9120dbf86a5242a0055e54648d1776828b9
Ny <johnnynode@gmail.com>
2024-02-20 05:09:09 +0000

    feat(shared): introduce reactivity.d

--- packages/shared/src/reactivity.d.ts
+++ packages/shared/src/reactivity.d.ts
@@ -129,3 +129,4 @@ export interface WatchOptions<Immediate extends boolean = false> {
     /** 回调触发时的上下文 */
     onTrack?: (event: DebuggerEvent) => void;
     /** 回调触发时的上下文 */
+    onTrigger?: (event: DebuggerEvent) => void;
