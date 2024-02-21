abd640c2cedefdbb1899b328bdf0975e94d2de15
Ny <johnnynode@gmail.com>
2024-02-21 04:47:09 +0000

    feat(shared): implement reactivity.d

--- packages/shared/src/reactivity.d.ts
+++ packages/shared/src/reactivity.d.ts
@@ -148,3 +148,4 @@ export type ComputedSetter<T> = (value: T) => void;
 export type RefUnwrapBailTypes = boolean;
 /** 响应式工具类型 */
 export type UnwrapRef<T> = T extends Ref<infer V> ? V : T;
+export type UnwrapRefs<T> = {
