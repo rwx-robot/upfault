1f8db10e8ee652333d6cc62f3c0d08f6dda5c6ce
Ny <johnnynode@gmail.com>
2024-03-01 08:08:00 +0000

    feat(shared): add reactivity

--- packages/shared/src/reactivity.ts
+++ packages/shared/src/reactivity.ts
@@ -51,3 +51,4 @@ export type ReadonlyReactive<T extends object> = Readonly<T> & ReactiveMarker;
 export type ShallowReactive<T extends object> = T & ReactiveMarker & { readonly __v_isShallow: true };
 
 /** 依赖追踪上下文 */
+export interface Dep {
