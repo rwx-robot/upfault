c64d2bc9b5bbcf4a8d32b54027dea9360bbb0ea7
Ny <johnnynode@gmail.com>
2024-03-17 04:22:16 +0000

    fix(shared): handle scheduler

--- packages/shared/src/scheduler.js
+++ packages/shared/src/scheduler.js
@@ -33,3 +33,4 @@ export const PriorityNames = {
 export function priorityFromExpirationTime(expirationTime) {
     const now = performance.now();
     const timeLeft = expirationTime - now;
+    if (timeLeft <= 0)
