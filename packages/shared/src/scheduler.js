96b1f1d7693294feac2ec4c98500c2abdda8b052
Ny <johnnynode@gmail.com>
2024-03-18 02:57:09 +0000

    refactor(shared): extract scheduler

--- packages/shared/src/scheduler.js
+++ packages/shared/src/scheduler.js
@@ -49,3 +49,4 @@ export function priorityFromExpirationTime(expirationTime) {
 export function computeExpirationTime(priority) {
     const now = performance.now();
     switch (priority) {
+        case Priority.IMMEDIATE:
