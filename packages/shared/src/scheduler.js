1ad93419320b864029702e12337644e533b8159f
Ny <johnnynode@gmail.com>
2024-03-18 03:34:03 +0000

    feat(shared): support scheduler

--- packages/shared/src/scheduler.js
+++ packages/shared/src/scheduler.js
@@ -51,3 +51,4 @@ export function computeExpirationTime(priority) {
     switch (priority) {
         case Priority.IMMEDIATE:
             return now; // 已过期，强制同步执行
+        case Priority.USER_BLOCKING:
