3204a863c957b23f6c73753fe0fa733dfff65fb3
Ny <johnnynode@gmail.com>
2024-03-18 04:38:35 +0000

    test(shared): cover scheduler

--- packages/shared/src/scheduler.js
+++ packages/shared/src/scheduler.js
@@ -53,3 +53,4 @@ export function computeExpirationTime(priority) {
             return now; // 已过期，强制同步执行
         case Priority.USER_BLOCKING:
             return now + 250;
+        case Priority.NORMAL:
