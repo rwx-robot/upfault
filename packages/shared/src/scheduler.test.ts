153395cf0d92d3d06d9f1947ecd394aee50296d0
Ny <johnnynode@gmail.com>
2024-03-25 04:38:00 +0000

    test(shared): extend tests for scheduler.test

--- packages/shared/src/scheduler.test.ts
+++ packages/shared/src/scheduler.test.ts
@@ -119,3 +119,4 @@ describe('Scheduler Types', () => {
       expect(VNodeType.COMPONENT).toBe('COMPONENT');
       expect(VNodeType.BLOCK).toBe('BLOCK');
       expect(VNodeType.FRAGMENT).toBe('FRAGMENT');
+      expect(VNodeType.COMMENT).toBe('COMMENT');
