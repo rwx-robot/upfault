0453bff0487f4b07fb10a03d486ba2f555fc325b
Ny <johnnynode@gmail.com>
2024-03-26 08:16:36 +0000

    refactor(shared): inline scheduler

--- packages/shared/src/scheduler.ts
+++ packages/shared/src/scheduler.ts
@@ -28,3 +28,4 @@ export const enum Priority {
 export const PriorityNames: Record<Priority, string> = {
   [Priority.IMMEDIATE]: 'Immediate',
   [Priority.USER_BLOCKING]: 'UserBlocking',
+  [Priority.NORMAL]: 'Normal',
