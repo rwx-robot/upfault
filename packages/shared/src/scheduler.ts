df28d6ebb956701f23dcd1a858b1050c915a43d9
Ny <johnnynode@gmail.com>
2024-03-27 09:52:56 +0000

    chore(shared): clean up scheduler

--- packages/shared/src/scheduler.ts
+++ packages/shared/src/scheduler.ts
@@ -54,3 +54,4 @@ export function computeExpirationTime(priority: Priority): number {
   const now = performance.now();
   switch (priority) {
     case Priority.IMMEDIATE:
+      return now; // 已过期，强制同步执行
