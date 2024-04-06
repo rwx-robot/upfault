1abd76090862655730b052dbc6727c29b986db32
Ny <johnnynode@gmail.com>
2024-04-06 03:59:12 +0000

    test(shared): assert utils.d

--- packages/shared/src/utils.d.ts
+++ packages/shared/src/utils.d.ts
@@ -34,3 +34,4 @@ export declare function isRef<T>(value: unknown): value is Ref<T>;
 /**
  * 判断是否为 ComputedRef
  */
+export declare function isComputedRef<T>(value: unknown): value is ComputedRef<T>;
