6b822681f36c3fc33e9c4757dd247bb19130298a
Ny <johnnynode@gmail.com>
2024-04-06 05:13:38 +0000

    test(shared): extend tests for utils.d

--- packages/shared/src/utils.d.ts
+++ packages/shared/src/utils.d.ts
@@ -39,3 +39,4 @@ export declare function isComputedRef<T>(value: unknown): value is ComputedRef<T
  * 判断是否为响应式对象
  */
 export declare function isReactive(value: unknown): value is Reactive<Record<string, unknown>>;
+/**
