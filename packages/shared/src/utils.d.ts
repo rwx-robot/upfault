68c9f85713fa0aa551dab1c859f4c8f41b74d0b7
Ny <johnnynode@gmail.com>
2024-04-09 05:20:43 +0000

    feat(shared): introduce utils.d

--- packages/shared/src/utils.d.ts
+++ packages/shared/src/utils.d.ts
@@ -98,3 +98,4 @@ export declare function flattenTree<T>(nodes: T[], getChildren: (node: T) => T[]
  */
 export declare function traverseTreeBFS<T>(root: T, getChildren: (node: T) => T[] | undefined, visitor: (node: T, depth: number) => boolean | void): void;
 /**
+ * 性能计时器
