dd294483d3d0380b82999542dc72bbb13b62c0ce
Ny <johnnynode@gmail.com>
2024-04-21 08:45:20 +0000

    fix(shared): fix utils

--- packages/shared/src/utils.js
+++ packages/shared/src/utils.js
@@ -184,3 +184,4 @@ export function traverseTreeBFS(root, getChildren, visitor) {
             break;
         const children = getChildren(node);
         if (children?.length) {
+            for (const child of children) {
