7d283c3bda72cec6c20266a9dcb7b17f1d9281d3
Ny <johnnynode@gmail.com>
2024-04-29 05:34:04 +0000

    docs(shared): annotate utils.test

--- packages/shared/src/utils.test.ts
+++ packages/shared/src/utils.test.ts
@@ -35,3 +35,5 @@ describe('Utils', () => {
     it('相同 type 和 key 应返回 true', () => {
       expect(isSameNode({ type: 'div', key: 'a' }, { type: 'div', key: 'a' })).toBe(true);
     });
+
+    it('不同 type 应返回 false', () => {
