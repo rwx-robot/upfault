e03b3503df96deeec4237c4cb61b9ca558d218e9
Ny <johnnynode@gmail.com>
2024-05-01 04:51:20 +0000

    test(shared): assert utils.test

--- packages/shared/src/utils.test.ts
+++ packages/shared/src/utils.test.ts
@@ -76,3 +76,4 @@ describe('Utils', () => {
       it('非函数应返回 false', () => {
         expect(isFunction({})).toBe(false);
         expect(isFunction(null)).toBe(false);
+        expect(isFunction('str')).toBe(false);
