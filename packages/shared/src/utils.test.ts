fbd9ed819d05ef4fe50dfb0497df81657b3654c4
Ny <johnnynode@gmail.com>
2024-05-03 08:38:48 +0000

    refactor(shared): restructure utils.test

--- packages/shared/src/utils.test.ts
+++ packages/shared/src/utils.test.ts
@@ -126,3 +126,4 @@ describe('Utils', () => {
 
     describe('isRef / isComputedRef / isReactive / isReadonly / isVNode', () => {
       it('应正确识别标记对象', () => {
+        expect(isRef({ __v_isRef: true })).toBe(true);
