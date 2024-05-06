c4a60cdeb49b82c3cec5023e7554e8775c0c9d80
Ny <johnnynode@gmail.com>
2024-05-06 02:29:32 +0000

    feat(shared): introduce utils.test

--- packages/shared/src/utils.test.ts
+++ packages/shared/src/utils.test.ts
@@ -177,3 +177,4 @@ describe('Utils', () => {
   describe('flatten', () => {
     it('应扁平化嵌套数组', () => {
       expect(flatten([1, [2, 3], 4])).toEqual([1, 2, 3, 4]);
+      expect(flatten([[1, 2], [3, 4]])).toEqual([1, 2, 3, 4]);
