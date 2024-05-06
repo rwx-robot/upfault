81079dbb3a67d2edc532867bf6d5a884359e14b0
Ny <johnnynode@gmail.com>
2024-05-06 03:47:20 +0000

    refactor(shared): inline utils.test

--- packages/shared/src/utils.test.ts
+++ packages/shared/src/utils.test.ts
@@ -179,3 +179,4 @@ describe('Utils', () => {
       expect(flatten([1, [2, 3], 4])).toEqual([1, 2, 3, 4]);
       expect(flatten([[1, 2], [3, 4]])).toEqual([1, 2, 3, 4]);
       expect(flatten([1, 2, 3])).toEqual([1, 2, 3]);
+      expect(flatten([])).toEqual([]);
