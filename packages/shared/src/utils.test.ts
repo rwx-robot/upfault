37f2014f82e75a18a5e4ccc242a167fe913ee689
Ny <johnnynode@gmail.com>
2024-05-08 07:05:33 +0000

    test(shared): assert utils.test

--- packages/shared/src/utils.test.ts
+++ packages/shared/src/utils.test.ts
@@ -232,3 +232,4 @@ describe('Utils', () => {
   describe('unique', () => {
     it('应去重并保持顺序', () => {
       expect(unique([1, 2, 2, 3, 1, 4])).toEqual([1, 2, 3, 4]);
+      expect(unique(['a', 'b', 'a', 'c'])).toEqual(['a', 'b', 'c']);
