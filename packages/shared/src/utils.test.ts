ccc7692a052664980b2867cbb112bef54c1f5c71
Ny <johnnynode@gmail.com>
2024-05-11 06:24:44 +0000

    perf(shared): cache utils.test

--- packages/shared/src/utils.test.ts
+++ packages/shared/src/utils.test.ts
@@ -292,3 +292,4 @@ describe('Utils', () => {
       const tree = { id: 1, children: [{ id: 2 }, { id: 3, children: [{ id: 4 }] }] };
       const visited: number[] = [];
       
+      traverseTreeBFS(tree, n => n.children, (n, depth) => {
