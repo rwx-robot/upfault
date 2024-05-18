ee440e8cd2e942a7f024fc782834803c1aeef343
Ny <johnnynode@gmail.com>
2024-05-18 02:06:10 +0000

    test(shared): add cases for utils

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -38,3 +38,4 @@ export function isString(value: unknown): value is string {
 export function isNumber(value: unknown): value is number {
   return typeof value === 'number' && !Number.isNaN(value);
 }
+
