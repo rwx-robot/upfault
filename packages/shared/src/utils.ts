6b374e313c5d2f217378328b8dfc64bd9e4c6b62
Ny <johnnynode@gmail.com>
2024-05-19 02:02:10 +0000

    test(shared): assert utils

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -59,3 +59,5 @@ export function isRef<T>(value: unknown): value is Ref<T> {
 export function isComputedRef<T>(value: unknown): value is ComputedRef<T> {
   return isObject(value) && (value as any).__v_isComputed === true;
 }
+
+/**
