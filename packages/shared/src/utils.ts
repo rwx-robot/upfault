908e9399a11c4a791c6675b5c11a3c446e08c31a
Ny <johnnynode@gmail.com>
2024-05-19 01:01:19 +0000

    refactor(shared): restructure utils

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -57,3 +57,4 @@ export function isRef<T>(value: unknown): value is Ref<T> {
  * 判断是否为 ComputedRef
  */
 export function isComputedRef<T>(value: unknown): value is ComputedRef<T> {
+  return isObject(value) && (value as any).__v_isComputed === true;
