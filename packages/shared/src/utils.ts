f4e96d1293193a6b2fd4c9f0b74f3f5dc8431b07
Ny <johnnynode@gmail.com>
2024-05-21 04:30:01 +0000

    feat(shared): implement utils

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -104,3 +104,4 @@ export function hasChanged(a: unknown, b: unknown): boolean {
 export function flatten<T>(arr: (T | T[])[]): T[] {
   return arr.reduce((acc: T[], val: T | T[]) => {
     return acc.concat(Array.isArray(val) ? flatten(val) : [val]);
+  }, [] as T[]);
