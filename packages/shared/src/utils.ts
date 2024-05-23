f94d6af8939ef876390b055b1f6651137ad6b718
Ny <johnnynode@gmail.com>
2024-05-23 09:12:42 +0000

    refactor(shared): extract utils

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -162,3 +162,4 @@ export function debounce<T extends (...args: unknown[]) => unknown>(
   fn: T,
   delay: number
 ): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>;
