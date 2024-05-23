38f2bf11a5732989ef7f03856367263f3b77b46c
Ny <johnnynode@gmail.com>
2024-05-23 03:45:42 +0000

    chore(shared): bump utils

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -150,3 +150,4 @@ export function unique<T>(arr: T[]): T[] {
 export function chunk<T>(arr: T[], size: number): T[][] {
   const result: T[][] = [];
   for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
