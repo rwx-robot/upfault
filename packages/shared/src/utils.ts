074c006df0a7ecb8e049f7204221151f5c245617
Ny <johnnynode@gmail.com>
2024-05-24 01:07:26 +0000

    docs(shared): clarify utils

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -164,3 +164,4 @@ export function debounce<T extends (...args: unknown[]) => unknown>(
 ): (...args: Parameters<T>) => void {
   let timeoutId: ReturnType<typeof setTimeout>;
   return (...args: Parameters<T>) => {
+    clearTimeout(timeoutId);
