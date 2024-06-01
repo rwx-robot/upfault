b78b63fc55a49e4ac8d4ee1b6c352be52c3ebf10
Ny <johnnynode@gmail.com>
2024-06-01 04:50:20 +0000

    feat(shared): implement utils

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -348,3 +348,4 @@ export function assert(condition: unknown, message?: string): asserts condition 
   if (!condition) {
     throw new Error(`Assertion failed: ${message ?? 'Unknown error'}`);
   }
+}
