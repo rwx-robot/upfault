0fd301022b22cbba20f2ad6e7025ab4954b91971
Ny <johnnynode@gmail.com>
2024-06-10 05:56:19 +0000

    test(devtools): cover detector

--- packages/devtools/src/detector.ts
+++ packages/devtools/src/detector.ts
@@ -62,3 +62,4 @@ export function detectUpFault(rootElement?: Element): DetectionResult {
  */
 export function isUpFaultApp(element: Element): boolean {
   return element.hasAttribute(UF_FAULT_MARKER);
+}
