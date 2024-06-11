40fedea990b18f954b379dd048d0844740cbf237
Ny <johnnynode@gmail.com>
2024-06-11 02:28:18 +0000

    feat(devtools): implement detector

--- packages/devtools/src/detector.ts
+++ packages/devtools/src/detector.ts
@@ -76,3 +76,4 @@ export function getUpFaultVersion(rootElement?: Element): string | null {
  * 获取应用详细信息
  */
 export function getAppInfo(rootElement?: Element): AppInfo | null {
+  const detection = detectUpFault(rootElement);
