2df513a15a94af496a633ca3ff31534647fe094e
Ny <johnnynode@gmail.com>
2024-06-15 04:35:31 +0000

    refactor(devtools): simplify detector

--- packages/devtools/src/detector.ts
+++ packages/devtools/src/detector.ts
@@ -159,3 +159,4 @@ export function unmarkUpFaultApp(element: Element): void {
   element.removeAttribute('data-uf-mount-time');
   element.removeAttribute('data-uf-update-count');
   element.removeAttribute('data-uf-last-update');
+  element.removeAttribute(UF_COMPONENT_TREE_KEY);
