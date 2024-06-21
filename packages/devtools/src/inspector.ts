ba8618de763aad9498f359f1520c6e8808b08784
Ny <johnnynode@gmail.com>
2024-06-21 09:16:03 +0000

    feat(devtools): introduce inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -64,3 +64,4 @@ export function createInspector(): Inspector {
       effects: inspectEffects(instance),
       lifecycle: inspectLifecycle(instance),
       parent: instance.parent ? inspectComponent(instance.parent) : undefined,
+      children: instance.children?.map(inspectComponent) || [],
