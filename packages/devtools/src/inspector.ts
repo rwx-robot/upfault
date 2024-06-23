a9c18817bd38cc0e4d799f4dd9375145576f3a25
Ny <johnnynode@gmail.com>
2024-06-23 06:04:45 +0000

    test(devtools): cover inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -101,3 +101,4 @@ export function createInspector(): Inspector {
 
   function findComponentByUid(rootInstance: ComponentInstance, uid: number): ComponentInfo | null {
     const tree = inspectComponent(rootInstance);
+    return findInTree(tree, c => c.uid === uid);
