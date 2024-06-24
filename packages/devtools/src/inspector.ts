dd2a1dd77597f291c4872092115ea01ed8a8312f
Ny <johnnynode@gmail.com>
2024-06-24 05:47:01 +0000

    feat(devtools): introduce inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -121,3 +121,4 @@ export function createInspector(): Inspector {
     const rect = element.getBoundingClientRect();
     highlightOverlay = document.createElement('div');
     highlightOverlay.style.cssText = `
+      position: fixed;
