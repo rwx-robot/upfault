231b3260ad03dfb19741e7576ee7bf0a7d586f8b
Ny <johnnynode@gmail.com>
2024-06-25 07:51:46 +0000

    refactor(devtools): inline inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -151,3 +151,4 @@ function getComponentName(type: any): string {
   if (typeof type === 'string') return type;
   if (type?.displayName) return type.displayName;
   if (type?.name) return type.name;
+  if (type?.constructor?.name) return type.constructor.name;
