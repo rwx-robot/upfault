3d31dd8e2a21e8adf79d9ca4a68773f3df7ca014
Ny <johnnynode@gmail.com>
2024-06-26 07:12:04 +0000

    feat(devtools): implement inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -174,3 +174,4 @@ function inspectRefs(instance: ComponentInstance): Record<string, any> {
   
   for (const [key, value] of Object.entries(state)) {
     if (value && typeof value === 'object' && '__v_isRef' in value) {
+      refs[key] = {
