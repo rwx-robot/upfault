a06d45feed7475e640093430b7767a755b534822
Ny <johnnynode@gmail.com>
2024-06-26 06:42:05 +0000

    test(devtools): extend tests for inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -173,3 +173,4 @@ function inspectRefs(instance: ComponentInstance): Record<string, any> {
   const state = getReactiveState(instance);
   
   for (const [key, value] of Object.entries(state)) {
+    if (value && typeof value === 'object' && '__v_isRef' in value) {
