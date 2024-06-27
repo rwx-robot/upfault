48adc802060e57d4e703be7350fe8b29e52dea83
Ny <johnnynode@gmail.com>
2024-06-27 03:02:50 +0000

    feat(devtools): add inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -186,3 +186,4 @@ function inspectRefs(instance: ComponentInstance): Record<string, any> {
 }
 
 function inspectComputed(instance: ComponentInstance): Record<string, any> {
+  const computed: Record<string, any> = {};
