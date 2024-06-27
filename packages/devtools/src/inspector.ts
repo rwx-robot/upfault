e6dec93f4bf071ad0ed3485448ac99e84aebff40
Ny <johnnynode@gmail.com>
2024-06-27 04:09:33 +0000

    feat(devtools): add inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -190,3 +190,4 @@ function inspectComputed(instance: ComponentInstance): Record<string, any> {
   const state = getReactiveState(instance);
   
   for (const [key, value] of Object.entries(state)) {
+    if (value && typeof value === 'object' && '__v_isComputed' in value) {
