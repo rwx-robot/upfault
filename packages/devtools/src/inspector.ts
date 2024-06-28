1aee41719cfa6ac8b40676798d2a217ee77da5c5
Ny <johnnynode@gmail.com>
2024-06-28 08:16:38 +0000

    feat(devtools): implement inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -218,3 +218,4 @@ function inspectLifecycle(instance: ComponentInstance): any {
     onUpdated: Array.isArray(instance.onUpdated) && instance.onUpdated.length > 0,
     onBeforeUnmount: Array.isArray(instance.onBeforeUnmount) && instance.onBeforeUnmount.length > 0,
     onUnmounted: Array.isArray(instance.onUnmounted) && instance.onUnmounted.length > 0,
+  };
