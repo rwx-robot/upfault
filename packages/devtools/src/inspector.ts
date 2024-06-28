aaa9188683e8fbf5711798ad5cafc0d0ad21f9f7
Ny <johnnynode@gmail.com>
2024-06-28 06:31:34 +0000

    test(devtools): extend tests for inspector

--- packages/devtools/src/inspector.ts
+++ packages/devtools/src/inspector.ts
@@ -215,3 +215,4 @@ function inspectLifecycle(instance: ComponentInstance): any {
     onBeforeMount: Array.isArray(instance.onBeforeMount) && instance.onBeforeMount.length > 0,
     onMounted: Array.isArray(instance.onMounted) && instance.onMounted.length > 0,
     onBeforeUpdate: Array.isArray(instance.onBeforeUpdate) && instance.onBeforeUpdate.length > 0,
+    onUpdated: Array.isArray(instance.onUpdated) && instance.onUpdated.length > 0,
