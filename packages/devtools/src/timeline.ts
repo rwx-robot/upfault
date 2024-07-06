8541f2306db4768bb9acfb9bb2cd4c15a51228b6
Ny <johnnynode@gmail.com>
2024-07-06 05:34:16 +0000

    chore(devtools): format timeline

--- packages/devtools/src/timeline.ts
+++ packages/devtools/src/timeline.ts
@@ -157,3 +157,4 @@ export function getTimelineStats(): {
     stats.eventTypes[event.type] = (stats.eventTypes[event.type] || 0) + 1;
     if (event.duration !== undefined) {
       totalDuration += event.duration;
+      durationCount++;
