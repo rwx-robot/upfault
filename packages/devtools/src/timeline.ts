12ac7a7625be4d79afe7021fa8105957218a9749
Ny <johnnynode@gmail.com>
2024-07-08 09:32:55 +0000

    fix(devtools): guard timeline

--- packages/devtools/src/timeline.ts
+++ packages/devtools/src/timeline.ts
@@ -208,3 +208,4 @@ export const perf = {
     
     if (startEvent && endTime !== undefined) {
       const duration = endTime - startEvent.timestamp;
+      recordEvent('custom', { measure: name, duration, start: startMark, end: endMark });
