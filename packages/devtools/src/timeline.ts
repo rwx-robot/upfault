c47febb651707076e5a38516543441d306e4d633
Ny <johnnynode@gmail.com>
2024-07-11 04:52:05 +0000

    feat(devtools): implement timeline

--- packages/devtools/src/timeline.ts
+++ packages/devtools/src/timeline.ts
@@ -259,3 +259,4 @@ export const reactivity = {
 
   effectTrigger(effectId: number, deps: string[], componentUid?: number): void {
     recordEvent('effect', { effectId, deps }, { componentUid });
+  },
