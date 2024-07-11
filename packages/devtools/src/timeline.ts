e44dca36a9f2820aa46c18b9e928cc45dc66fa50
Ny <johnnynode@gmail.com>
2024-07-11 02:44:40 +0000

    test(devtools): add cases for timeline

--- packages/devtools/src/timeline.ts
+++ packages/devtools/src/timeline.ts
@@ -255,3 +255,4 @@ export const reactivity = {
 
   computedEval(computedName: string, value: any, dependencies: string[], componentUid?: number): void {
     recordEvent('computed-eval', { computedName, value, dependencies }, { componentUid });
+  },
