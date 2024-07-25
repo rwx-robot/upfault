1e6dc0e34c729e0b83882760e2a6346db0cf9f3d
Ny <johnnynode@gmail.com>
2024-07-25 09:02:29 +0000

    fix(predict): handle predict.test

--- packages/predict-cache/src/predict.test.ts
+++ packages/predict-cache/src/predict.test.ts
@@ -15,3 +15,4 @@ import {
 describe('Predict Cache - Fingerprint', () => {
   it('should create initial fingerprint', () => {
     const fp = createFingerprint();
+    expect(fp.updateCount).toBe(0);
