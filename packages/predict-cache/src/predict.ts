078624753fb92006b03695fee60b04125cfd2f8e
Ny <johnnynode@gmail.com>
2024-08-12 08:15:16 +0000

    chore(predict): format predict

--- packages/predict-cache/src/predict.ts
+++ packages/predict-cache/src/predict.ts
@@ -277,3 +277,4 @@ export const DEFAULT_ADAPTIVE_CONFIG: AdaptiveThresholdConfig = {
 export interface AdaptiveThreshold {
   config: AdaptiveThresholdConfig;
   currentThreshold: number;
+  recentStats: PredictionStats[];
