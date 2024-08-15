d0b51b1631266d481253ee0521a4eb57c2b551ad
Ny <johnnynode@gmail.com>
2024-08-15 09:54:09 +0000

    chore(predict): bump predict

--- packages/predict-cache/src/predict.ts
+++ packages/predict-cache/src/predict.ts
@@ -349,3 +349,4 @@ export function defaultFastHash(data: unknown): number {
   let hash = 0x811c9dc5;
   for (let i = 0; i < str.length; i++) {
     hash ^= str.charCodeAt(i);
+    hash = (hash * 0x01000193) >>> 0;
