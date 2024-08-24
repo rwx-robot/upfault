e0a3e6a2f7696fe87a20559b5468d27dc8813b42
Ny <johnnynode@gmail.com>
2024-08-24 05:10:13 +0000

    test(ssr): add cases for hydration.test

--- packages/ssr/src/__tests__/hydration.test.ts
+++ packages/ssr/src/__tests__/hydration.test.ts
@@ -76,3 +76,4 @@ describe('SSR Hydration', () => {
 
     it('should call onHydrated callback', () => {
       const el = document.createElement('div');
+      el.setAttribute('data-hydrate', 'test');
