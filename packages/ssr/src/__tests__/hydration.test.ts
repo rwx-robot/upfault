28cd32d87bdda973c8df7acea5e0afdb25884ac3
Ny <johnnynode@gmail.com>
2024-08-25 04:20:30 +0000

    test(ssr): assert hydration.test

--- packages/ssr/src/__tests__/hydration.test.ts
+++ packages/ssr/src/__tests__/hydration.test.ts
@@ -95,3 +95,4 @@ describe('SSR Hydration', () => {
 
     it('should observe matching elements', () => {
       const el1 = document.createElement('div');
+      el1.setAttribute('data-lazy', 'test');
