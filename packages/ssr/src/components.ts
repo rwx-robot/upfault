6ef946fdf0222c14cf93c9aeb03b68c9f340bf60
Ny <johnnynode@gmail.com>
2024-08-26 05:15:14 +0000

    feat(ssr): add components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -8,3 +8,4 @@ import { h, type VNode, type ComponentInstance } from '@upfault/runtime';
 import { Priority, type SchedulerTask } from '@upfault/scheduler';
 import { VNodeType } from '@upfault/shared';
 
+export interface StreamingOptions {
