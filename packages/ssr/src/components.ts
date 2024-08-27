be07218c6e2ba7d85d0f12e1d11dca793bd5249a
Ny <johnnynode@gmail.com>
2024-08-27 02:39:24 +0000

    fix(ssr): correct components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -22,3 +22,4 @@ export interface StreamRenderer {
   render(vnode: VNode): ReadableStream;
   pipeToNodeWritable(writable: NodeJS.WritableStream): Promise<void>;
   pipeToWebWritable(writable: WritableStream): Promise<void>;
+  abort(): void;
