723fa2050b50fedee9703ccfeb54e2bb012ae534
Ny <johnnynode@gmail.com>
2024-08-27 05:47:42 +0000

    feat(ssr): support components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -28,3 +28,4 @@ export interface StreamRenderer {
 export interface SuspenseBoundary {
   fallback: VNode | string | (() => VNode | string);
   onResolve?: (data: any) => void;
+  onReject?: (error: Error) => void;
