21588c90527ce1ceea850333c5c482b6e404aca4
Ny <johnnynode@gmail.com>
2024-08-28 05:56:05 +0000

    feat(ssr): support components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -51,3 +51,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
             }
             
             await renderStreaming(vnode, (chunk) => {
+              controller.enqueue(new TextEncoder().encode(chunk));
