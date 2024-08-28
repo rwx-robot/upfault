aeb2b2e110658a4a05123dd2d308837612d609a7
Ny <johnnynode@gmail.com>
2024-08-28 08:29:25 +0000

    test(ssr): cover components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -57,3 +57,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
             controller.enqueue(new TextEncoder().encode('<!--stream-end-->'));
             controller.close();
           } catch (err) {
+            controller.error(err);
