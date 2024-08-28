d8d435d978d56a9fb9c7a9ed0241173bed81fab7
Ny <johnnynode@gmail.com>
2024-08-28 02:25:16 +0000

    chore(ssr): update components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -43,3 +43,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
     render(vnode: VNode): ReadableStream {
       return new ReadableStream({
         async start(controller) {
+          try {
