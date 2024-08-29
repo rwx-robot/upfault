6462996e17984cb378d48a376daa1f3447cc3410
Ny <johnnynode@gmail.com>
2024-08-29 05:04:09 +0000

    feat(ssr): support components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -67,3 +67,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
       const reader = stream.getReader();
       try {
         while (true) {
+          const { done, value } = await reader.read();
