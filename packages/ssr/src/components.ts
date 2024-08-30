33270a638d0ac09279330862d1d315b3a60fde37
Ny <johnnynode@gmail.com>
2024-08-30 01:39:45 +0000

    feat(ssr): add components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -80,3 +80,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
     },
     async pipeToWebWritable(writable: WritableStream): Promise<void> {
       const stream = this.render({ 
+        type: VNodeType.ELEMENT, 
