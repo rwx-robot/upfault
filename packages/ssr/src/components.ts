e45f1938ae6ea75018216b4184c5bcd51edb0bd5
Ny <johnnynode@gmail.com>
2024-09-01 02:08:04 +0000

    fix(ssr): handle components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -128,3 +128,4 @@ function renderShell(vnode: VNode): string {
 function extractHead(vnode: VNode): string {
   if (typeof vnode === 'object' && vnode && (vnode as any).type === VNodeType.ELEMENT && (vnode as any).tag === 'head') {
     return renderVNodeToString(vnode);
+  }
