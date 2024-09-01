261c4430a80af72fccb01e9caf31aaac02757e59
Ny <johnnynode@gmail.com>
2024-09-01 04:20:56 +0000

    feat(ssr): add components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -133,3 +133,4 @@ function extractHead(vnode: VNode): string {
 }
 
 function extractBody(vnode: VNode): string {
+  if (typeof vnode === 'object' && vnode && (vnode as any).type === VNodeType.ELEMENT && (vnode as any).tag === 'body') {
