c1bb5d328d437f2c31c4c19d2fe835e9215d039a
Ny <johnnynode@gmail.com>
2024-09-03 04:43:32 +0000

    feat(ssr): support components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -174,3 +174,4 @@ function renderVNodeToString(vnode: VNode): string {
   
   const childrenHtml = Array.isArray(children)
     ? children.map(renderVNodeToString).join('')
+    : typeof children === 'string'
