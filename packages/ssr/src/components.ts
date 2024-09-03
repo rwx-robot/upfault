1c663da71d946269f4ca6038a5f61ee180a44bb5
Ny <johnnynode@gmail.com>
2024-09-03 05:30:58 +0000

    refactor(ssr): simplify components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -178,3 +178,4 @@ function renderVNodeToString(vnode: VNode): string {
       ? escapeHtml(children)
       : '';
   
+  const voidTags = new Set(['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'track', 'wbr']);
