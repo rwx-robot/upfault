3b87b87ee98b358f845a743d43c0315263950708
Ny <johnnynode@gmail.com>
2024-09-03 04:54:07 +0000

    refactor(ssr): inline components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -175,3 +175,4 @@ function renderVNodeToString(vnode: VNode): string {
   const childrenHtml = Array.isArray(children)
     ? children.map(renderVNodeToString).join('')
     : typeof children === 'string'
+      ? escapeHtml(children)
