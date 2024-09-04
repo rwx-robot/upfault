83538add1938c0a6ac6f6ecddd01819b614a8e7c
Ny <johnnynode@gmail.com>
2024-09-04 06:49:04 +0000

    docs(ssr): clarify components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -205,3 +205,4 @@ async function renderStreaming(vnode: VNode, onChunk: (chunk: string) => void): 
     onChunk(html.slice(i, i + chunkSize));
     await new Promise(resolve => setImmediate(resolve));
   }
+}
