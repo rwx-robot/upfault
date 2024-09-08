b8f6d266b3afbe116cdd71dcaee5c6d5eb119e4a
Ny <johnnynode@gmail.com>
2024-09-08 09:44:13 +0000

    test(ssr): assert components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -302,3 +302,4 @@ export function defineAsyncComponent(
           return typeof comp === 'function' 
             ? comp(props) 
             : (comp.render ? comp.render() : h('div', null, 'Loaded'));
+        }
