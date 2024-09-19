908972ba6906799d22f81258c5a133a5ad158813
Ny <johnnynode@gmail.com>
2024-09-19 04:31:17 +0000

    feat(ssr): implement components

--- packages/ssr/src/components.ts
+++ packages/ssr/src/components.ts
@@ -520,3 +520,4 @@ function matchPattern(name: string, pattern: string | RegExp): boolean {
 export function Fragment(props: { children?: any }): any {
   return {
     type: VNodeType.FRAGMENT,
+    tag: 'fragment',
