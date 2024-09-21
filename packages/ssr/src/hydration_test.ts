5b462a2b535b6d8f585d2da8b685620b82ffeb25
Ny <johnnynode@gmail.com>
2024-09-21 04:52:36 +0000

    test(ssr): cover hydration_test

--- packages/ssr/src/hydration_test.ts
+++ packages/ssr/src/hydration_test.ts
@@ -25,3 +25,4 @@ export interface HydrationOptions {
 
 export function hydrate(vnode: VNode, container: HTMLElement | string): ComponentInstance {
   const containerEl = typeof container === 'string' 
+    ? (document.querySelector(container) as HTMLElement)! 
