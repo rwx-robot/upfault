c8339e012c9b45ba181220233eabf3fe4c78658b
Ny <johnnynode@gmail.com>
2024-09-24 04:23:35 +0000

    feat(ssr): add hydration_test

--- packages/ssr/src/hydration_test.ts
+++ packages/ssr/src/hydration_test.ts
@@ -90,3 +90,4 @@ export function hydrateRoot(vnode: VNode, container: HTMLElement | string, optio
     (vnode as any).__hydrated = true;
     if (instance && (instance as any).vnode?.componentInstance?.onMounted) {
       (instance as any).vnode.componentInstance.onMounted.forEach((hook: () => void) => hook());
+    }
