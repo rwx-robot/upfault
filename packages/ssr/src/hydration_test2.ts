48da7ed27d5efd4511566eb84e0cf6b469004fb2
Ny <johnnynode@gmail.com>
2024-10-03 02:59:50 +0000

    chore(ssr): bump hydration_test2

--- packages/ssr/src/hydration_test2.ts
+++ packages/ssr/src/hydration_test2.ts
@@ -52,3 +52,4 @@ export function hydrate(vnode: VNode, container: HTMLElement | string): Componen
         }
       } else if (key in el) {
         (el as any)[key] = value;
+      } else if (value == null || value === false) {
