ea5fe9092ad0af1be505e814bfbaa51ff7b13ef0
Ny <johnnynode@gmail.com>
2024-10-15 08:28:28 +0000

    feat(ssr): add hydration

--- packages/ssr/src/hydration.ts
+++ packages/ssr/src/hydration.ts
@@ -78,3 +78,4 @@ export function hydrateRoot(vnode: VNode, container: HTMLElement | string, optio
 } = {}): ComponentInstance {
   const containerEl = typeof container === 'string' 
     ? (document.querySelector(container) as HTMLElement)! 
+    : container;
