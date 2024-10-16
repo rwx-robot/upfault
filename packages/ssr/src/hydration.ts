2e2718e0e6d2d938cfea08eae084c45323c52958
Ny <johnnynode@gmail.com>
2024-10-16 07:28:39 +0000

    test(ssr): add cases for hydration

--- packages/ssr/src/hydration.ts
+++ packages/ssr/src/hydration.ts
@@ -98,3 +98,4 @@ export function hydrateRoot(vnode: VNode, container: HTMLElement | string, optio
 
 export function hydrateNodeStream(
   stream: NodeJS.ReadableStream,
+  container: HTMLElement | string,
