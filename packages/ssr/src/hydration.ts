44d0f525ccde01212658a0890e418b6c991a3203
Ny <johnnynode@gmail.com>
2024-10-16 02:38:19 +0000

    feat(ssr): support hydration

--- packages/ssr/src/hydration.ts
+++ packages/ssr/src/hydration.ts
@@ -87,3 +87,4 @@ export function hydrateRoot(vnode: VNode, container: HTMLElement | string, optio
   const instance = hydrate(vnode, containerEl as HTMLElement);
   
   queueMicrotask(() => {
+    (vnode as any).__hydrated = true;
