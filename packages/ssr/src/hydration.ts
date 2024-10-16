0a1ce4442d7d17055fed0c6775c2d6e1284195d1
Ny <johnnynode@gmail.com>
2024-10-16 07:57:15 +0000

    feat(ssr): add hydration

--- packages/ssr/src/hydration.ts
+++ packages/ssr/src/hydration.ts
@@ -99,3 +99,4 @@ export function hydrateRoot(vnode: VNode, container: HTMLElement | string, optio
 export function hydrateNodeStream(
   stream: NodeJS.ReadableStream,
   container: HTMLElement | string,
+  options: {
