2a60297650cd86ea3f4bc9fe0e138ec2e2c99267
Ny <johnnynode@gmail.com>
2024-11-21 05:08:40 +0000

    docs(ssr): annotate middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -28,3 +28,4 @@ export interface SSRResponse {
 export interface MiddlewareOptions {
   renderOptions?: RenderOptions;
   render?: (vnode: any, options: any) => Promise<string>;
+  routes?: Array<{
