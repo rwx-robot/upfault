ea6b36d728348fd233c29e00afce179342717c9f
Ny <johnnynode@gmail.com>
2024-11-23 07:31:52 +0000

    fix(ssr): guard middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -79,3 +79,4 @@ export function createSSRMiddleware(options: MiddlewareOptions = {}) {
   return async function ssrMiddleware(
     req: SSRRequest,
     res: SSRResponse,
+    next?: () => Promise<void>
