2031e5ba2195c80f091b3c53cff6a60e04427c00
Ny <johnnynode@gmail.com>
2024-11-25 07:25:17 +0000

    feat(ssr): introduce middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -123,3 +123,4 @@ export function createSSRMiddleware(options: MiddlewareOptions = {}) {
           const { renderToNodeStream } = await import('./render');
           const stream = renderToNodeStream(vnode, renderOpts);
           
+          for await (const chunk of stream) {
