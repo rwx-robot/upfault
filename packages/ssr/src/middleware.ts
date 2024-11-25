2653a5100e51ba65664db10dae3559b270b79bec
Ny <johnnynode@gmail.com>
2024-11-25 05:32:19 +0000

    chore(ssr): bump middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -117,3 +117,4 @@ export function createSSRMiddleware(options: MiddlewareOptions = {}) {
         
         if (streaming) {
           res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          res.setHeader('Transfer-Encoding', 'chunked');
