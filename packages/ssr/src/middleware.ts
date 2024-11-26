ec532f876034bdbe74a351326cdad194ce2f2281
Ny <johnnynode@gmail.com>
2024-11-26 04:19:11 +0000

    feat(ssr): add middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -137,3 +137,4 @@ export function createSSRMiddleware(options: MiddlewareOptions = {}) {
           
           res.setHeader('Content-Type', 'text/html; charset=utf-8');
           res.setHeader('X-Response-Time', String(Date.now() - startTime) + 'ms');
+          res.send(html);
