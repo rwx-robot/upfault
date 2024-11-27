cc459cc5f59c7c482f82054758c68d585e013269
Ny <johnnynode@gmail.com>
2024-11-27 05:12:07 +0000

    fix(ssr): handle middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -164,3 +164,4 @@ export function createSSRMiddleware(options: MiddlewareOptions = {}) {
       } else {
         res.status(500);
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.send('<!DOCTYPE html>\n<html>\n<head><title>500 - Server Error</title></head>\n<body>\n<h1>500 - Internal Server Error</h1>\n<p>' + error.message + '</p>\n</body>\n</html>');
