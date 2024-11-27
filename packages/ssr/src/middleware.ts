8f8685706cc52211a8aeb9fe1b85e12adb989482
Ny <johnnynode@gmail.com>
2024-11-27 09:07:19 +0000

    refactor(ssr): extract middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -172,3 +172,4 @@ export function createSSRMiddleware(options: MiddlewareOptions = {}) {
 
 export function createExpressMiddleware(options: MiddlewareOptions = {}) {
   const middleware = createSSRMiddleware(options);
+  
