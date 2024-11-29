3650882c4a73a243cf29793a5c8e8b2106b4ecc1
Ny <johnnynode@gmail.com>
2024-11-29 03:46:56 +0000

    feat(ssr): add middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -201,3 +201,4 @@ export function createKoaMiddleware(options: MiddlewareOptions = {}) {
   
   return async function koaMiddleware(ctx: any, next: () => Promise<void>) {
     const adaptedReq = {
+      url: ctx.url,
