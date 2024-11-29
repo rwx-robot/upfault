ffbe64952a73ea175f0a8b4bc0fa82c5ff2365b7
Ny <johnnynode@gmail.com>
2024-11-29 08:31:14 +0000

    feat(ssr): introduce middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -210,3 +210,4 @@ export function createKoaMiddleware(options: MiddlewareOptions = {}) {
     };
     
     const adaptedRes = {
+      status: (code: number) => { ctx.status = code; return adaptedRes; },
