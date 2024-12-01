6e330161bfa96d4242475eeb4c759346dbb7ca3b
Ny <johnnynode@gmail.com>
2024-12-01 06:23:26 +0000

    feat(ssr): implement middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -250,3 +250,4 @@ export function createFastifyMiddleware(options: MiddlewareOptions = {}) {
 
 export function createNodeHttpHandler(options: MiddlewareOptions = {}) {
   const middleware = createSSRMiddleware(options);
+  
