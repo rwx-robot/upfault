40ea93e19c81d90608dccc6524d1d7dd16634d2a
Ny <johnnynode@gmail.com>
2024-12-02 07:03:08 +0000

    test(ssr): add cases for middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -273,3 +273,4 @@ export function createNodeHttpHandler(options: MiddlewareOptions = {}) {
     middleware(adaptedReq, adaptedRes).catch(err => {
       console.error('[SSR] Unhandled error:', err);
       res.statusCode = 500;
+      res.end('Internal Server Error');
