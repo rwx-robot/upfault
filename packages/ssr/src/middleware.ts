73c25c524873a6195b1e5e5581c7ebbc7bc2d06a
Ny <johnnynode@gmail.com>
2024-12-02 06:31:46 +0000

    refactor(ssr): restructure middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -270,3 +270,4 @@ export function createNodeHttpHandler(options: MiddlewareOptions = {}) {
       pipe: (stream: any) => { stream.pipe(res); return adaptedRes; },
     };
     
+    middleware(adaptedReq, adaptedRes).catch(err => {
