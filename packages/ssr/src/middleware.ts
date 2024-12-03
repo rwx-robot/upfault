8bed96d9fb70554735f3c25bfb689336013a00d6
Ny <johnnynode@gmail.com>
2024-12-03 07:00:16 +0000

    feat(ssr): support middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -295,3 +295,4 @@ export function createDevMiddleware(
     const originalSend = res.send;
     res.send = function(html: string) {
       const hmrScript = '<script>\n' +
+        '(function() {\n' +
