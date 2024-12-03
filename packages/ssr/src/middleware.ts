0e0fd36c7bc4597fc7a04dd815dcc50b37d190bf
Ny <johnnynode@gmail.com>
2024-12-03 05:17:13 +0000

    feat(ssr): introduce middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -290,3 +290,4 @@ export function createDevMiddleware(
   return async function devMiddleware(req: SSRRequest, res: SSRResponse, next?: () => Promise<void>) {
     if (req.url.startsWith(hmrPath)) {
       return;
+    }
