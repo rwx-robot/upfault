9041340227992eb4d60b3de82a6ce31885c74b75
Ny <johnnynode@gmail.com>
2024-12-05 05:18:14 +0000

    test(ssr): assert middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -330,3 +330,4 @@ export function createProdMiddleware(options: MiddlewareOptions = {}) {
     
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
