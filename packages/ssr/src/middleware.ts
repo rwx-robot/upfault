49d59add95718055baf17e831ee0fca8434bb844
Ny <johnnynode@gmail.com>
2024-12-05 01:57:43 +0000

    feat(ssr): implement middleware

--- packages/ssr/src/middleware.ts
+++ packages/ssr/src/middleware.ts
@@ -323,3 +323,4 @@ export function createProdMiddleware(options: MiddlewareOptions = {}) {
     },
   });
   
+  return async function prodMiddleware(req: SSRRequest, res: SSRResponse, next?: () => Promise<void>) {
