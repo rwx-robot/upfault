69b55aa82a4de1a0ac0979b5038fb93882357963
Ny <johnnynode@gmail.com>
2024-12-06 03:40:51 +0000

    chore(ssr): update render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -7,3 +7,5 @@
 import { h, type VNode, type ComponentInstance } from '@upfault/runtime';
 import { Priority, type SchedulerTask } from '@upfault/scheduler';
 import { VNodeType } from '@upfault/shared';
+
+export interface SSRContext {
