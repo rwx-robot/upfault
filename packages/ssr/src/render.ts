e7ce1b7fe7ee4633aa5c549afe5a62d5e39e94b2
Ny <johnnynode@gmail.com>
2024-12-10 07:43:09 +0000

    refactor(ssr): extract render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -103,3 +103,4 @@ export function renderToWebStream(
         });
         const headHtml = generateHeadHtml(context, options);
         controller.enqueue(new TextEncoder().encode(generateHeadHtml(context, options)));
+        controller.close();
