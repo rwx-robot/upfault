44ca87739e400f2f0ef95ed9b49bcb28f3b6afc8
Ny <johnnynode@gmail.com>
2024-12-14 05:51:18 +0000

    feat(ssr): support render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -181,3 +181,4 @@ function createSSRRenderer(context: SSRContext) {
     async renderToStream(vnode: VNode, onChunk: (chunk: string) => void) {
       await renderVNodeToStream(vnode, onChunk);
     },
+  };
