9524159bcc1a1088db411dbc1c23f9ad448dd536
Ny <johnnynode@gmail.com>
2024-12-15 08:02:22 +0000

    test(ssr): cover render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -208,3 +208,4 @@ function renderVNodeToString(vnode: VNode): string {
   
   // Element or Component
   const tag = typeof (vnode as any).type === 'string' ? (vnode as any).type : 'div';
+  const props = (vnode as any).props || {};
