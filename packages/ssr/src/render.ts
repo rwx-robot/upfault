e80b14b9582e3312b21bc2d6c6cf06d97350ec96
Ny <johnnynode@gmail.com>
2024-12-15 08:06:11 +0000

    fix(ssr): guard render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -209,3 +209,4 @@ function renderVNodeToString(vnode: VNode): string {
   // Element or Component
   const tag = typeof (vnode as any).type === 'string' ? (vnode as any).type : 'div';
   const props = (vnode as any).props || {};
+  const children = (vnode as any).children as VNode[];
