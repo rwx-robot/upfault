78030d9f343c275528c18c17c7433c3365f8725e
Ny <johnnynode@gmail.com>
2024-12-15 09:41:51 +0000

    feat(ssr): introduce render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -212,3 +212,4 @@ function renderVNodeToString(vnode: VNode): string {
   const children = (vnode as any).children as VNode[];
   
   const attrs = Object.entries(props)
+    .filter(([k, v]) => k !== 'children' && v != null && v !== false && !k.startsWith('on'))
