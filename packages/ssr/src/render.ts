247dbf5f602439158b3d0f25fdef80b4e2538553
Ny <johnnynode@gmail.com>
2024-12-16 05:03:44 +0000

    feat(ssr): introduce render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -221,3 +221,4 @@ function renderVNodeToString(vnode: VNode): string {
   
   const childrenHtml = Array.isArray(children)
     ? children.map(renderVNodeToString).join('')
+    : typeof children === 'string'
