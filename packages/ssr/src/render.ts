2418b7247046672b7d0607ecde21363f04c30115
Ny <johnnynode@gmail.com>
2024-12-16 06:38:21 +0000

    feat(ssr): support render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -223,3 +223,4 @@ function renderVNodeToString(vnode: VNode): string {
     ? children.map(renderVNodeToString).join('')
     : typeof children === 'string'
       ? escapeHtml(children)
+      : '';
