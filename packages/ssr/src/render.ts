fa4c6c42becba3445cd16e8add1ee5a22893574e
Ny <johnnynode@gmail.com>
2024-12-16 07:06:22 +0000

    feat(ssr): implement render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -226,3 +226,4 @@ function renderVNodeToString(vnode: VNode): string {
       : '';
   
   const voidTags = new Set(['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'track', 'wbr']);
+  
