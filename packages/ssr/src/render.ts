7946dbda4d46c5b1fbcd3149a9505b42843a685f
Ny <johnnynode@gmail.com>
2024-12-18 03:00:23 +0000

    feat(ssr): support render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -260,3 +260,4 @@ function injectHead(html: string, context: SSRContext, options: RenderOptions): 
   // 注入到 <head> 标签中
   if (html.includes('<head>')) {
     return html.replace('<head>', `<head>${headHtml}`);
+  }
