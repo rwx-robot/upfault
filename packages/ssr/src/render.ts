0ddf661a83cf45cc5e2a110e12e8d69b94d5869a
Ny <johnnynode@gmail.com>
2024-12-18 08:09:01 +0000

    fix(ssr): handle render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -273,3 +273,4 @@ function injectHead(html: string, context: SSRContext, options: RenderOptions): 
 
 function generateHeadHtml(context: SSRContext, options: RenderOptions): string {
   const parts: string[] = [];
+  
