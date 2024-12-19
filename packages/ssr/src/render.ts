7cc109b70366341aebe61f6e979c71e321b2dff0
Ny <johnnynode@gmail.com>
2024-12-19 03:09:46 +0000

    test(ssr): assert render

--- packages/ssr/src/render.ts
+++ packages/ssr/src/render.ts
@@ -283,3 +283,4 @@ function generateHeadHtml(context: SSRContext, options: RenderOptions): string {
   if (options.preload !== false) {
     for (const link of context.preloadLinks) {
       parts.push(`<link rel="preload" href="${link}" as="script">`);
+    }
