c2910de089697a0181ffa1a26375b9e0ed28aa25
Ny <johnnynode@gmail.com>
2024-12-22 06:45:23 +0000

    test(ssr): assert runtime-types.d

--- packages/ssr/src/runtime-types.d.ts
+++ packages/ssr/src/runtime-types.d.ts
@@ -40,3 +40,4 @@ declare module '@upfault/runtime' {
   export function onDeactivated(fn: () => void | Promise<void>): void;
   export function onErrorCaptured(fn: (err: Error, instance: any, info: string) => boolean | void): void;
   export function onRenderTracked(fn: (e: any) => void): void;
+  export function onRenderTriggered(fn: (e: any) => void): void;
