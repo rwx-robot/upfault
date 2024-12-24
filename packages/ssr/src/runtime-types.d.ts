a215d4d206d8c9c06b356c2b6d64fa538971df94
Ny <johnnynode@gmail.com>
2024-12-24 04:23:01 +0000

    refactor(ssr): simplify runtime-types.d

--- packages/ssr/src/runtime-types.d.ts
+++ packages/ssr/src/runtime-types.d.ts
@@ -73,3 +73,4 @@ declare module '@upfault/runtime' {
     insert: (child: Node, parent: HostElement, anchor?: HostElement | null) => void;
     remove: (child: Node) => void;
     patchProp: (el: HostElement, key: string, prevValue: any, nextValue: any) => void;
+    parentNode?: (node: HostElement) => HostElement | null;
