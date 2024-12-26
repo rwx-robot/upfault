3bf7f7049cc8d92017bc7ce50b832a26abdf9a21
Ny <johnnynode@gmail.com>
2024-12-26 09:06:25 +0000

    refactor(ssr): restructure streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -34,3 +34,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
   return {
     render(vnode: VNode): ReadableStream {
       return new ReadableStream({
+        async start(controller) {
