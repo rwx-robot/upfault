3d3a73c2c31f2474ee6a67cb14a6732db7a58c7c
Ny <johnnynode@gmail.com>
2024-12-27 04:04:38 +0000

    fix(ssr): handle streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -43,3 +43,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
             }
             
             await renderStreaming(vnode, (chunk) => {
+              controller.enqueue(new TextEncoder().encode(chunk));
