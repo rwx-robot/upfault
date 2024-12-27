69d26285eb37e9593e5faa2086b7430484022a20
Ny <johnnynode@gmail.com>
2024-12-27 03:14:29 +0000

    chore(ssr): clean up streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -41,3 +41,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
               controller.enqueue(new TextEncoder().encode(shell));
               shellSent = true;
             }
+            
