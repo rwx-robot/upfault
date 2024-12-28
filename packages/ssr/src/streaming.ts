e7acb29d5faf475221e8df3ebc29d4d789801cdc
Ny <johnnynode@gmail.com>
2024-12-28 08:26:33 +0000

    refactor(ssr): extract streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -74,3 +74,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
       const stream = this.render({ 
         type: VNodeType.ELEMENT, 
         tag: 'div', 
+        props: {}, 
