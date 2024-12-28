8f221748117482cae883947dac3ddd6917e8aa42
Ny <johnnynode@gmail.com>
2024-12-28 04:36:10 +0000

    feat(ssr): add streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -64,3 +64,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
           await new Promise<void>((resolve, reject) => {
             writable.write(value, (err) => err ? reject(err) : resolve());
           });
+        }
