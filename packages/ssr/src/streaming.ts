c9c1f2a808ff83c93643734d5ef4baa326e679cc
Ny <johnnynode@gmail.com>
2024-12-28 08:15:16 +0000

    feat(ssr): introduce streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -72,3 +72,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
     },
     async pipeToWebWritable(writable: WritableStream): Promise<void> {
       const stream = this.render({ 
+        type: VNodeType.ELEMENT, 
