293281fe658c0d7efc1bd2c41b77c11cd5e85721
Ny <johnnynode@gmail.com>
2024-12-28 03:26:43 +0000

    feat(ssr): support streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -61,3 +61,4 @@ export function createStreamRenderer(options: StreamingOptions = {}): StreamRend
         while (true) {
           const { done, value } = await reader.read();
           if (done) break;
+          await new Promise<void>((resolve, reject) => {
