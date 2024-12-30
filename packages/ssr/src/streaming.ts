e235fbae7afb51059a41ccf47f17b57ee1dbcc84
Ny <johnnynode@gmail.com>
2024-12-30 17:50:52 +0000

    fix(ssr): correct streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -170,3 +170,4 @@ function renderVNodeToString(vnode: VNode): string {
       ? escapeHtml(children)
       : '';
   
+  const voidTags = new Set(['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'track', 'wbr']);
