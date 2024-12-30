814975e8c8f4af3729ae6c5c5f9ed1bb61be685c
Ny <johnnynode@gmail.com>
2024-12-30 15:30:32 +0000

    feat(ssr): support streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -156,3 +156,4 @@ function renderVNodeToString(vnode: VNode): string {
   const props = (vnode as any).props || {};
   const children = (vnode as any).children as VNode[];
   
+  const attrs = Object.entries(props)
