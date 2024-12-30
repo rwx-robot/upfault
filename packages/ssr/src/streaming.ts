8eac4f2d6ab7b891ac2eb53a03dc07c9b50368ba
Ny <johnnynode@gmail.com>
2024-12-30 14:26:45 +0000

    fix(ssr): fix streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -149,3 +149,4 @@ function renderVNodeToString(vnode: VNode): string {
   
   if (type === VNodeType.FRAGMENT) {
     const children = (vnode as any).children as VNode[];
+    return children.map(renderVNodeToString).join('');
