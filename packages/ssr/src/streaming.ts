1716a6bdd248806410275cfb3cd0c6fb06e5b7b6
Ny <johnnynode@gmail.com>
2024-12-30 15:11:22 +0000

    feat(ssr): introduce streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -153,3 +153,4 @@ function renderVNodeToString(vnode: VNode): string {
   }
   
   const tag = typeof (vnode as any).type === 'string' ? (vnode as any).type : 'div';
+  const props = (vnode as any).props || {};
