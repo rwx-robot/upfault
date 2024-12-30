31641a982648cf3b71147f1c90004e52508985d3
Ny <johnnynode@gmail.com>
2024-12-30 17:28:16 +0000

    fix(ssr): fix streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -167,3 +167,4 @@ function renderVNodeToString(vnode: VNode): string {
   const childrenHtml = Array.isArray(children)
     ? children.map(renderVNodeToString).join('')
     : typeof children === 'string'
+      ? escapeHtml(children)
