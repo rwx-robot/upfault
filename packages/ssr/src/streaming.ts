c8048042d23fc1a9597a2164bb1e4d46fbcc850a
Ny <johnnynode@gmail.com>
2024-12-30 15:54:33 +0000

    chore(ssr): bump streaming

--- packages/ssr/src/streaming.ts
+++ packages/ssr/src/streaming.ts
@@ -158,3 +158,4 @@ function renderVNodeToString(vnode: VNode): string {
   
   const attrs = Object.entries(props)
     .filter(([k, v]) => k !== 'children' && v != null && v !== false && !k.startsWith('on'))
+    .map(([k, v]) => {
